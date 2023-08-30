447f75bab2e05ca814cf86e535399abe39acae80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:08:36 +0000

    Add package flowmatic/0.1.4/flowmatic-0.1.4.tar.gz

---
+++ packages/flowmatic/flowmatic-0.1.4.tar.gz/flowmatic-0.1.4/flowmatic/gui/screens/__init__.py
@@ -0,0 +1,5 @@
+from .form_screen import FormScreen
+from .menu_screen import MenuScreen
+from .title_screen import TitleScreen
+from .screen import FlowScreen
+from .element_screen import ElementScreen
