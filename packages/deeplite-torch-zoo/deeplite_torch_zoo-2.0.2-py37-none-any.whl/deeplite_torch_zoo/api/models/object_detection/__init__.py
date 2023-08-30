12cc0945d909a930503fca9c5e90e679b6ebeb22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 14:08:03 +0000

    Add package deeplite-torch-zoo/2.0.2/deeplite_torch_zoo-2.0.2-py37-none-any.whl

---
+++ packages/deeplite-torch-zoo/deeplite_torch_zoo-2.0.2-py37-none-any.whl/deeplite_torch_zoo/api/models/object_detection/__init__.py
@@ -0,0 +1,4 @@
+from deeplite_torch_zoo.api.models.object_detection.timm_yolo import *  # pylint: disable=unused-import
+from deeplite_torch_zoo.api.models.object_detection.flexible_yolo import *  # pylint: disable=unused-import
+from deeplite_torch_zoo.api.models.object_detection.yolo import *  # pylint: disable=unused-import
+from deeplite_torch_zoo.api.models.object_detection.yolov6 import *  # pylint: disable=unused-import
