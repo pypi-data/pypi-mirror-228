788d92440c9592cec80224a8b6b1fb1dd3e4af93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:09:40 +0000

    Add package fictus/0.0.8/fictus-0.0.8.tar.gz

---
+++ packages/fictus/fictus-0.0.8.tar.gz/fictus-0.0.8/fictus/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["FictusFileSystem", "Renderer", "DisplayModel"]
+
+
+from .displaymodel import DisplayModel
+from .fictusfilesystem import FictusFileSystem
+from .renderer import Renderer
