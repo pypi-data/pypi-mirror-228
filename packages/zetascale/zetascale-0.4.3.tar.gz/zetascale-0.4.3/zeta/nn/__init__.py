8b55054e5500a628c2a54e1103783e9ab12ad12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 14:08:05 +0000

    Add package zetascale/0.4.3/zetascale-0.4.3.tar.gz

---
+++ packages/zetascale/zetascale-0.4.3.tar.gz/zetascale-0.4.3/zeta/nn/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2022 Agora
+# Licensed under The MIT License [see LICENSE for details]
+
+######### Attention
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+from zeta.nn import *
+from zeta.nn.attention.cross_attention import CrossAttend
+from zeta.nn.attention.multihead_attention import MultiheadAttention
+from zeta.nn.attention.flash_attention2 import FlashAttentionTwo
+
+
+
+
+#utils
+from zeta.nn.utils.utils import *
+from zeta.nn.utils.helpers import *
+from zeta.nn.utils.inference_helpers import *
+from zeta.nn.utils.model_utils import *
+from zeta.nn.utils.tensor_helpers import *
+from zeta.nn.utils.multi_modal_helpers import * 
