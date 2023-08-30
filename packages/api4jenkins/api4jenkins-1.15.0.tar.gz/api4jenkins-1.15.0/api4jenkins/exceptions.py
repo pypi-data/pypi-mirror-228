2c8959949abec65aed872d51859363e56fa8712f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:08:57 +0000

    Add package api4jenkins/1.15.0/api4jenkins-1.15.0.tar.gz

---
+++ packages/api4jenkins/api4jenkins-1.15.0.tar.gz/api4jenkins-1.15.0/api4jenkins/exceptions.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+class JenkinsAPIException(Exception):
+    pass
+
+
+class ItemNotFoundError(JenkinsAPIException):
+    pass
+
+
+class AuthenticationError(JenkinsAPIException):
+    pass
+
+
+class ItemExistsError(JenkinsAPIException):
+    pass
+
+
+class UnsafeCharacterError(JenkinsAPIException):
+    pass
+
+
+class BadRequestError(JenkinsAPIException):
+    pass
+
+
+class ServerError(JenkinsAPIException):
+    pass
