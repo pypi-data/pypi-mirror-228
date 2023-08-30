f3265fdee991a4b6012f5c17b94f3504956a70a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 14:07:11 +0000

    Add package react-stats/0.5/react-stats-0.5.tar.gz

---
+++ packages/react-stats/react-stats-0.5.tar.gz/react-stats-0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="react-stats",
+    version="0.5",
+    packages=find_packages(),
+    package_data={
+        'react_stats.commands': ['*.json']
+    },
+    entry_points={
+        'console_scripts': [
+            'get-stats = react_stats.commands.get_stats:main',
+            'make-assets = react_stats.commands.make_assets:main',
+            'auto-assets = react_stats.commands.auto_assets:main'
+        ],
+    },
+    install_requires=[
+        'tabulate',
+        'chardet',
+        'pandas',
+        'asciibars'
+    ],
+    include_package_data=True
+)
