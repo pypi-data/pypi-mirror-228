1bdb68ebe4966f2f693c3eaed551ae473de358dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 14:07:41 +0000

    Add package ape-alchemy/0.6.3/ape-alchemy-0.6.3.tar.gz

---
+++ packages/ape-alchemy/ape-alchemy-0.6.3.tar.gz/ape-alchemy-0.6.3/tests/test_integration.py
@@ -0,0 +1,30 @@
+import pytest
+from ape import networks
+from ape.utils import ZERO_ADDRESS
+
+from ape_alchemy.provider import Alchemy
+
+
+@pytest.mark.parametrize(
+    "ecosystem,network",
+    [
+        ("ethereum", "mainnet"),
+        ("ethereum", "goerli"),
+        ("ethereum", "sepolia"),
+        ("arbitrum", "mainnet"),
+        ("arbitrum", "goerli"),
+        ("base", "mainnet"),
+        ("base", "goerli"),
+        ("optimism", "mainnet"),
+        ("optimism", "goerli"),
+        ("polygon", "mainnet"),
+        ("polygon", "mumbai"),
+    ],
+)
+def test_alchemy(ecosystem, network):
+    ecosystem_cls = networks.get_ecosystem(ecosystem)
+    network_cls = ecosystem_cls.get_network(network)
+    with network_cls.use_provider("alchemy") as provider:
+        assert isinstance(provider, Alchemy)
+        assert provider.get_balance(ZERO_ADDRESS) > 0
+        assert provider.get_block(0)
