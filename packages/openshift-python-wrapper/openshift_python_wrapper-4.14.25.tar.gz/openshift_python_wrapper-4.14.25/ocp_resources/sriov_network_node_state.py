aa94a3d6c602b2450ccd3ab3cc0b36eba8abe9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 16:10:05 +0000

    Add package openshift-python-wrapper/4.14.25/openshift_python_wrapper-4.14.25.tar.gz

---
+++ packages/openshift-python-wrapper/openshift_python_wrapper-4.14.25.tar.gz/openshift_python_wrapper-4.14.25/ocp_resources/sriov_network_node_state.py
@@ -0,0 +1,45 @@
+from ocp_resources.resource import NamespacedResource
+from ocp_resources.utils import TimeoutExpiredError, TimeoutSampler
+
+
+class SriovNetworkNodeState(NamespacedResource):
+    """
+    SriovNetworkNodeState object.
+    """
+
+    api_group = NamespacedResource.ApiGroup.SRIOVNETWORK_OPENSHIFT_IO
+
+    @property
+    def interfaces(self):
+        return self.instance.status.interfaces
+
+    @staticmethod
+    def iface_name(iface):
+        return iface.name
+
+    @staticmethod
+    def pciaddress(iface):
+        return iface.pciAddress
+
+    @staticmethod
+    def totalvfs(iface):
+        return iface.totalvfs
+
+    def wait_for_status_sync(self, wanted_status, timeout=1000):
+        self.logger.info(
+            f"Wait for {self.kind} {self.name} status to be {wanted_status}"
+        )
+        try:
+            for sample in TimeoutSampler(
+                wait_timeout=timeout,
+                sleep=3,
+                func=lambda: self.instance.status.syncStatus,
+            ):
+                if sample == wanted_status:
+                    return
+        except TimeoutExpiredError:
+            self.logger.error(
+                f"after {timeout} seconds, {self.name} status is"
+                f" {self.instance.status.syncStatus}"
+            )
+            raise
