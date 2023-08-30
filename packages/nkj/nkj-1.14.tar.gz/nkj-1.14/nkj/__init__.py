cf5cbc28a8c37af995ab775a16dd839ea0d54921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:10:40 +0000

    Add package nkj/1.14/nkj-1.14.tar.gz

---
+++ packages/nkj/nkj-1.14.tar.gz/nkj-1.14/nkj/__init__.py
@@ -0,0 +1,12 @@
+#
+# [name] __init__.py
+#
+# Written by Yoshikazu NAKAJIMA
+#
+
+from nkj.str import *
+from nkj.core import *
+from nkj.math import *
+from nkj.os import *
+
+__all__ = ['str', 'core', 'math', 'os']
