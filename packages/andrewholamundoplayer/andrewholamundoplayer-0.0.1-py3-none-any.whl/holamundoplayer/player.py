24209c4b2606f5b18ba451353ee106286930b724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:08:54 +0000

    Add package andrewholamundoplayer/0.0.1/andrewholamundoplayer-0.0.1-py3-none-any.whl

---
+++ packages/andrewholamundoplayer/andrewholamundoplayer-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,23 @@
+"""
+Este es el modulo que incluye la clase
+de reporductor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproductor de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que recibio como prametro
+        Parameters:
+        song (str): este es un string con el path de la cancion
+
+        Returns:
+        int: Devuelve 1 si reproduce con exito, sino devuelve 0
+        """
+
+    def stop(self):
+        print("Stopping")
