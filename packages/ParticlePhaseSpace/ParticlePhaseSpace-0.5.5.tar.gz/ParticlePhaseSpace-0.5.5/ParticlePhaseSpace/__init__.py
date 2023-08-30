114afaaa853d0c26462b11029643378bab756f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:09:17 +0000

    Add package ParticlePhaseSpace/0.5.5/ParticlePhaseSpace-0.5.5.tar.gz

---
+++ packages/ParticlePhaseSpace/ParticlePhaseSpace-0.5.5.tar.gz/ParticlePhaseSpace-0.5.5/ParticlePhaseSpace/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.5.5'
+
+# from ParticlePhaseSpace._ParticlePhaseSpace import PhaseSpace
+from ParticlePhaseSpace.__unit_config__ import ParticlePhaseSpaceUnits, UnitSet
+from ParticlePhaseSpace._ParticlePhaseSpace import PhaseSpace
