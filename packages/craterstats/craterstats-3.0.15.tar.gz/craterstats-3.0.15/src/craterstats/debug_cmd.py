5184bab1449f31f59fe0a40bec27b38db9cc5cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 16:10:05 +0000

    Add package craterstats/3.0.15/craterstats-3.0.15.tar.gz

---
+++ packages/craterstats/craterstats-3.0.15.tar.gz/craterstats-3.0.15/src/craterstats/debug_cmd.py
@@ -0,0 +1,16 @@
+
+import cli
+
+def main():
+    out = ' -o d:/mydocs/tmp/out.png'
+    cmd = '--help'
+    cmd = '-p source=sample/sample.scc'+out
+    cmd = r'-cs MarsNI2001 -p source=D:\mydocs\tmp\test_opencratertools\test1.scc -p type=bpois,range=[8,20]' + out
+
+    
+    print(f'\nDebugging command: craterstats '+cmd)
+    a = cmd.split()
+    cli.main(a)
+
+if __name__ == '__main__':
+    main()
