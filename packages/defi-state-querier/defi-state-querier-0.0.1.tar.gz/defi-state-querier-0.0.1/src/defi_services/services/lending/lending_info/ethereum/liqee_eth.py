a1f24023b503f102ef0789fa7bdaabf06e7f176f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 16:10:07 +0000

    Add package defi-state-querier/0.0.1/defi-state-querier-0.0.1.tar.gz

---
+++ packages/defi-state-querier/defi-state-querier-0.0.1.tar.gz/defi-state-querier-0.0.1/src/defi_services/services/lending/lending_info/ethereum/liqee_eth.py
@@ -0,0 +1,31 @@
+LIQEE_ETH = {
+    "name": "Liqee Lending Pool",
+    "rewardToken": "0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0",
+    "comptrollerAddress": "",
+    "lensAddress": "",
+    "controllerAddress": "0x3ba6e5e5df88b9a88b2c19449778a4754170ea17",
+    "lendingDataAddress": "0x4b30c26d61be2f76bc7d9f2c5edbacba84982358",
+    "poolToken": "0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0",
+    "type": "LENDING_POOL",
+    "forked": "compound",
+    "reservesList": {
+        "0x0a5e677a6a24b2f1a2bf4f3bffc443231d2fdec8": {
+            "cToken": "0xf54954ba7e3cdfda23941753b48039ab5192aea0"
+        },
+        "0xb986f3a2d91d3704dc974a24fb735dcc5e3c1e70": {
+            "cToken": "0xab9c8c81228abd4687078ebda5ae236789b08673"
+        },
+        "0x8dc6987f7d8e5ae9c39f767a324c5e46c1f731eb": {
+            "cToken": "0xa4c13398dadb3a0a7305647b406acdcd0689fcc5"
+        },
+        "0xc4ba45bee9004408403b558a26099134282f2185": {
+            "cToken": "0x3481e1a5a8014f9c7e03322e4d4532d8ec723409"
+        },
+        "0x966e726853ca97449f458a3b012318a08b508202": {
+            "cToken": "0xaab2bab88ceedcf6788f45885155b278fad09110"
+        },
+        "0x32f9063bc2a2a57bcbe26ef662dc867d5e6446d1": {
+            "cToken": "0xb0ffbd1e81b60c4e8a8e19cef3a6a92fe18be86d"
+        }
+    }
+}
