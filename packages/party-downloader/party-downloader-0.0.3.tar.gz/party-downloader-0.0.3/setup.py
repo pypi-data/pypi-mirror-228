be676bb94e83c9e711278fc5f39d2f56b6587ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 14:08:28 +0000

    Add package party-downloader/0.0.3/party-downloader-0.0.3.tar.gz

---
+++ packages/party-downloader/party-downloader-0.0.3.tar.gz/party-downloader-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='party-downloader',
+    version='0.0.3',
+    # description='A description of your package',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'tqdm',
+        'beautifulsoup4',
+    ],
+)
