6dd4f875d4e99b1445e14577c2ededd0d92c5773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:10:14 +0000

    Add package avrox/0.0.1/avrox-0.0.1.tar.gz

---
+++ packages/avrox/avrox-0.0.1.tar.gz/avrox-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="avrox",
+    version="0.0.1",
+    author="avrox",
+    author_email="avrox163@163.com",
+    description="  有好玩的哦！ \n  It's fun!",
+    #long_description_content_type="text/markdown",
+    url="https://github.com/avrox-github/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"]
+)
