85f28211e6394312f34e8d512254610cfb59a876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:10:10 +0000

    Add package hawkflow/0.0.97/hawkflow-0.0.97.tar.gz

---
+++ packages/hawkflow/hawkflow-0.0.97.tar.gz/hawkflow-0.0.97/src/hawkflowclient/_endpoints.py
@@ -0,0 +1,55 @@
+import os
+import json
+
+from ._validation import _validate_timed_data
+from ._validation import _validate_metric_data
+from ._validation import _validate_exception_data
+from ._validation import _clean_process, _clean_meta, _clean_meta_key
+
+
+def _timed_data(process: str, meta: str, uid: str) -> str:
+    process = _clean_process(process)
+    meta = _clean_meta(meta)
+
+    _validate_timed_data(process, meta, uid)
+
+    x = json.dumps({
+        "process": process,
+        "meta": meta,
+        "uid": uid
+    })
+
+    return x
+
+
+def _metric_data(process: str, meta: str, items: dict, df: int) -> str:
+    process = _clean_process(process)
+    meta = _clean_meta(meta)
+    items = _clean_meta_key(items)
+
+    _validate_metric_data(process, meta, items, df)
+
+    x = json.dumps({
+        "process": process,
+        "meta": meta,
+        "items": items,
+        "df": df
+    })
+
+    return x
+
+
+def _exception_data(process: str, meta: str, exception_text: str) -> str:
+    process = _clean_process(process)
+    meta = _clean_meta(meta)
+
+    _validate_exception_data(process, meta, exception_text)
+
+    x = json.dumps({
+        "process": process,
+        "meta": meta,
+        "exception": exception_text
+    })
+
+    return x
+
