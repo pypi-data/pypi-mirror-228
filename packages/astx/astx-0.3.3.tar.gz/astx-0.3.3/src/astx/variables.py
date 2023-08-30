8ce59233d373fda40b2c7f50c7f37fa94310384d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:08:30 +0000

    Add package astx/0.3.3/astx-0.3.3.tar.gz

---
+++ packages/astx/astx-0.3.3.tar.gz/astx-0.3.3/src/astx/variables.py
@@ -0,0 +1,60 @@
+"""Module for Variables."""
+from typing import List, Tuple
+
+from public import public
+
+from astx.base import (
+    ASTKind,
+    DataType,
+    Expr,
+    ExprType,
+    SourceLocation,
+    StatementType,
+)
+from astx.blocks import Block
+from astx.datatypes import DataTypeOps
+
+
+@public
+class VarDecl(StatementType):
+    """AST class for variable declaration."""
+
+    var_names: List[Tuple[str, Expr]]
+    type_name: str
+    body: Block
+
+    def __init__(
+        self,
+        var_names: List[Tuple[str, Expr]],
+        type_name: str,
+        body: Block,
+        loc: SourceLocation = SourceLocation(0, 0),
+    ) -> None:
+        """Initialize the VarExprAST instance."""
+        self.loc = loc
+        self.var_names = var_names
+        self.type_name = type_name
+        self.body = body
+        self.kind = ASTKind.VarKind
+
+
+@public
+class Variable(DataTypeOps):
+    """AST class for the variable usage."""
+
+    type_: ExprType
+    value: DataType
+
+    def __init__(
+        self,
+        name: str,
+        type_: ExprType,
+        value: DataType,
+        loc: SourceLocation = SourceLocation(0, 0),
+    ) -> None:
+        """Initialize the Variable instance."""
+        super().__init__(loc)
+        self.name = name
+        self.type_ = type_
+        self.kind = ASTKind.VariableKind
+        self.value: DataType = value
