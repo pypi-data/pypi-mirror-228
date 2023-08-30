17fe80406f58d45ee8abcfbf5cfde9aca57847e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 15:08:37 +0000

    Add package PyDemoTest/0.1.0/PyDemoTest-0.1.0-py3-none-any.whl

---
+++ packages/PyDemoTest/PyDemoTest-0.1.0-py3-none-any.whl/PyDemoTest/main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Test: Uploading to Pypi.org!')
+
+
+def multiply(x, y):
+    print(f"{x} * {y} = { x * y}")
+    return x*y
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Pypi.org')
+    multiply(2, 2)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
