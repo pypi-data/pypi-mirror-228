996888d16cae89e9f6ca5f6f8a18ee6bceed6ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:13:13 +0000

    Add package gdsfactory/7.4.1/gdsfactory-7.4.1.tar.gz

---
+++ packages/gdsfactory/gdsfactory-7.4.1.tar.gz/gdsfactory-7.4.1/gdsfactory/samples/02_component_autoname.py
@@ -0,0 +1,13 @@
+"""When you create components you have to make sure they have unique names.
+
+the cell decorator gives unique names to components that depend on their parameters.
+"""
+
+from __future__ import annotations
+
+import gdsfactory as gf
+
+
+def test_autoname() -> None:
+    c1 = gf.components.straight(length=5)
+    assert c1.name.split("_")[0] == "straight"
