ceb6e781fed87532f0aa7291f8c296df54cec6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:07:44 +0000

    Add package gen3-util/0.0.7rc1/gen3_util-0.0.7rc1.tar.gz

---
+++ packages/gen3-util/gen3_util-0.0.7rc1.tar.gz/gen3_util-0.0.7rc1/tests/integration/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.fixture
+def dependency() -> str:
+    """Include fixtures here."""
+    return "TODO"
+
+
+@pytest.fixture
+def data_bucket() -> str:
+    """Expected data bucket, keep in sync with development.aced-idp.org."""
+    return "aced-ohsu-development"
