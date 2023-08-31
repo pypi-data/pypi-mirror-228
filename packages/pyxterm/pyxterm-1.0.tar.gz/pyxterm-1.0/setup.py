53055669e5a7d8babeaa39490d00c8dc9229ae9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:14:41 +0000

    Add package pyxterm/1.0/pyxterm-1.0.tar.gz

---
+++ packages/pyxterm/pyxterm-1.0.tar.gz/pyxterm-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyxterm",
+    version="1.0",
+    author="Akame23",
+    author_email="zakamett@gmail.com",
+    description="A python module to customize terminal and add coloring.",
+    url="https://github.com/lutherantz/pyshode",
+    packages=find_packages(),
+)
