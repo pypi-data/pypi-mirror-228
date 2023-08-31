d6a4ea30d9d6018d8b718575201910c2828ccfec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:09:06 +0000

    Add package css-inline/0.10.5/css_inline-0.10.5-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/css-inline/css_inline-0.10.5-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/css_inline/__init__.pyi
@@ -0,0 +1,34 @@
+class CSSInliner:
+    def __init__(
+        self,
+        keep_style_tags: bool = False,
+        keep_link_tags: bool = False,
+        base_url: str | None = None,
+        load_remote_stylesheets: bool = True,
+        extra_css: str | None = None,
+        preallocate_node_capacity: int | None = None,
+    ) -> None: ...
+    def inline(self, html: str) -> str: ...
+    def inline_many(self, html: list[str]) -> list[str]: ...
+
+def inline(
+    html: str,
+    keep_style_tags: bool = False,
+    keep_link_tags: bool = False,
+    base_url: str | None = None,
+    load_remote_stylesheets: bool = True,
+    extra_css: str | None = None,
+    preallocate_node_capacity: int | None = None,
+) -> str: ...
+
+def inline_many(
+    html: list[str],
+    keep_style_tags: bool = False,
+    keep_link_tags: bool = False,
+    base_url: str | None = None,
+    load_remote_stylesheets: bool = True,
+    extra_css: str | None = None,
+    preallocate_node_capacity: int | None = None,
+) -> list[str]: ...
+
+class InlineError(ValueError): ...
