c3206f2129ab873fd4f862626682aa19282a904c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:11:41 +0000

    Add package watlow/0.6.1/watlow-0.6.1.tar.gz

---
+++ packages/watlow/watlow-0.6.1.tar.gz/watlow-0.6.1/setup.py
@@ -0,0 +1,52 @@
+"""Install parameters for CLI and python import."""
+from setuptools import setup
+
+with open('README.md') as in_file:
+    long_description = in_file.read()
+
+setup(
+    name="watlow",
+    version="0.6.1",
+    description="Python driver for Watlow EZ-Zone temperature controllers.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/numat/watlow/",
+    author="Patrick Fuller",
+    author_email="pat@numat-tech.com",
+    maintainer="Alex Ruddick",
+    maintainer_email="alex@numat-tech.com",
+    packages=["watlow"],
+    install_requires=[
+        'pymodbus>=2.4.0; python_version == "3.8"',
+        'pymodbus>=2.4.0; python_version == "3.9"',
+        'pymodbus>=3.0.2,<3.6.0; python_version >= "3.10"',
+        "pyserial",
+        "crcmod"],
+    extras_require={
+        'test': [
+            'mypy==1.5.1',
+            'pytest',
+            'pytest-cov',
+            'pytest-asyncio',
+            'ruff==0.0.286',
+            'types-pyserial',
+        ],
+    },
+    entry_points={
+        "console_scripts": [("watlow = watlow:command_line")]
+    },
+    license="GPLv2",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Development Status :: 4 - Beta",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)"
+    ]
+)
