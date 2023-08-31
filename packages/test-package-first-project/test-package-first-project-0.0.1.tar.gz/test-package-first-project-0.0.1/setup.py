94364b0b54fd56269b86e8da8d28482fb237f4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:07:40 +0000

    Add package test-package-first-project/0.0.1/test-package-first-project-0.0.1.tar.gz

---
+++ packages/test-package-first-project/test-package-first-project-0.0.1.tar.gz/test-package-first-project-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+  
+setuptools.setup(
+    name="test-package-first-project",
+    version="0.0.1",
+    author="Arm pongpol",
+    packages=["my_package"],
+    install_requires=[]
+)
