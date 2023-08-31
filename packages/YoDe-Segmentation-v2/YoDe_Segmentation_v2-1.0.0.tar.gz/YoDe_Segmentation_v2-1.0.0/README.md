4d4fd28f4610d8140cd15f663e8d3dc994628ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:13:30 +0000

    Add package YoDe-Segmentation-v2/1.0.0/YoDe_Segmentation_v2-1.0.0.tar.gz

---
+++ packages/YoDe-Segmentation-v2/YoDe_Segmentation_v2-1.0.0.tar.gz/YoDe_Segmentation_v2-1.0.0/README.md
@@ -0,0 +1,13 @@
+# YoDe-Segmentation
+**An open sourced software for automatic chemical molecular structure maps extraction.**
+
+## Project link
+https://github.com/OneChorm/YoDe-Segmentation \
+Please raise issues on the project link if you encounter any problems.
+
+## Author info
+Author: OneChorm \
+Email: zconechorm@163.com
+
+## Acknowledgement
+The software will be subject to usage restrictions under the MIT license.
---
+++ packages/YoDe-Segmentation-v2/YoDe_Segmentation_v2-1.0.0.tar.gz/YoDe_Segmentation_v2-1.0.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# with open('requirements.txt', 'r') as reqs:
+#     requirements = reqs.read().split()
+
+setuptools.setup(
+    name="YoDe_Segmentation_v2",
+    version="1.0.0",
+    author="zconechorm",
+    author_email="Email:zconechorm@163.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OneChorm/YoDe-Segmentation",
+    packages=setuptools.find_namespace_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'yode_seg = YODE_Segmentation.predict_molecular:run_main',
+        ]
+    },
+    # install_requires=[
+    #     'numpy',
+    #     'pandas >= 1.4.0, != 1.4.4, <= 2.0.3',
+    # ],
+    # install_requires=requirements,
+)
