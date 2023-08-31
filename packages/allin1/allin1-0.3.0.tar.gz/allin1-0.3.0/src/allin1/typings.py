ff48d3f0cad2b736ebd54ec587680b2670873c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:09:09 +0000

    Add package allin1/0.3.0/allin1-0.3.0.tar.gz

---
+++ packages/allin1/allin1-0.3.0.tar.gz/allin1-0.3.0/src/allin1/typings.py
@@ -0,0 +1,36 @@
+import torch
+from os import PathLike
+from pathlib import Path
+from typing import List, Dict, Optional, Union
+from dataclasses import dataclass
+from numpy.typing import NDArray
+
+PathLike = Union[str, PathLike]
+
+
+@dataclass
+class AllInOneOutput:
+  logits_beat: torch.FloatTensor = None
+  logits_downbeat: torch.FloatTensor = None
+  logits_section: torch.FloatTensor = None
+  logits_function: torch.FloatTensor = None
+  embeddings: torch.FloatTensor = None
+
+
+@dataclass
+class Segment:
+  start: float
+  end: float
+  label: str
+
+
+@dataclass
+class AnalysisResult:
+  path: Path
+  bpm: int
+  beats: List[float]
+  downbeats: List[float]
+  beat_positions: List[int]
+  segments: List[Segment]
+  activations: Optional[Dict[str, NDArray]] = None
+  embeddings: Optional[NDArray] = None
