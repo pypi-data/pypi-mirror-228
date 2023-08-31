e426fef38202d9aea20b314ca6da7f7910e8dc01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:08:45 +0000

    Add package colour-datasets/0.2.2/colour_datasets-0.2.2-py3-none-any.whl

---
+++ packages/colour-datasets/colour_datasets-0.2.2-py3-none-any.whl/colour_datasets/records/__init__.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+
+from .configuration import Configuration, sandbox, use_sandbox
+from .zenodo import Community, Record
+
+__all__ = [
+    "Configuration",
+    "sandbox",
+    "use_sandbox",
+]
+__all__ += [
+    "Community",
+    "Record",
+]
+
+_COMMUNITY: Community | None = None
+"""
+Singleton instance of the *Zenodo* community that holds the datasets
+information.
+"""
+
+
+def datasets() -> Community:
+    """
+    Singleton factory that returns *Zenodo* community that holds the datasets
+    information.
+
+    Returns
+    -------
+    :class:`colour_datasets.Community`
+        Singleton instance of the *Zenodo* community.
+
+    Examples
+    --------
+    # Doctests skip for Python 2.x compatibility.
+    >>> datasets()["3245883"].title  # doctest: +SKIP
+    'Camera Spectral Sensitivity Database - Jiang et al. (2013)'
+    """
+
+    global _COMMUNITY  # noqa: PLW0603
+
+    if _COMMUNITY is None:
+        _COMMUNITY = Community.from_id(Configuration().community)
+
+    return _COMMUNITY
+
+
+__all__ += [
+    "datasets",
+]
