e28149f51514d9b5ef6cf6c2b41eb8724f15d9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:40 +0000

    Add package ellar/0.4.5/ellar-0.4.5.tar.gz

---
+++ packages/ellar/ellar-0.4.5.tar.gz/ellar-0.4.5/ellar/core/interceptors/consumer.py
@@ -0,0 +1,56 @@
+import functools
+import typing as t
+
+from ellar.common import EllarInterceptor, IExecutionContext, IInterceptorsConsumer
+from ellar.common.constants import ROUTE_INTERCEPTORS, SCOPE_RESPONSE_STARTED
+from ellar.di import injectable
+
+if t.TYPE_CHECKING:  # pragma: no cover
+    from ellar.common.routing import RouteOperationBase
+
+
+@injectable
+class EllarInterceptorConsumer(IInterceptorsConsumer):
+    def get_interceptor(
+        self,
+        context: IExecutionContext,
+        interceptor: t.Union[t.Type[EllarInterceptor], EllarInterceptor],
+    ) -> EllarInterceptor:
+        if isinstance(interceptor, type):
+            return t.cast(
+                EllarInterceptor, context.get_service_provider().get(interceptor)
+            )
+        return interceptor
+
+    async def execute(
+        self, context: IExecutionContext, route_operation: "RouteOperationBase"
+    ) -> t.Any:
+        reflector = context.get_app().reflector
+        route_interceptors: t.List[EllarInterceptor] = list(
+            map(
+                functools.partial(self.get_interceptor, context),
+                reflector.get_all_and_override(
+                    ROUTE_INTERCEPTORS, *[context.get_handler(), context.get_class()]
+                )
+                or context.get_app().get_interceptors(),
+            )
+        )
+        route_interceptors_length = len(route_interceptors or [])
+
+        if route_interceptors:
+
+            async def handler(idx: int) -> t.Any:
+                if idx >= route_interceptors_length:
+                    return await route_operation.handle_request(context=context)
+                return await route_interceptors[idx].intercept(
+                    context, functools.partial(handler, idx + 1)
+                )
+
+            res = await handler(0)
+        else:
+            res = await route_operation.handle_request(context=context)
+
+        if context.get_args()[0][SCOPE_RESPONSE_STARTED]:
+            print("\nResponse Started")
+            return
+        await route_operation.handle_response(context, res)
---
+++ packages/ellar/ellar-0.4.5.tar.gz/ellar-0.4.5/tests/test_response/test_defined_response_model.py
@@ -0,0 +1,84 @@
+from typing import List, Optional
+
+from ellar.common import ModuleRouter
+from ellar.core import AppFactory
+from pydantic import BaseModel
+
+mr = ModuleRouter("")
+
+
+class Item(BaseModel):
+    name: str
+    price: Optional[float] = None
+    owner_ids: Optional[List[int]] = None
+
+
+@mr.get("/items/valid", response=Item)
+def get_valid():
+    return {"name": "valid", "price": 1.0}
+
+
+@mr.get("/items/coerce", response=Item)
+def get_coerce():
+    return {"name": "coerce", "price": "1.0"}
+
+
+@mr.get("/items/validlist", response=List[Item])
+def get_validlist():
+    return [
+        {"name": "foo"},
+        {"name": "bar", "price": 1.0},
+        {"name": "baz", "price": 2.0, "owner_ids": [1, 2, 3]},
+    ]
+
+
+@mr.get("/items/valid_tuple_return", response={200: List[Item], 201: Item})
+def get_valid_tuple_return():
+    return 201, {"name": "baz", "price": 2.0, "owner_ids": [1, 2, 3]}
+
+
+@mr.get("/items/not_found_res_model", response={200: List[Item], 201: Item})
+def get_not_found_res_model():
+    return 301, {"name": "baz", "price": 2.0, "owner_ids": [1, 2, 3]}
+
+
+app = AppFactory.create_app(routers=(mr,))
+
+
+def test_valid_tuple_return(test_client_factory):
+    client = test_client_factory(app)
+    response = client.get("/items/valid_tuple_return")
+    response.raise_for_status()
+    assert response.json() == {"name": "baz", "price": 2.0, "owner_ids": [1, 2, 3]}
+
+
+def test_get_not_found_res_model_uses_empty_model(test_client_factory):
+    client = test_client_factory(app)
+    res = client.get("/items/not_found_res_model")
+    assert res.status_code == 301
+    assert res.json() == {"name": "baz", "price": 2.0, "owner_ids": [1, 2, 3]}
+
+
+def test_valid(test_client_factory):
+    client = test_client_factory(app)
+    response = client.get("/items/valid")
+    response.raise_for_status()
+    assert response.json() == {"name": "valid", "price": 1.0, "owner_ids": None}
+
+
+def test_coerce(test_client_factory):
+    client = test_client_factory(app)
+    response = client.get("/items/coerce")
+    response.raise_for_status()
+    assert response.json() == {"name": "coerce", "price": 1.0, "owner_ids": None}
+
+
+def test_validlist(test_client_factory):
+    client = test_client_factory(app)
+    response = client.get("/items/validlist")
+    response.raise_for_status()
+    assert response.json() == [
+        {"name": "foo", "price": None, "owner_ids": None},
+        {"name": "bar", "price": 1.0, "owner_ids": None},
+        {"name": "baz", "price": 2.0, "owner_ids": [1, 2, 3]},
+    ]
