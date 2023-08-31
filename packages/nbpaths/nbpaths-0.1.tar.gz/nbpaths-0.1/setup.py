07880a1f03f1ac84b0434a7f006935a70ba48c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:08:47 +0000

    Add package nbpaths/0.1/nbpaths-0.1.tar.gz

---
+++ packages/nbpaths/nbpaths-0.1.tar.gz/nbpaths-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='nbpaths',
+version='0.1',
+description='Testing installation of Package',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['nbpaths'],
+zip_safe=False)
