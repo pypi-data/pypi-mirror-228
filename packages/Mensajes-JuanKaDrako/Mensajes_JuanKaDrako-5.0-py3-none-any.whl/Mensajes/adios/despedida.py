fe0077b951d17bfd2eb1c2e1710528be8c095a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:08:43 +0000

    Add package Mensajes-JuanKaDrako/5.0/Mensajes_JuanKaDrako-5.0-py3-none-any.whl

---
+++ packages/Mensajes-JuanKaDrako/Mensajes_JuanKaDrako-5.0-py3-none-any.whl/Mensajes/adios/despedida.py
@@ -0,0 +1,10 @@
+def despedir():
+    print("Adios, te damos la despedida desde despedidas.despedir()")
+
+class Despedida:
+   def __init__(self):
+       print("Adios, me despido desde Despedida.__init__")
+
+
+if __name__ == '__main__':
+    despedir()
---
+++ packages/Mensajes-JuanKaDrako/Mensajes_JuanKaDrako-5.0-py3-none-any.whl/tests/test_adios.py
@@ -0,0 +1,8 @@
+from Mensajes.hola.saludos import *
+from Mensajes.adios.despedida import *
+
+saludar()
+Saludo()
+
+despedir()
+Despedida()
