04433b612e1dbd7c9306240dd9dfa760bba4ea68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 16:13:16 +0000

    Add package test-asic-package/1.0.1/test_asic_package-1.0.1.tar.gz

---
+++ packages/test-asic-package/test_asic_package-1.0.1.tar.gz/test_asic_package-1.0.1/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='test_asic_package',
+    version='1.0.1',
+    author='自动化测试基础包',
+    author_email='tianjincn@163.com',
+    packages=find_packages(),
+    description='python 自动化测试基础包'
+)
