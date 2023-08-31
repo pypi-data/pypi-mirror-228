59791086e3291358e74908ae9cdab3bd2f930044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:06:59 +0000

    Add package nbpaths/0.13/nbpaths-0.13.tar.gz

---
+++ packages/nbpaths/nbpaths-0.13.tar.gz/nbpaths-0.13/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='nbpaths',
+version='0.13',
+description='Import package to add parent folders to path for Jupyter notebooks. Will not go higher up tree than .gitignore if found.',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['nbpaths'],
+zip_safe=False)
