c7224abab15706ef71bb63973bb2888d5eeccf66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:08:23 +0000

    Add package example-os-linzy/0.0.3/example_os_linzy-0.0.3-py3-none-any.whl

---
+++ packages/example-os-linzy/example_os_linzy-0.0.3-py3-none-any.whl/example_os_linzy/example.py
@@ -0,0 +1,14 @@
+ID = 'AKID2uXu20mJdkfMq9E7mBNJ0RdaHjvsa456'
+_KEY = 'agfahfjacafghjkaaaaaaaaaa'
+
+def add_one(number):
+    return number + 1
+
+def add_two(number):
+    return number - 1
+
+def get_id():
+    return ID
+
+def get_key():
+    return _KEY
