489a21803597e1f8bda79c9ed6dc6187254ef4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 16:12:25 +0000

    Add package glr-utilities/0.1/glr_utilities-0.1.tar.gz

---
+++ packages/glr-utilities/glr_utilities-0.1.tar.gz/glr_utilities-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glr_utilities',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your utility functions might have
+    ],
+)
