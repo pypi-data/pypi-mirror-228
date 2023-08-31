f5cfb78a6f8c4d93c39ea558052b7c66ab869721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:28 +0000

    Add package EcodeX/0.0.0.2/EcodeX-0.0.0.2-py3-none-any.whl

---
+++ packages/EcodeX/EcodeX-0.0.0.2-py3-none-any.whl/EcodeX/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.0.1"
+
+class Install():
+    def pack(name, #软件名
+            version, #软件版本
+            install_requires, #所需的Python库及版本
+            author, #软件作者
+            author_email, #软件作者邮箱
+            description #软件介绍
+            ):
+        pass
