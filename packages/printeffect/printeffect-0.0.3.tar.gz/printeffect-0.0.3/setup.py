63d08604f4a316c487f5a0655fe99b7ef49a291f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:38 +0000

    Add package printeffect/0.0.3/printeffect-0.0.3.tar.gz

---
+++ packages/printeffect/printeffect-0.0.3.tar.gz/printeffect-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A print effect'
+
+# Setting up
+setup(
+    name="printeffect",
+    version=VERSION,
+    author="NoticedCloud",
+    author_email="noticedcloud@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
