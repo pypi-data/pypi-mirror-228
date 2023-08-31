884935886f84cfe68187b51b7943b992c6fcdcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:15:03 +0000

    Add package constellationpy/0.2.1/constellationpy-0.2.1-py3-none-any.whl

---
+++ packages/constellationpy/constellationpy-0.2.1-py3-none-any.whl/constellationpy/const.py
@@ -0,0 +1,12 @@
+# La version minimale de @constl/serveur (paquet npm) compatible avec le client Python
+V_SERVEUR_NÉCESSAIRE = "^0.3.2"
+
+# La commande pour lancer Constellation de la ligne de commande
+EXE_CONSTL = "constl"
+
+# Les paquets npm Constellation
+PAQUET_SERVEUR = "@constl/serveur"
+PAQUET_IPA = "@constl/ipa"
+
+# Le lien pour signaler des problèmes avec le logiciel Constellation
+LIEN_SIGNALEMENT_ERREURS = "https://github.com/reseau-constellation/serveur-ws/issues"
