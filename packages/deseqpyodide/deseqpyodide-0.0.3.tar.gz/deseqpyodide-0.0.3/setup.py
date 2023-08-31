d052e5b403d021547461602bec49fa36188f81e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:14:38 +0000

    Add package deseqpyodide/0.0.3/deseqpyodide-0.0.3.tar.gz

---
+++ packages/deseqpyodide/deseqpyodide-0.0.3.tar.gz/deseqpyodide-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deseqpyodide',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        "anndata>=0.8.0",
+        "numpy>=1.23.0",
+        "pandas>=1.4.0",
+        "scikit-learn>=1.1.0",
+        "scipy>=1.8.0",
+        "statsmodels",
+        "matplotlib>=3.6.2",
+    ]
+)
