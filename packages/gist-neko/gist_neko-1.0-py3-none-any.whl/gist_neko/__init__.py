82297b891aef69b674402895d7484a90904bcb78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:34 +0000

    Add package gist-neko/1.0/gist_neko-1.0-py3-none-any.whl

---
+++ packages/gist-neko/gist_neko-1.0-py3-none-any.whl/gist_neko/__init__.py
@@ -0,0 +1,28 @@
+from .download import download_gists
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description="Download specified user's all gists at once")
+    parser.add_argument("-u", "--username", type=str, metavar="Username", help="Github username")
+    parser.add_argument("-t", "--token", type=str, metavar="Token", help="Github public access token")
+    parser.add_argument("-g", "--git", type=bool, help="Whether to download with git or not. False by default since it's dependent on whether or not git is downloaded (and your ssh/gpg key). IF YOU TYPE ANYTHING IN AFTER -g/--git IT WILL BE ACCEPTED AS TRUE.")
+    
+    args = parser.parse_args()
+    
+    if args.username:
+        username = args.username
+    else:
+        username = os.getenv("GITHUB_USERNAME")
+    
+    if args.token:
+        token = args.token
+    else:
+        token = os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
+    
+    git_check = args.git # false by default/if you don't use the argument
+    
+    download_gists(username, token, git_check)
+    
+if __name__ == "__main__":
+    main()
