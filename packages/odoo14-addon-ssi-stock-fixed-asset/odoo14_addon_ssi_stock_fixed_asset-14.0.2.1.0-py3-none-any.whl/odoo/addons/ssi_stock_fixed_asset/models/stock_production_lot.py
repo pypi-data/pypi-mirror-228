05f3b64e86ca5bbe83d9e269f36eb1dcfd915f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:09:06 +0000

    Add package odoo14-addon-ssi-stock-fixed-asset/14.0.2.1.0/odoo14_addon_ssi_stock_fixed_asset-14.0.2.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-stock-fixed-asset/odoo14_addon_ssi_stock_fixed_asset-14.0.2.1.0-py3-none-any.whl/odoo/addons/ssi_stock_fixed_asset/models/stock_production_lot.py
@@ -0,0 +1,15 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockProductionLot(models.Model):
+    _name = "stock.production.lot"
+    _inherit = ["stock.production.lot"]
+
+    fixed_asset_id = fields.Many2one(
+        string="Fixed Assets",
+        comodel_name="fixed.asset.asset",
+    )
