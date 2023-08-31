8f6e9396606520e672af0246656617eaf2ae0797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:12:50 +0000

    Add package py-hopscotch-dict/2.1.2/py_hopscotch_dict-2.1.2-py3-none-any.whl

---
+++ packages/py-hopscotch-dict/py_hopscotch_dict-2.1.2-py3-none-any.whl/py_hopscotch_dict/__init__.py
@@ -0,0 +1,8 @@
+################################################################################
+#                              py-hopscotch-dict                               #
+#    Full-featured `dict` replacement with guaranteed constant-time lookups    #
+#                 (C) 2017, 2019-2020, 2022-2023  Jeremy Brown                 #
+#                Released under Prosperity Public License 3.0.0                #
+################################################################################
+
+from py_hopscotch_dict.hopscotchdict import HopscotchDict as HopscotchDict  # noqa
