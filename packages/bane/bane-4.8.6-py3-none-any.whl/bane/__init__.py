8df1081b57c5639ef101d68c8e0b4a5257c2b2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:12:43 +0000

    Add package bane/4.8.6/bane-4.8.6-py3-none-any.whl

---
+++ packages/bane/bane-4.8.6-py3-none-any.whl/bane/__init__.py
@@ -0,0 +1,29 @@
+#coding: utf-8
+'''
+
+email:
+
+      ala.bouali.1997@gmail.com
+
+facebook:
+
+      https://www.facebook.com/ala.bouali.99
+
+linkedin:
+
+      https://tn.linkedin.com/in/ala-bouali
+'''
+from bane.payloads import *
+from bane.ddos import *
+from bane.hasher import *
+from bane.pager import *
+from bane.info_s import *
+from bane.extrafun import *
+from bane.bruteforcer import *
+from bane.iot import *
+from bane.vulns import *
+from bane.wp import *
+from bane.proxer import *
+from bane.amplifier import *
+from bane.updating import *
+from bane.swtch import *
