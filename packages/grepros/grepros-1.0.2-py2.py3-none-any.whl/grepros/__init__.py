dc05557cb6d6d5c981a21745c294e12f85f23bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:13:59 +0000

    Add package grepros/1.0.2/grepros-1.0.2-py2.py3-none-any.whl

---
+++ packages/grepros/grepros-1.0.2-py2.py3-none-any.whl/grepros/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+------------------------------------------------------------------------------
+This file is part of grepros - grep for ROS bag files and live topics.
+Released under the BSD License.
+
+@author      Erki Suurjaak
+@created     31.10.2021
+@modified    30.08.2023
+------------------------------------------------------------------------------
+"""
+## @namespace grepros
+__title__        = "grepros"
+__version__      = "1.0.2"
+__version_info__ = (1, 0, 2)
+__version_date__ = "30.08.2023"
+
+from . library import *
