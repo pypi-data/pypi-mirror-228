90db9a7d99acbdc392c1c3edf6f3ec5e065ff552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:14:44 +0000

    Add package invenio-users-resources/2.6.0/invenio-users-resources-2.6.0.tar.gz

---
+++ packages/invenio-users-resources/invenio-users-resources-2.6.0.tar.gz/invenio-users-resources-2.6.0/invenio_users_resources/resources/users/config.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 TU Wien.
+# Copyright (C) 2022 CERN.
+#
+# Invenio-Users-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Users resource config."""
+
+import marshmallow as ma
+from invenio_records_resources.resources import (
+    RecordResourceConfig,
+    SearchRequestArgsSchema,
+)
+
+
+#
+# Resource config
+#
+class UsersResourceConfig(RecordResourceConfig):
+    """Users resource configuration."""
+
+    blueprint_name = "users"
+    url_prefix = "/users"
+    routes = {
+        "list": "",
+        "item": "/<id>",
+        "item-avatar": "/<id>/avatar.svg",
+        "approve": "/<id>/approve",
+        "block": "/<id>/block",
+        "restore": "/<id>/restore",
+        "deactivate": "/<id>/deactivate",
+    }
+
+    request_view_args = {
+        "id": ma.fields.Str(),
+    }
+
+    request_search_args = SearchRequestArgsSchema
