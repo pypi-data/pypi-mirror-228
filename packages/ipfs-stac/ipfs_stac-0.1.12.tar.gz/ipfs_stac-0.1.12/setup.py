dd6a72cf8c4d9556fed8d8791d7aa19691221e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:09:04 +0000

    Add package ipfs-stac/0.1.12/ipfs_stac-0.1.12.tar.gz

---
+++ packages/ipfs-stac/ipfs_stac-0.1.12.tar.gz/ipfs_stac-0.1.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ipfs_stac',
+    packages=find_packages(include=['ipfs_stac']),
+    version='0.1.12',
+    description='The EASIER Data Initiative Python IPFS-STAC Client',
+    author='The EASIER Data Initiative',
+    license='MIT',
+    install_requires=['ipfsspec', 'fsspec', 'requests', 'pandas', 'beautifulsoup4', 'pystac-client', 'Pillow', 'numpy', 'rasterio'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    project_urls={
+        "Twitter": "https://twitter.com/easierdataorg",
+        "GitHub": "https://github.com/easierdata/ipfs-stac"
+    }
+)
