1c94f66724c117e98c13bba056963c4f4e92316d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:07:46 +0000

    Add package mt5connect/0.1.2/mt5connect-0.1.2.tar.gz

---
+++ packages/mt5connect/mt5connect-0.1.2.tar.gz/mt5connect-0.1.2/mt5connect/tg_send.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def tg_send(message, conf):
+    print(conf)
+    TOKEN = conf["telegram_token"]
+    CHAT_ID = conf["telegram_chat_ID"]
+    apiURL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    try:
+        response = requests.post(apiURL, json={"chat_id": CHAT_ID, "text": message})
+    except Exception as e:
+        print(e)
