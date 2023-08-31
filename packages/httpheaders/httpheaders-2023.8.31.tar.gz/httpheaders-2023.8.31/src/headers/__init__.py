d7894484b2b85034e138888e1ae605a65ddfdd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:40 +0000

    Add package httpheaders/2023.8.31/httpheaders-2023.8.31.tar.gz

---
+++ packages/httpheaders/httpheaders-2023.8.31.tar.gz/httpheaders-2023.8.31/src/headers/__init__.py
@@ -0,0 +1,8 @@
+"""HTTP Headers
+
+see: https://www.iana.org/assignments/http-fields/http-fields.xhtml
+generated at: 2023-08-31T02:14:19.074022
+"""
+from .string import *
+
+__version__ = "2023.08.31"
