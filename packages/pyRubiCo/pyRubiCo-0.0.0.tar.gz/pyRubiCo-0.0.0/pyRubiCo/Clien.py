3b10607d61c7473865298d42b6acec290b64304e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:12:41 +0000

    Add package pyRubiCo/0.0.0/pyRubiCo-0.0.0.tar.gz

---
+++ packages/pyRubiCo/pyRubiCo-0.0.0.tar.gz/pyRubiCo-0.0.0/pyRubiCo/Clien.py
@@ -0,0 +1,17 @@
+class clien:
+    web = {
+        "app_name": "Main",
+        "app_version": "4.4.3",
+        "platform": "Web",
+        "package": "web.rubika.ir",
+        "lang_code": "fa"
+    }
+
+    android = {
+        "app_name": "Main",
+        "app_version": "3.4.2",
+        "platform": "Android",
+        "package": "app.rbmain.a",
+        "lang_code": "fa"
+    }
+		
