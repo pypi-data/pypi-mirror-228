669bfe72137734a3b70b91c9b5be3b7252716a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:07:52 +0000

    Add package gamma-config/0.7.7/gamma_config-0.7.7-py3-none-any.whl

---
+++ packages/gamma-config/gamma_config-0.7.7-py3-none-any.whl/gamma/config/builtin_tags.py
@@ -0,0 +1,359 @@
+"""Module that implements renderers for builtin-tags"""
+
+import importlib
+import os
+import threading
+
+from beartype.typing import Any, Union
+from ruamel.yaml import YAML
+from ruamel.yaml.nodes import MappingNode, Node, ScalarNode, SequenceNode
+
+from gamma.config import dispatch
+from gamma.config.confignode import ConfigNode
+from gamma.config.dump_dict import to_dict
+
+from .findconfig import get_config_root
+from .render import render_node
+from .render_context import get_render_context
+from .tags import Tag, TagException
+
+UNDEFINED = "~~UNDEFINED~~"
+yaml = YAML(typ="safe")
+
+
+RefTag = Tag["!ref"]
+EnvTag = Tag["!env"]
+EnvSecretTag = Tag["!env_secret"]
+ExprTag = Tag["!expr"]
+J2Tag = Tag["!j2"]
+J2SecretTag = Tag["!j2_secret"]
+PyTag = Tag["!py"]
+ObjTag = Tag["!obj"]
+PathTag = Tag["!path"]
+
+j2_cache = threading.local()
+
+
+@dispatch
+def render_node(node: Node, tag: EnvTag, **ctx):
+    """[!env] Maps the value to an environment variable of the same name.
+
+    You can provide a default using the ``|`` (pipe) character after the variable
+    name.
+
+    Examples:
+
+        my_var: !env MYVAR|my_default
+    """
+    name = node.value
+    name, default = _split_default(name)
+    env_val = os.getenv(name, default)
+    if env_val == UNDEFINED:
+        raise TagException(
+            f"Env variable '{name}' not found when resolving node and no default set"
+        )
+    elif env_val is not None:
+        env_val = yaml.load(env_val)
+
+    return env_val
+
+
+# process: !env_secret
+@dispatch
+def render_node(node: Node, tag: EnvSecretTag, *, dump=False, **ctx):
+    """[!env_secret] Similar to !env, but never returns the value when dumping."""
+    if dump:
+        return node
+    return render_node(node, EnvTag(), dump=dump, **ctx)
+
+
+# process: !expr
+@dispatch
+def render_node(node: Node, tag: ExprTag, **ctx):
+    """[!expr] Uses ``eval()`` to render arbitrary Python expressions.
+
+    By default, we add the root configuration as `c` variable.
+
+    See ``gamma.config.render_context.context_providers`` documentation to add your
+    own variables to the context.
+    """
+
+    _locals = {}
+    _globals = get_render_context(node=node, tag=tag, **ctx)
+    return eval(node.value, _globals, _locals)
+
+
+@dispatch
+def render_node(node: Node, tag: J2Tag, *, config=None, key=None, **ctx):
+    """[!j2] Treats the value a Jinj2 Template
+
+    See ``gamma.config.render_context.context_providers`` documentation to add your
+    own variables to the context.
+
+    In practice, in the snippet bellow, ``foo1`` and ``foo2`` are equivalent
+
+        myvar: 100
+        foo1: !expr f"This is a number = {c.myvar}"
+        foo2: !j2 This is a number = {c.myvar}
+
+    You can customize the Jinja2 environment by providing a reference to a Python
+    function in the `j2_env` key in `00-meta.yaml`. Example
+
+        j2_env: my_app.my_module:my_func
+
+    Notes:
+        * Jinja2 is not installed by default, you should install it manually.
+    """
+    try:
+        import jinja2
+        import jinja2.exceptions
+        from jinja2.runtime import StrictUndefined
+    except ModuleNotFoundError:  # pragma: no cover
+        raise Exception(
+            "Could not find Jinja2 installed. You must manually install it with "
+            "`pip install jinja2` if you want to use the !j2 tag"
+        )
+
+    render_ctx = get_render_context(node=node, tag=tag, config=config, key=key, **ctx)
+
+    if not hasattr(j2_cache, "env"):
+        root = config and config._root
+        env_factory = root.get("j2_env")
+        if env_factory:
+            module_name, func_name = env_factory.split(":", 1)
+            mod = importlib.import_module(module_name)
+            func = getattr(mod, func_name)
+            j2_cache.env = func()
+        else:
+            j2_cache.env = jinja2.Environment(undefined=StrictUndefined)
+
+    try:
+        res = j2_cache.env.from_string(node.value).render(**render_ctx)
+    except jinja2.exceptions.UndefinedError as ex:
+        msg = f'Error rendering key `{key.value}: "{node.value}"` -> {ex.message}'
+        raise ValueError(msg)
+
+    return res
+
+
+# process: !j2_secret
+@dispatch
+def render_node(node: Node, tag: J2SecretTag, *, dump=False, **ctx):
+    """[!j2_secret] Similar to !j2, but never returns the value when dumping."""
+
+    if dump:
+        return node
+    return render_node(node, J2Tag(), dump=dump, **ctx)
+
+
+@dispatch
+def render_node(node: Node, tag: RefTag, *, config=None, recursive=False, **ctx):
+    """[!ref] References other entries in the config object.
+
+    Navigate the object using the dot notation. Complex named keys can be accessed
+    using quotes.
+    """
+
+    import functools
+    import operator
+    import shlex
+
+    lex = shlex.shlex(instream=node.value, posix=True)
+    lex.whitespace = "."
+    tokens = []
+    token = lex.get_token()
+    while token:
+        tokens.append(token)
+        token = lex.get_token()
+
+    parent = functools.reduce(operator.getitem, tokens[:-1], config._root)
+    val = parent[tokens[-1]]
+
+    if recursive:
+        while isinstance(val, (ConfigNode, Node)):
+            # chain dump if needed
+            val = render_node(val, config=config, recursive=recursive, **ctx)
+
+    return val
+
+
+# process: !py
+@dispatch
+def render_node(node: ScalarNode, tag: PyTag, *, path=None, **ctx):
+    """[!py] Pass the node value to a Python callable.
+
+    This tag should be used as a URI-style tag on the form `!py:<module>:<callable>`
+
+    The scalar node value is first implicitly resolved to a Python object using
+    `yaml.load`. For instance:
+
+    ```yaml
+    foo: !py:myapp.mymodule:myfunc 100
+    bar: !py:myapp.mymodule:myfunc "100"
+    zig: !py:myapp.mymodule:myfunc a value
+    zag: !py:myapp.mymodule:myfunc
+    ```
+
+    Will call the function `myfunc` in `myapp.mymodule` module with the arguments:
+    - `type(value) == int; value == 100` for `foo`
+    - `type(value) == str; value == "100"` for `bar`
+    - `type(value) == str; value == "a value"` for `zig`
+    - no argument passed
+    """
+
+    func = _py_tag_get_func("py", path)
+    val = yaml.load(node.value)
+    if val:
+        return func(val)
+    else:
+        return func()
+
+
+# process: !py
+@dispatch
+def render_node(
+    node: Union[SequenceNode, MappingNode], tag: PyTag, *, path=None, **ctx
+) -> Any:
+    """[!py] Pass the node value to a Python callable.
+
+    This tag should be used as a URI-style tag on the form `!py:<module>:<callable>`
+
+    The `map` or `seq` node value is first converted to a Python `dict`/`list`
+    recursively using the `to_dict` method.
+    """
+
+    func = _py_tag_get_func("py", path)
+    val = to_dict(node, **ctx)
+    return func(val)
+
+
+def _py_tag_get_func(tag, path, default_module=None):
+    """Get the callable for a given tag `path` argument
+
+    Used by `!py`, `!obj` tags meta config.
+
+    Args:
+        tag: either "obj" or "py"
+        path: the tag "path" part in the format `!<tag>:<module>:<callable>`
+    """
+
+    import importlib
+
+    if not path:
+        raise ValueError(f"The !{tag} tag is missing the 'path' part after the ':'")
+
+    if ":" in path:
+        module_name, func_name = path.split(":", 1)
+    elif tag == "obj" and default_module:
+        module_name = default_module
+        func_name = path
+    else:
+        raise ValueError(f"The !{tag} tag has an invalid reference: {path}")
+
+    try:
+        module = importlib.import_module(module_name)
+    except ModuleNotFoundError as ex:
+        raise ValueError(f"Module part from '!{tag}:{path}' not found") from ex
+
+    func = getattr(module, func_name, None)
+    if not func:
+        raise ValueError(f"Name '{func_name}' not found in module {module}")
+
+    return func
+
+
+# process: !obj
+@dispatch
+def render_node(
+    node: MappingNode, tag: ObjTag, *, path=None, config=None, **ctx
+) -> Any:
+    """[!obj] Create a Python object by passing the mapping value as nested dicts to
+    the object constructor.
+
+    This tag should be used as a URI-style tag on the form `!obj:<module>:<callable>`
+    It behaves like `!py`, but only applies to `map` nodes, and
+    automatically unpack the mapping.
+
+    You may omit the `<module>` part of the path if you define an `obj_default_module`
+    scalar entry at the root of the config.
+
+    Examples:
+
+    ```yaml
+    foo: !obj:myapp.mymodule:MyClass
+        a: 100
+        b: a value
+
+    # the above `foo` is equivalent to `bar` below
+
+    obj_default_module: myapp.mymodule
+
+    bar: !obj:MyClass
+        a: 100
+        b: a value
+    ```
+    """
+
+    root = config and config._root
+    default_module = (root and root.get("obj_default_module")) or None
+    func = _py_tag_get_func("obj", path, default_module=default_module)
+    val = to_dict(node)
+    return func(**val)
+
+
+@dispatch
+def render_node(node: ScalarNode, tag: ObjTag, *, path=None, config=None, **ctx) -> Any:
+    """Handle scalar nodes by passing the value as the single argument.
+
+    Examples:
+    ```yaml
+    foo: !obj:myapp.mymodule:MyClass 100
+    bar: !obj:myapp.mymodule:MyClass
+    ```
+
+    `foo` will try to call `myapp.mymodule.MyClass(100)` and `bar` will try to call
+    `myapp.mymodule.MyClass(100)`
+    """
+
+    root = config and config._root
+    default_module = (root and root.get("obj_default_module")) or None
+    func = _py_tag_get_func("obj", path, default_module=default_module)
+    val = yaml.load(node.value)
+
+    if val is None:
+        return func()
+    return func(val)
+
+
+# process: !path
+@dispatch
+def render_node(node: Node, tag: PathTag, **ctx) -> str:
+    """[!path] Construct an absolute file path by joining a path
+    fragment to the known path of the *parent* of config root directory
+
+    Examples:
+        # should point to `<config-root>/../data/hello_world.csv`
+        my_var: !path data/hello_world.csv
+    """
+    path_fragment = node.value
+    base = get_config_root().parent
+    return str(base.joinpath(path_fragment).absolute())
+
+
+###
+# Utilities
+###
+
+
+def _split_default(value):
+    """Get a value optional default, using the content after the last "|"
+    (pipe) as convention.
+
+    The default will be parsed as a YAML value.
+    """
+
+    default = UNDEFINED
+    parsed = value
+    if "|" in value:
+        parsed, default = value.rsplit("|", 1)
+    return parsed, default
---
+++ packages/gamma-config/gamma_config-0.7.7-py3-none-any.whl/gamma/config/scaffold.py
@@ -0,0 +1,112 @@
+"""Module to 'scaffold' append bootstrap config for gamma-io"""
+import shutil
+from importlib.metadata import entry_points
+from pathlib import Path
+
+import colorama
+from beartype.typing import NamedTuple
+from colorama import Fore, Style
+
+from gamma.config import dispatch
+
+ENTRYPOINT_GROUP = "gamma.config.scaffold"
+
+
+class GammaConfigScaffold(NamedTuple):
+    name = "gamma-config"
+
+
+@dispatch
+def get_source(module: GammaConfigScaffold):
+    return Path(__file__).parent / "sample"
+
+
+@dispatch
+def get_files(module, src):
+    return sorted(src.glob("**/*"))
+
+
+def scaffold(target, force):
+    """Initialize the config folder with samples.
+
+    The set of files to be deployed on scaffolding can be extended via the Python
+    "entrypoint" plugin system.
+
+    First you need to add your the directives in `setup.cfg` pointing to the
+    `gamma.config.scaffold` group. Example:
+
+    ```ini
+    [options.entry_points]
+    gamma.config.scaffold =
+        gamma-io = gamma.io.scaffold:setup
+    ```
+
+    In the example above, `setup` function under `gamma.io.scaffold` module should
+    return an instance of any class that will be used for dispatching the `get_files`
+    and `get_source` functions.
+    """
+
+    colorama.init()  # for fellow windows users
+
+    if not target:
+        target = Path(".").absolute()
+
+    print(f"TARGET: {target}")
+    target = Path(target)
+    confdir: Path = target / "config"
+    confdir.mkdir(exist_ok=True)
+
+    # load plugins from ENTRYPOINT_GROUP
+    modules = [GammaConfigScaffold()]
+    for ep in entry_points().get(ENTRYPOINT_GROUP, []):
+        plugin = ep.load()
+        modules.append(plugin())
+
+    # find source under sample folder
+    for mod in modules:
+        src = get_source(mod)
+        for srcfile in get_files(mod, src):
+            # Check if target file already exists
+            relfile = srcfile.relative_to(src)
+            dstfile: Path = confdir / relfile
+            if srcfile.is_dir():
+                continue
+            elif not force and dstfile.exists():
+                print(
+                    Fore.YELLOW + f"[{mod.name}] File '{relfile}' already exists "
+                    "and --force was not set",
+                    Style.RESET_ALL,
+                )
+            else:
+                dstfile.parent.mkdir(parents=True, exist_ok=True)
+                shutil.copyfile(srcfile, dstfile)
+                _file = Fore.CYAN + str(relfile) + Style.RESET_ALL
+                print(
+                    f"[{mod.name}] copied file {_file} to",
+                    str(confdir.relative_to(target)),
+                )
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Initialize config folder with samples"
+    )
+    parser.add_argument(
+        "-t",
+        "--target",
+        help="The target folder. Default to current folder.",
+        default=None,
+    )
+    parser.add_argument(
+        "--force",
+        help="If set, do not check for existing files.",
+        action="store_true",
+        default=False,
+    )
+
+    args = parser.parse_args()
+    from gamma.config.scaffold import scaffold as _run
+
+    _run(args.target, args.force)
