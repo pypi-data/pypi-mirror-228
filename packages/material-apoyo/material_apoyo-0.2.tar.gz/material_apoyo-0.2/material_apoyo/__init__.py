01430b2bc59c850b4118540592e0a5387117dea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:38 +0000

    Add package material-apoyo/0.2/material_apoyo-0.2.tar.gz

---
+++ packages/material-apoyo/material_apoyo-0.2.tar.gz/material_apoyo-0.2/material_apoyo/__init__.py
@@ -0,0 +1,1 @@
+from material_apoyo.funciones import revisar_alarma
---
+++ packages/material-apoyo/material_apoyo-0.2.tar.gz/material_apoyo-0.2/material_apoyo/funciones.py
@@ -0,0 +1,7 @@
+import random
+# hola
+def revisar_alarma(hora, minuto):
+    seed = str(hora) + str(minuto)
+    random.seed(int(seed))
+
+    return random.choice([True, False, False, False])
