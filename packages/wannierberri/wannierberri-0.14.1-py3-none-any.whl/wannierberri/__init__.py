217718d24b2da961834161dcac073db63c35a888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:08:20 +0000

    Add package wannierberri/0.14.1/wannierberri-0.14.1-py3-none-any.whl

---
+++ packages/wannierberri/wannierberri-0.14.1-py3-none-any.whl/wannierberri/__init__.py
@@ -0,0 +1,57 @@
+#                                                            #
+# This file is distributed as part of the WannierBerri code  #
+# under the terms of the GNU General Public License. See the #
+# file `LICENSE' in the root directory of the WannierBerri   #
+# distribution, or http://www.gnu.org/copyleft/gpl.txt       #
+#                                                            #
+# The WannierBerri code is hosted on GitHub:                 #
+# https://github.com/stepan-tsirkin/wannier-berri            #
+#                     written by                             #
+#           Stepan Tsirkin, University of Zurich             #
+#                                                            #
+#------------------------------------------------------------
+"""
+wannierberri - a module for Wannier interpolation
+"""
+
+__version__ = "0.14.1"
+
+from .run import run
+from .__old_API.__main import integrate, tabulate #, integrate_options, tabulate_options, print_options
+from . import symmetry
+from . import system
+from .system import System_w90, System_fplo, System_tb, System_PythTB, System_TBmodels, System_ASE, System_Phonon_QE
+from .grid import Grid, Path
+from . import calculators
+from . import result
+from .parallel import Parallel, Serial
+from .smoother import get_smoother
+
+from termcolor import cprint
+
+
+def figlet(text, font='cosmike', col='red'):
+    init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
+    letters = [figlet_format(X, font=font).rstrip("\n").split("\n") for X in text]
+    logo = []
+    for i in range(len(letters[0])):
+        logo.append("".join(L[i] for L in letters))
+    cprint("\n".join(logo), col, attrs=['bold'])
+
+def welcome():
+    # ogiginally obtained by
+    # figlet("WANN IER BERRI",font='cosmic',col='yellow')
+    # with small modifications
+    logo = """
+.::    .   .::: .:::::::.  :::.    :::.:::.    :::. :::.,::::::  :::::::..       :::::::.  .,::::::  :::::::..   :::::::..   :::
+';;,  ;;  ;;;' '  ;;`;;  ` `;;;;,  `;;;`;;;;,  `;;; ;;;;;;;''''  ;;;;``;;;;       ;;;'';;' ;;;;''''  ;;;;``;;;;  ;;;;``;;;;  ;;;
+ '[[, [[, [['    ,[[ '[[,    [[[[[. '[[  [[[[[. '[[ [[[ [[cccc    [[[,/[[['       [[[__[[\\. [[cccc    [[[,/[[['   [[[,/[[['  [[[
+   Y$c$$$c$P    c$$$cc$$$c   $$$ "Y$c$$  $$$ "Y$c$$ $$$ $$\"\"\"\"    $$$$$$c         $$\"\"\"\"Y$$ $$\"\"\"\"    $$$$$$c     $$$$$$c    $$$
+    "88"888      888   888,  888    Y88  888    Y88 888 888oo,__  888b "88bo,    _88o,,od8P 888oo,__  888b "88bo, 888b "88bo,888
+     "M "M"      YMM   ""`   MMM     YM  MMM     YM MMM \"\"\"\"YUMMM MMMM   "W"     ""YUMMMP"  \"\"\"\"YUMMM MMMM   "W"  MMMM   "W" MMM
+"""
+    cprint(logo, 'yellow')
+
+    cprint("""\n  The Web page is :  HTTP://WANNIER-BERRI.ORG  \n""", 'yellow')
+
+    cprint("\nVersion: {}\n".format(__version__), 'cyan', attrs=['bold'])
---
+++ packages/wannierberri/wannierberri-0.14.1-py3-none-any.whl/wannierberri/calculators/tabulate.py
@@ -0,0 +1,113 @@
+import numpy as np
+from . import Calculator
+from ..formula import covariant as frml
+from ..formula import covariant_basic as frml_basic
+from ..result import KBandResult
+
+
+# The base class for Tabulating
+# particular calculators are below
+
+
+class Tabulator(Calculator):
+
+    def __init__(self, Formula, kwargs_formula={}, **kwargs):
+        self.Formula = Formula
+        self.ibands = None
+        self.kwargs_formula = kwargs_formula
+        super().__init__(**kwargs)
+
+    def __call__(self, data_K):
+        formula = self.Formula(data_K, **self.kwargs_formula)
+        nk = data_K.nk
+        NB = data_K.num_wann
+        ibands = self.ibands
+        if ibands is None:
+            ibands = np.arange(NB)
+        band_groups = data_K.get_bands_in_range_groups(
+            -np.Inf, np.Inf, degen_thresh=self.degen_thresh, degen_Kramers=self.degen_Kramers, sea=False)
+        # bands_groups  is a digtionary (ib1,ib2):E
+        # now select only the needed groups
+        band_groups = [
+            [n for n in groups.keys() if np.any((ibands >= n[0]) * (ibands < n[1]))] for groups in band_groups
+        ]  # select only the needed groups
+        group = [[] for ik in range(nk)]
+        for ik in range(nk):
+            for ib in ibands:
+                for n in band_groups[ik]:
+                    if ib < n[1] and ib >= n[0]:
+                        group[ik].append(n)
+                        break
+
+        rslt = np.zeros((nk, len(ibands)) + (3, ) * formula.ndim)
+        for ik in range(nk):
+            values = {}
+            for n in band_groups[ik]:
+                inn = np.arange(n[0], n[1])
+                out = np.concatenate((np.arange(0, n[0]), np.arange(n[1], NB)))
+                values[n] = formula.trace(ik, inn, out) / (n[1] - n[0])
+            for ib, b in enumerate(ibands):
+                rslt[ik, ib] = values[group[ik][ib]]
+        return KBandResult(rslt, transformTR=formula.transformTR, transformInv=formula.transformInv)
+
+
+###############################################
+###############################################
+###############################################
+###############################################
+####                                     ######
+####        Implemented calculators      ######
+####                                     ######
+###############################################
+###############################################
+###############################################
+###############################################
+
+
+
+
+
+class Energy(Tabulator):
+
+    def __init__(self, **kwargs):
+        super().__init__(frml.Eavln, **kwargs)
+
+class Velocity(Tabulator):
+
+    def __init__(self, **kwargs):
+        super().__init__(frml.Velocity, **kwargs)
+
+
+class BerryCurvature(Tabulator):
+
+    def __init__(self, **kwargs):
+        super().__init__(frml.Omega, **kwargs)
+
+
+class Spin(Tabulator):
+    r" Spin expectation :math:` \langle u | \mathbf{\sigma} | u \rangle`"
+    def __init__(self, **kwargs):
+        super().__init__(frml.Spin, **kwargs)
+
+class DerBerryCurvature(Tabulator):
+    r"Derivative of Berry curvature :math:`X_{ab}\partial_b\Omega_a`"
+    def __init__(self, **kwargs):
+        super().__init__(frml.DerOmega, **kwargs)
+
+class OrbitalMoment(Tabulator):
+
+    def __init__(self, **kwargs):
+        super().__init__(frml.morb, **kwargs)
+
+
+class DerOrbitalMoment(Tabulator):
+
+    def __init__(self, **kwargs):
+        super().__init__(frml_basic.Der_morb, **kwargs)
+
+
+class SpinBerry(Tabulator):
+
+    def __init__(self, **kwargs):
+        super().__init__(frml.SpinOmega, **kwargs)
+
