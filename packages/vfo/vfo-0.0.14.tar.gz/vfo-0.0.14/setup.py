df9eb27e3669bceb5b5973f90905589d62ab7492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:09:06 +0000

    Add package vfo/0.0.14/vfo-0.0.14.tar.gz

---
+++ packages/vfo/vfo-0.0.14.tar.gz/vfo-0.0.14/setup.py
@@ -0,0 +1,31 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vfo", # Replace with your own username
+    version="0.0.14",
+    author="anurag upadhyay",
+    author_email="iitg.anurag@gmail.com",
+    description="A package for visualization of OpenSees models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/u-anurag/vfo",
+    packages=setuptools.find_packages(),
+	install_requires=[
+            'numpy',
+            'matplotlib',
+			'pyvista',
+			'ipywidgets',
+			'trame',
+			'trame-vtk', 'trame-vuetify', 'trame-plotly'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
