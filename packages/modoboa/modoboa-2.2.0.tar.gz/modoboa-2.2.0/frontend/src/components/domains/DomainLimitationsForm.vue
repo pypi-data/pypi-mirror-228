8609522395a7079f44000271eb5dd9da47d7440c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:13:06 +0000

    Add package modoboa/2.2.0/modoboa-2.2.0.tar.gz

---
+++ packages/modoboa/modoboa-2.2.0.tar.gz/modoboa-2.2.0/frontend/src/components/domains/DomainLimitationsForm.vue
@@ -0,0 +1,77 @@
+<template>
+<validation-observer ref="observer">
+  <validation-provider
+    v-slot="{ errors }"
+    rules="required"
+    >
+    <v-text-field
+      :label="'Quota' | translate"
+      :hint="'Quota shared between mailboxes. Can be expressed in KB, MB (default) or GB. A value of 0 means no quota.' | translate"
+      persistent-hint
+      v-model="form.quota"
+      :error-messages="errors"
+      class="mb-4"
+      outlined
+      @input="update"
+      />
+  </validation-provider>
+  <validation-provider
+    v-slot="{ errors }"
+    rules="required"
+    >
+    <v-text-field
+      :label="'Default mailbox quota' | translate"
+      :hint="'Default quota applied to mailboxes. Can be expressed in KB, MB (default) or GB. A value of 0 means no quota.' | translate"
+      persistent-hint
+      v-model="form.default_mailbox_quota"
+      :error-messages="errors"
+      class="mb-4"
+      outlined
+      @input="update"
+      />
+  </validation-provider>
+  <validation-provider
+    v-slot="{ errors }"
+    rules="numeric"
+    >
+    <v-text-field
+      :label="'Message sending limit' | translate"
+      :hint="'Number of messages this domain can send per day. Leave empty for no limit.' | translate"
+      persistent-hint
+      v-model="form.message_sending_limit"
+      :error-messages="errors"
+      outlined
+      @input="update"
+      />
+  </validation-provider>
+</validation-observer>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Object
+  },
+  data () {
+    return {
+      form: {}
+    }
+  },
+  methods: {
+    reset () {
+      this.form = {}
+    },
+    update () {
+      this.$emit('input', this.form)
+    }
+  },
+  watch: {
+    value: {
+      handler: function (newValue) {
+        this.form = { ...this.value }
+      },
+      immediate: true
+    }
+  }
+}
+</script>
