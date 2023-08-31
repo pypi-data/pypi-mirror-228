8ddb43845f3edc4c6ae4f5a2a2a8860c1d921900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:14:39 +0000

    Add package wmill-pg/1.163.0/wmill_pg-1.163.0.tar.gz

---
+++ packages/wmill-pg/wmill_pg-1.163.0.tar.gz/wmill_pg-1.163.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wmill_pg']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['psycopg2-binary', 'wmill>=1.5.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'wmill-pg',
+    'version': '1.163.0',
+    'description': 'An extension client for the wmill client library focused on pg',
+    'long_description': '# wmill\n\nThe postgres extension client for the [Windmill](https://windmill.dev) platform.\n\n[windmill-api](https://pypi.org/project/windmill-api/).\n\n## Quickstart\n\n```python\nimport wmill_pg\n\n\ndef main():\n    my_list = query("UPDATE demo SET value = \'value\' RETURNING key, value")\n    for key, value in my_list:\n        ...\n```\n',
+    'author': 'Ruben Fiszel',
+    'author_email': 'ruben@windmill.dev',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://windmill.dev',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
