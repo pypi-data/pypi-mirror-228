8d4a9f0ad199dd31b802c935c6f633a9c6cad20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:55 +0000

    Add package fedrq/0.11.0/fedrq-0.11.0-py3-none-any.whl

---
+++ packages/fedrq/fedrq-0.11.0-py3-none-any.whl/fedrq/repoquery.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: 2022 Maxwell G <gotmax@e.email>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+"""
+Provides access to the default package backend interface.
+It is recommended to use the backend directly instead of this module.
+"""
+
+from __future__ import annotations
+
+from collections.abc import Callable
+
+from fedrq.backends import get_default_backend
+from fedrq.backends.base import (
+    BackendMod,
+    BaseMakerBase,
+    PackageCompat,
+    PackageQueryCompat,
+    RepoqueryBase,
+)
+
+backend: BackendMod = get_default_backend()
+BaseMaker: type[BaseMakerBase] = backend.BaseMaker
+Package: type[PackageCompat] = backend.Package
+PackageQuery: type[PackageQueryCompat] = backend.PackageQuery
+RepoError: type[BaseException] = backend.RepoError
+Repoquery: type[RepoqueryBase] = backend.Repoquery
+get_releasever: Callable[[], str] = backend.get_releasever
+BACKEND: str = backend.BACKEND
+
+__all__ = (
+    "BACKEND",
+    "BaseMaker",
+    "Repoquery",
+    "backend",
+    "get_releasever",
+)
