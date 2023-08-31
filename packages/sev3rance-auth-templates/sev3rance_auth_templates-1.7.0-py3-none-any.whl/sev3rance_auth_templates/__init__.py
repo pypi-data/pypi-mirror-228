1808a2d58b03ab70ff40749486f5323ecb0a1bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:08:27 +0000

    Add package sev3rance-auth-templates/1.7.0/sev3rance_auth_templates-1.7.0-py3-none-any.whl

---
+++ packages/sev3rance-auth-templates/sev3rance_auth_templates-1.7.0-py3-none-any.whl/sev3rance_auth_templates/__init__.py
@@ -0,0 +1,11 @@
+"""
+App init
+"""
+
+# Standard Library
+from importlib import metadata
+
+__version__ = metadata.version("sev3rance-auth-templates")
+__title__ = "Sev3rance Alliance Auth Template Overrides"
+
+del metadata
