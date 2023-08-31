e205a9edfab88d1f6309bd0c693026d3029bbd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:29 +0000

    Add package autodropstack/1.0.0/autodropstack-1.0.0.tar.gz

---
+++ packages/autodropstack/autodropstack-1.0.0.tar.gz/autodropstack-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autodropstack",
+    version="1.0.0",
+    description="filo stack",
+    author="thearyadev",
+    packages=find_packages(),
+    package_data={"": ["*.txt", "*.md"]},
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+)
