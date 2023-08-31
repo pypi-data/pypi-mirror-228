19a9cdb0ec66c352ef2d6925f0ca61b83f62c2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:08:29 +0000

    Add package mlhybridx/1.0.0/mlhybridx-1.0.0.tar.gz

---
+++ packages/mlhybridx/mlhybridx-1.0.0.tar.gz/mlhybridx-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages 
+
+with open('README.md', 'r',encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'ML module'
+
+setup(
+    name="mlhybridx",
+    version=VERSION,
+    author="Deepak and Sudheer",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(), 
+    install_requires=['scikit-learn', 'pandas', 'numpy','seaborn'],
+    keywords=['train', 'split', 'df', 'predict'],
+    classifiers=[
+        "Development Status :: 1 - Planning", 
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ]
+)
+
+# ///////////////// TOKEN /////////////////////////////
+
+# "pypi-AgENdGVzdC5weXBpLm9yZwIkMDA3OWYzNDctNTVhNS00MWFjLTg4MjAtMjMyZjYyOGQ1ZTkzAAIRWzEsWyJtbGh5YnJpZHgiXV0AAixbMixbIjNjMmIxNWFlLWRhZmYtNGM3Ny05YTMyLWFhNDc2N2QxMWVmNiJdXQAABiAgaTP3DlblEPDgpKLErsaeCtxB5ZN1RCikKSjZ3VO-Iw"
