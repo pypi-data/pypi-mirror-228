b59cedc06040d469d945452880f248dfb60cd7bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:08:40 +0000

    Add package weboptout/0.3.1/weboptout-0.3.1-py3-none-any.whl

---
+++ packages/weboptout/weboptout-0.3.1-py3-none-any.whl/weboptout/__init__.py
@@ -0,0 +1,6 @@
+## Copyright © 2023, Alex J. Champandard.  Licensed under MIT; see LICENSE! ⚘
+
+__version__ = "0.3.1"
+
+from .types import rsv, Reservation, Status
+from .web import check_domain_reservation
