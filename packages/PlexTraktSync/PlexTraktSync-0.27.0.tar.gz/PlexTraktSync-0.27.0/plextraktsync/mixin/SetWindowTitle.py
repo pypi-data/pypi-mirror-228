f55ce9d8b47bef31bc807668e24a85eb998672c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:08:30 +0000

    Add package PlexTraktSync/0.27.0/PlexTraktSync-0.27.0.tar.gz

---
+++ packages/PlexTraktSync/PlexTraktSync-0.27.0.tar.gz/PlexTraktSync-0.27.0/plextraktsync/mixin/SetWindowTitle.py
@@ -0,0 +1,12 @@
+from functools import cached_property
+
+
+class SetWindowTitle:
+    @cached_property
+    def console(self):
+        from plextraktsync.factory import factory
+
+        return factory.console
+
+    def set_window_title(self, title: str):
+        self.console.set_window_title(f"PlexTraktSync: {title}")
