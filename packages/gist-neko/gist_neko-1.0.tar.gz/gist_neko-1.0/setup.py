c68e99f9bff40eb3be52b702c6a9707afd0b082a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:34 +0000

    Add package gist-neko/1.0/gist_neko-1.0.tar.gz

---
+++ packages/gist-neko/gist_neko-1.0.tar.gz/gist_neko-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0"
+DESCRIPTION = "CLI for downloading all gists from a specified user."
+LONG_DESCRIPTION = "Download specified user's all gists at once. You can also use environment variables if you don't want to pass through arguments everytime you want to download a gist."
+AUTHOR = "NecRaul"
+
+setup(
+    name="gist_neko",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    packages=find_packages(),
+    install_requires=["requests"],
+    keywords=["python", "gists downloader", "downloader", "gists", "gist-neko", "kuroneko"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP"
+    ],
+    py_modules=["download"],
+    entry_points={
+        "console_scripts": [
+            "gist-neko = gist_neko.__init__:main",
+        ],
+    },
+)
