73069b40fc20abf4f3d1eac57c9b12b8de8091cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:07:16 +0000

    Add package easy-chromedriver-windows-install/0.0.2/easy_chromedriver_windows_install-0.0.2.tar.gz

---
+++ packages/easy-chromedriver-windows-install/easy_chromedriver_windows_install-0.0.2.tar.gz/easy_chromedriver_windows_install-0.0.2/README.md
@@ -0,0 +1,15 @@
+# Python Easy Chromedriver Windows Install
+
+This is a simple python package to install chromedriver on windows. Easy to use with selenium.
+
+Use this pip command to install:
+
+```pip install easy-chromedriver-windows-install```
+
+### Example
+
+```python
+from easy_chromedriver_windows_install import install_chromedriver
+
+install_chromedriver() # install chromedriver
+```
---
+++ packages/easy-chromedriver-windows-install/easy_chromedriver_windows_install-0.0.2.tar.gz/easy_chromedriver_windows_install-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(
+    name='easy_chromedriver_windows_install',
+    version='0.0.2',
+    license='MIT License',
+    author='Luan Grabher',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='luanggcontato@gmail.com',
+    keywords='chromedriver selenium webdriver',
+    description=u'Download chromedriver easily on Windows',
+    packages=['easy_chromedriver_windows_install'],
+    install_requires=['requests']
+)
