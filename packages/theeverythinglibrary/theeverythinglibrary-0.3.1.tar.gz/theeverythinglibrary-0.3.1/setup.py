26b30ebb086d86e691e81991c59fbc80569e0baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:08:17 +0000

    Add package theeverythinglibrary/0.3.1/theeverythinglibrary-0.3.1.tar.gz

---
+++ packages/theeverythinglibrary/theeverythinglibrary-0.3.1.tar.gz/theeverythinglibrary-0.3.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="theeverythinglibrary",
+    version="0.3.1",
+    description="Library with a module for almost everything",
+    url="https://github.com/RobertArnosson/TheEverythingLibrary",
+    author="Robert Arnorsson",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires= [
+        "cryptography",
+        "uuid",
+        "requests"
+    ],
+    python_requires=">=3.9",
+    project_urls={
+        "Bug Reports": "https://github.com/RobertArnosson/TheEverythingLibrary/issues",
+        "Source": "https://github.com/RobertArnosson/TheEverythingLibrary",
+    },
+)
