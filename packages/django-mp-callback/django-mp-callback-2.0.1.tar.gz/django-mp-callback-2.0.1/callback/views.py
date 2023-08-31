54792c1a47e560897fb40616f0fa1aa8cc39ea7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 16:13:01 +0000

    Add package django-mp-callback/2.0.1/django-mp-callback-2.0.1.tar.gz

---
+++ packages/django-mp-callback/django-mp-callback-2.0.1.tar.gz/django-mp-callback-2.0.1/callback/views.py
@@ -0,0 +1,61 @@
+from django.apps import apps
+from django.contrib.sites.models import Site
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.views.generic import FormView
+from django.utils.translation import gettext_lazy as _
+
+from callback.forms import CallbackForm
+from djmail import mail_managers
+
+
+class CreateCallbackView(FormView):
+
+    form_class = CallbackForm
+    template_name = 'callback/modal.html'
+
+    def get_initial(self):
+
+        user = self.request.user
+
+        if user.is_authenticated and hasattr(user, 'profile'):
+            return {**self.initial, 'mobile': user.profile.mobile}
+
+        return self.initial
+
+    def form_valid(self, form):
+
+        referrer = self.request.GET.get("referrer", "")
+
+        obj = form.save(commit=False)
+
+        if referrer:
+            site = Site.objects.get_current()
+            obj.comment += f"\nhttps://{site.domain}{referrer}"
+
+        if self.request.user.is_authenticated:
+            obj.user = self.request.user
+
+        obj.save()
+
+        subject = '%s #%s' % (_("New callback request"), obj.id)
+
+        html = render_to_string('callback/email.html', {
+            'object': obj,
+            'site': Site.objects.get_current()
+        })
+
+        mail_managers(subject, html)
+
+        if apps.is_installed('turbosms'):
+            from turbosms.lib import send_sms
+            send_sms('%s #%s %s' % (_('Callback'), obj.id, obj.mobile))
+
+        return JsonResponse({
+            "message": _('Callback request was successfully sent')
+        })
+
+    def form_invalid(self, form):
+        return render(
+            self.request, 'callback/form.html', {'form': form}, status=403)
