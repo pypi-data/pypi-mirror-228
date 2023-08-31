3d081f718b1c37b8f6b4ada8497bdb8f15f5f85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:13:37 +0000

    Add package pelicun-test20230830/3.2b4/pelicun-test20230830-3.2b4.tar.gz

---
+++ packages/pelicun-test20230830/pelicun-test20230830-3.2b4.tar.gz/pelicun-test20230830-3.2b4/setup.py
@@ -0,0 +1,61 @@
+﻿from setuptools import find_namespace_packages
+from setuptools import setup
+import io
+import pelicun
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+long_description = read('README.md')
+
+setup(
+    name='pelicun-test20230830',
+    version=pelicun.__version__,
+    url='http://nheri-simcenter.github.io/pelicun/',
+    license='BSD License',
+    author='Adam Zsarnóczay',
+    tests_require=['pytest'],
+    author_email='adamzs@stanford.edu',
+    description='Probabilistic Estimation of Losses, Injuries, and Community resilience Under Natural hazard events',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['pelicun'],
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'numpy>=1.22.0, <2.0',
+        'scipy>=1.7.0, <2.0',
+        'pandas>=1.4.0, <2.0',
+        'tables>=3.7.0',
+        'requests>=2.0.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Framework :: Jupyter',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering',
+    ],
+    extras_require={
+        'testing': ['pytest'],
+    },
+)
