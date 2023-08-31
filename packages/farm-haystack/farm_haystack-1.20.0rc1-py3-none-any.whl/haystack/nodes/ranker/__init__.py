8fc08f599a39020f0dbdeed703981d483b30d2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:07:24 +0000

    Add package farm-haystack/1.20.0rc1/farm_haystack-1.20.0rc1-py3-none-any.whl

---
+++ packages/farm-haystack/farm_haystack-1.20.0rc1-py3-none-any.whl/haystack/nodes/ranker/__init__.py
@@ -0,0 +1,3 @@
+from haystack.nodes.ranker.base import BaseRanker
+from haystack.nodes.ranker.sentence_transformers import SentenceTransformersRanker
+from haystack.nodes.ranker.cohere import CohereRanker
