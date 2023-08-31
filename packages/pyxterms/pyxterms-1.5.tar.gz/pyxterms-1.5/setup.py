ad6b703fa114a79553d3d77edaf33ce9537bbb1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:14:35 +0000

    Add package pyxterms/1.5/pyxterms-1.5.tar.gz

---
+++ packages/pyxterms/pyxterms-1.5.tar.gz/pyxterms-1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="pyxterms",
+    version="1.5",
+    author="akamedagoat",
+    author_email="akamecanic@gmail.com",
+    description="A python module to customize terminal and add coloring.",
+    url="https://github.com/lutherantz/pyxterms",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_data={
+        "": ["README.md", "LICENSE.md"],
+    },
+)
