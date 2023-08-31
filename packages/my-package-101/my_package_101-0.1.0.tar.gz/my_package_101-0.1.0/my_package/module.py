768ba8138b35c6b12f2e9d298667ce643c56bfb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:08:10 +0000

    Add package my-package-101/0.1.0/my_package_101-0.1.0.tar.gz

---
+++ packages/my-package-101/my_package_101-0.1.0.tar.gz/my_package_101-0.1.0/my_package/module.py
@@ -0,0 +1,4 @@
+# my_package/module.py
+
+def add(a, b):
+    return a + b
---
+++ packages/my-package-101/my_package_101-0.1.0.tar.gz/my_package_101-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package_101",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # List your dependencies here
+    url="https://gitlab.com/nurdeka/my_package",
+    license="MIT",
+    author="Nurdeka Hidayanto",
+    author_email="nurdeka.bmkg@gmail.com",
+    description="A simple Python package",
+)
