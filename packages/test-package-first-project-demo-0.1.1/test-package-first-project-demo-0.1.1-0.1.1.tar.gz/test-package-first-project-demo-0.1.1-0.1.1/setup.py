16c8acc6ca388ef0565409c739090db3ff2a0759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:08:13 +0000

    Add package test-package-first-project-demo-0.1.1/0.1.1/test-package-first-project-demo-0.1.1-0.1.1.tar.gz

---
+++ packages/test-package-first-project-demo-0.1.1/test-package-first-project-demo-0.1.1-0.1.1.tar.gz/test-package-first-project-demo-0.1.1-0.1.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+  
+setuptools.setup(
+    name="test-package-first-project-demo-0.1.1",
+    version="0.1.1",
+    author="Arm pongpol",
+    packages=["my_package"],
+    url="https://github.com/Armynous/abc.git",
+    install_requires=[]
+)
