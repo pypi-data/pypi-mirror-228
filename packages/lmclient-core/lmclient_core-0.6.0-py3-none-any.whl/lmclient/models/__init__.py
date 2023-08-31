dc31bdb62990af1015c6c1fefef4631a3df9ac96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 03:07:43 +0000

    Add package lmclient-core/0.6.0/lmclient_core-0.6.0-py3-none-any.whl

---
+++ packages/lmclient-core/lmclient_core-0.6.0-py3-none-any.whl/lmclient/models/__init__.py
@@ -0,0 +1,6 @@
+from lmclient.models.azure import AzureChat as AzureChat
+from lmclient.models.base import BaseChatModel as BaseChatModel
+from lmclient.models.minimax import MinimaxChat as MinimaxChat
+from lmclient.models.openai import OpenAIChat as OpenAIChat
+from lmclient.models.openai import OpenAIExtract as OpenAIExtract
+from lmclient.models.zhipu import ZhiPuChat as ZhiPuChat
---
+++ packages/lmclient-core/lmclient_core-0.6.0-py3-none-any.whl/lmclient/types.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import Any, Dict, Generic, Optional, Sequence, TypedDict, TypeVar, Union
+
+try:
+    from pydantic.v1 import BaseModel, Field
+except ImportError:
+    from pydantic import BaseModel, Field
+
+from typing_extensions import NotRequired
+
+T = TypeVar('T')
+
+
+class Message(TypedDict):
+    role: str
+    content: str
+    name: NotRequired[str]
+    function_call: NotRequired[str]
+
+
+MessageRequiredKeys = ('role', 'content')
+MessageNotRequiredKeys = ('name', 'function')
+Messages = Sequence[Message]
+ModelResponse = Dict[str, Any]
+Prompt = Union[str, Sequence[dict]]
+
+
+class ChatModelOutput(BaseModel, Generic[T]):  # type: ignore
+    parsed_result: Optional[T] = None
+    response: ModelResponse = Field(default_factory=dict)
