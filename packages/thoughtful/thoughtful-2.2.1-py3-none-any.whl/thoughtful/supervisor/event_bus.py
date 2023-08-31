5b862067f05007c1559e3ba78171a4209d05fe6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:08:26 +0000

    Add package thoughtful/2.2.1/thoughtful-2.2.1-py3-none-any.whl

---
+++ packages/thoughtful/thoughtful-2.2.1-py3-none-any.whl/thoughtful/supervisor/event_bus.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass, field
+from typing import Callable, List, Union
+
+from thoughtful.supervisor.manifest import Manifest
+from thoughtful.supervisor.reporting.status import Status
+from thoughtful.supervisor.reporting.step_report import StepReport
+
+
+@dataclass
+class StepReportChangeEvent:
+    step_report: StepReport
+
+
+@dataclass
+class NewManifestEvent:
+    manifest: Manifest
+
+
+@dataclass
+class RunStatusChangeEvent:
+    status: Status
+
+
+@dataclass
+class ArtifactsUploadedEvent:
+    output_uri: str
+
+
+Event = Union[
+    StepReportChangeEvent,
+    NewManifestEvent,
+    RunStatusChangeEvent,
+    ArtifactsUploadedEvent,
+]
+
+
+@dataclass
+class EventBus:
+    subscribers: List[Callable[[Event], None]] = field(default_factory=list)
+
+    def subscribe(self, callback: Callable[[Event], None]):
+        self.subscribers.append(callback)
+
+    def emit(self, event: Event):
+        for subscriber in self.subscribers:
+            subscriber(event)
