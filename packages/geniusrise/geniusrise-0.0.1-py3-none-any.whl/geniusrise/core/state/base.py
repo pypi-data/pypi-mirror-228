4910862a6d3e9575cee2ae4de2ab92611e6c9af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:13:13 +0000

    Add package geniusrise/0.0.1/geniusrise-0.0.1-py3-none-any.whl

---
+++ packages/geniusrise/geniusrise-0.0.1-py3-none-any.whl/geniusrise/core/state/base.py
@@ -0,0 +1,50 @@
+# 🧠 Geniusrise
+# Copyright (C) 2023  geniusrise.ai
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from abc import ABC, abstractmethod
+from typing import Dict, Optional
+
+
+class State(ABC):
+    """
+    Abstract base class for a state manager.
+
+    A state manager is responsible for getting and setting state.
+    """
+
+    @abstractmethod
+    def get_state(self, key: str) -> Optional[Dict]:
+        """
+        Get the state associated with a key.
+
+        Args:
+            key (str): The key to get the state for.
+
+        Returns:
+            Dict: The state associated with the key.
+        """
+        pass
+
+    @abstractmethod
+    def set_state(self, key: str, value: Dict):
+        """
+        Set the state associated with a key.
+
+        Args:
+            key (str): The key to set the state for.
+            value (Dict): The state to set.
+        """
+        pass
---
+++ packages/geniusrise/geniusrise-0.0.1-py3-none-any.whl/geniusrise/logging.py
@@ -0,0 +1,93 @@
+# 🧠 Geniusrise
+# Copyright (C) 2023  geniusrise.ai
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+import colorlog
+
+from geniusrise.config import LOGLEVEL
+
+
+def setup_logger() -> logging.Logger:
+    """
+    🛠️ **Setup Logger**: Configure and return a logger with a default ColoredFormatter.
+
+    This function sets up a logger for the `geniusrise-cli` with colorful logging outputs. The log level is determined by the `LOGLEVEL` from the configuration.
+
+    ## Usage:
+    ```python
+    logger = setup_logger()
+    logger.info("This is a fancy info log!")
+    ```
+
+    Returns:
+        logging.Logger: Configured logger with colorful outputs.
+    """
+    # Reset all existing loggers
+    for logger in logging.root.manager.loggerDict.values():
+        if isinstance(logger, logging.Logger):
+            logger.setLevel(logging.NOTSET)
+            for handler in logger.handlers.copy():
+                logger.removeHandler(handler)
+
+    # Define the custom formatter
+    formatter = (
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)-8s%(reset)s "
+            "%(yellow)s[%(asctime)s] "
+            "%(blue)s[%(name)s:%(lineno)d] "
+            "%(log_color)s%(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            reset=True,
+            log_colors={
+                "DEBUG": "white",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
+        )
+        if logging.getLevelName(LOGLEVEL) < logging.INFO
+        else colorlog.ColoredFormatter(
+            "%(log_color)-8s%(reset)s %(log_color)s%(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            reset=True,
+            log_colors={
+                "DEBUG": "white",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
+        )
+    )
+
+    # Suppress kafka library info logs
+    if logging.getLevelName(LOGLEVEL) < logging.INFO:
+        logger = logging.getLogger("kafka")
+        logger.setLevel(logging.WARN)
+
+    # Setup logger for geniusrise
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logging.basicConfig(encoding="utf-8", level=logging.getLevelName(LOGLEVEL), handlers=[handler])
+    logger = logging.getLogger("geniusrise")
+    logger.setLevel(logging.getLevelName(LOGLEVEL))
+
+    # logger.addHandler(handler)
+
+    return logger
