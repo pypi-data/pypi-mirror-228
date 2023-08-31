45f9d55f8c5f0dde3de37bb693edf33e10890d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:09:06 +0000

    Add package uritools/4.0.2/uritools-4.0.2.tar.gz

---
+++ packages/uritools/uritools-4.0.2.tar.gz/uritools-4.0.2/docs/conf.py
@@ -0,0 +1,14 @@
+project = "uritools"
+copyright = "2014-2023 Thomas Kemmer"
+version = "4.0"
+release = "4.0.2"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+]
+exclude_patterns = ["_build"]
+master_doc = "index"
+html_theme = "default"
