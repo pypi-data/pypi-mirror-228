65b5fc8741365de10aefedc877e2f2bf1dc7416d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 05:08:35 +0000

    Add package numequate/0.5.0/numequate-0.5.0.tar.gz

---
+++ packages/numequate/numequate-0.5.0.tar.gz/numequate-0.5.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='numequate',
+    version='0.5.0',
+    license="MIT License (MIT)",
+    description="A Python Mathematics Library.",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TuberAsk/numequate",
+    project_urls={
+        'Source': 'https://github.com/TuberAsk/numequate',
+        'Bug Reports': 'https://github.com/TuberAsk/numequate/issues',
+        'Documentation': 'https://github.com/TuberAsk/numequate/wiki',
+        'Funding': 'https://patreon.com/numequate',
+    },
+)
