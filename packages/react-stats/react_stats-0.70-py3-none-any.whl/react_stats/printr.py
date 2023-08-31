43d5804cd0d1329af66d906238b8386022950e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:12:51 +0000

    Add package react-stats/0.70/react_stats-0.70-py3-none-any.whl

---
+++ packages/react-stats/react_stats-0.70-py3-none-any.whl/react_stats/printr.py
@@ -0,0 +1,14 @@
+
+
+
+
+def printr():
+    print("package is working")
+    print("package is working")
+    print("package is working")
+    print("package is working")
+    print("package is working")
+    
+     
+def main():
+    printr()
