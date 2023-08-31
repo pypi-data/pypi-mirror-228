ec2571bba1ec9bf8106313cb88569b97701a998a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:14:29 +0000

    Add package stix-shifter-modules-cbcloud/5.3.1.dev120/stix_shifter_modules_cbcloud-5.3.1.dev120-py2.py3-none-any.whl

---
+++ packages/stix-shifter-modules-cbcloud/stix_shifter_modules_cbcloud-5.3.1.dev120-py2.py3-none-any.whl/stix_shifter_modules/cbcloud/stix_transmission/results_connector.py
@@ -0,0 +1,36 @@
+import json
+
+from stix_shifter_utils.modules.base.stix_transmission.base_json_results_connector import BaseJsonResultsConnector
+from stix_shifter_utils.utils.error_response import ErrorResponder
+from stix_shifter_utils.utils import logger
+
+
+class ResultsConnector(BaseJsonResultsConnector):
+    def __init__(self, api_client):
+        self.api_client = api_client
+        self.logger = logger.set_logger(__name__)
+        self.connector = __name__.split('.')[1]
+
+    async def create_results_connection(self, search_id, offset, length):
+        response = await self.api_client.get_search_results(search_id, offset, length)
+        response_code = response.code
+        response_text = response.read()
+        error = None
+        response_dict = dict()
+
+        try:
+            response_dict = json.loads(response_text)
+        except ValueError as ex:
+            self.logger.debug(response_text)
+            error = Exception(f'Can not parse response: {ex}')
+
+        return_obj = dict()
+        return_obj['success'] = False
+
+        if response_dict and response_code == 200:
+            return_obj['success'] = True
+            return_obj['data'] = response_dict['results']
+        else:
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'], error=error, connector=self.connector)
+
+        return return_obj
