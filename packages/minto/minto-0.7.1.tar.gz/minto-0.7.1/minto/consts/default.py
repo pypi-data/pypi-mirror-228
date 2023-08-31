fde7a50fbcfb7ea2c4d6d6306975b32729746f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:08:09 +0000

    Add package minto/0.7.1/minto-0.7.1.tar.gz

---
+++ packages/minto/minto-0.7.1.tar.gz/minto-0.7.1/minto/consts/default.py
@@ -0,0 +1,10 @@
+import pathlib
+
+DEFAULT_RESULT_DIR = pathlib.Path("./.jb_results")
+DEFAULT_PROBLEM_NAMES = [
+    "bin-packing",
+    "knapsack",
+    "nurse-scheduling",
+    "travelling-salesman",
+    "travelling-salesman-with-time-windows",
+]
