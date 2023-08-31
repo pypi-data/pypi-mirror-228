9841c64775715e9a7333f732faf37cdf42d39767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 04:13:31 +0000

    Add package pitters/1.2/pitters-1.2.tar.gz

---
+++ packages/pitters/pitters-1.2.tar.gz/pitters-1.2/pitters/__init__.py
@@ -0,0 +1,40 @@
+import unicodedata
+
+def sanitize_spaces(string):
+    return ' '.join(string.split())
+
+
+def convert_case(string, case='original'):
+    #Deja el case requerido
+    if case == 'original':
+        return string
+    elif case == 'upper' or case == 'uppercase' or case == 'mayus':
+        string = string.upper()
+
+    elif case =='lower' or case == 'lowercase' or case == 'minus':
+        string = string.lower()
+
+    elif case == 'capitalize' or case == 'capital':
+        string = string.capitalize()
+
+    else:
+        ValueError("El argumento de 'case' = ['original', 'upper', 'lower', 'capitalize']")
+    
+    return string
+
+
+def to_ascii(string, enie=False):
+    if enie == True:
+        string = string.replace("ñ", "#!#").replace("Ñ", "$!$")
+        string = unicodedata.normalize('NFKD', string).encode('ascii','ignore').decode('ascii')
+        string = string.replace("#!#", "ñ").replace("$!$", "Ñ")
+    else:
+        string = unicodedata.normalize('NFKD', string).encode('ascii','ignore').decode('ascii')
+    return string
+
+
+def magic(string, case='original', enie=False):
+    string = sanitize_spaces(string)
+    string = convert_case(string, case=case)
+    string = to_ascii(string, enie=enie)
+    return string
