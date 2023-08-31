23cbc91f6506309b9a9295d4c10dd84dc16a4d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 15:08:12 +0000

    Add package syncdns/1.0.1/syncdns-1.0.1-py3-none-any.whl

---
+++ packages/syncdns/syncdns-1.0.1-py3-none-any.whl/syncdns/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains project version information.
+
+.. currentmodule:: syncdns.version
+.. moduleauthor:: pradhuman_baid 
+"""
+
+__version__ = "1.0.1"  #: the working version
+__release__ = "1.0.1"  #: the release version
