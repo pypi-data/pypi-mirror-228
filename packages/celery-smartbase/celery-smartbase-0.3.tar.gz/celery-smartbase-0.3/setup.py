f70eea29cd5b7df4839936412a8424b8c0a27180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-31 14:07:44 +0000

    Add package celery-smartbase/0.3/celery-smartbase-0.3.tar.gz

---
+++ packages/celery-smartbase/celery-smartbase-0.3.tar.gz/celery-smartbase-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='celery-smartbase',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'Django~=3.2.11',
+        'djangorestframework==3.13.1',
+        'django-model-utils==3.1.2',
+        'django-celery-results==2.2.0',
+        'django-celery-beat==2.2.1',
+        'kombu~=5.2.3',
+        'celery~=5.2.3',
+    ],
+    description='An extension and improvement of the Celery package to avoid duplicate tasks and view pending tasks in Django.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/TonySchneider/celery-smartbase',
+)
