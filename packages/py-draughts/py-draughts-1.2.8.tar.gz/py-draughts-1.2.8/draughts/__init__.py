f6053bb1c84be922ee79e407cd56da74c9bcb1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:11:58 +0000

    Add package py-draughts/1.2.8/py-draughts-1.2.8.tar.gz

---
+++ packages/py-draughts/py-draughts-1.2.8.tar.gz/py-draughts-1.2.8/draughts/__init__.py
@@ -0,0 +1,53 @@
+# This file is part of the py-draughts library.
+# Copyright (C) 2023-2023 Michał Skibiński
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+A draughts library with advenced (customizable) WEB UI move generation and validation,
+PDN parsing and writing. Supports multiple variants of game.
+"""
+
+from typing import Literal, NewType
+
+__version__ = "1.2.8"
+__author__ = "Michał Skibiński"
+
+BaseBoard = NewType("BaseBoard", type)
+# create board type
+
+
+def get_board(
+    variant: Literal["standard", "american", "frisian"], fen: str = None
+) -> BaseBoard:
+    """
+    Board factory method.
+    - ``standard`` - standard draughts board
+    - ``american`` - american checkers board
+    - ``frisian`` - frisian draughts board
+    """
+    from draughts.boards.base import BaseBoard
+    from draughts.boards.standard import Board as StandardBoard
+    from draughts.boards.frisian import Board as FrisianBoard
+    from draughts.boards.american import Board as AmericanBoard
+
+    BOARDS = {
+        "standard": StandardBoard,
+        "frisian": FrisianBoard,
+        "american": AmericanBoard,
+    }
+    board_cls: BaseBoard = BOARDS[variant]
+    if fen:
+        return board_cls.from_fen(fen)
+    return board_cls()
---
+++ packages/py-draughts/py-draughts-1.2.8.tar.gz/py-draughts-1.2.8/setup.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+import setuptools
+
+from draughts.__init__ import __doc__, __version__
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.md").read_text()
+
+with open(this_directory / "requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="py-draughts",
+    install_requires=requirements,
+    version=__version__,
+    author="Michał Skibiński",
+    author_email="mskibinski109@gmail.com",
+    description=__doc__.replace("\n", " ").strip(),
+    long_description=long_description,
+    # rst
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    package_data={"draughts": ["static/js/*", "static/css/*", "templates/*"]},
+    license="GPL-3.0+",
+    keywords=" draughts, checkers, AI mini-max, game, board",
+    url="https://github.com/michalskibinski109/py-draughts",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Games/Entertainment :: Turn Based Strategy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed",
+    ],
+    project_urls={
+        "Documentation": "https://michalskibinski109.github.io/py-draughts/index.html",
+    },
+    python_requires=">=3.7",
+)
