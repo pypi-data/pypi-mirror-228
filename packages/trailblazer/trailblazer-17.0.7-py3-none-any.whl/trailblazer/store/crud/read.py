3ee5777d5159f95a8726242d4a8536b836536001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:14:31 +0000

    Add package trailblazer/17.0.7/trailblazer-17.0.7-py3-none-any.whl

---
+++ packages/trailblazer/trailblazer-17.0.7-py3-none-any.whl/trailblazer/store/crud/read.py
@@ -0,0 +1,114 @@
+from datetime import datetime
+from typing import Callable, Dict, List, Optional, Union
+
+from sqlalchemy import desc
+
+from trailblazer.store.base import BaseHandler_2
+from trailblazer.store.filters.analyses_filters import (
+    AnalysisFilter,
+    apply_analysis_filter,
+)
+from trailblazer.store.filters.job_filters import JobFilter, apply_job_filter
+from trailblazer.store.filters.user_filters import UserFilter, apply_user_filter
+from trailblazer.store.models import Analysis, Job, User
+
+
+class ReadHandler(BaseHandler_2):
+    """Class for reading items in the database."""
+
+    def get_nr_jobs_with_status_per_category(
+        self, status: str, since_when: datetime = None
+    ) -> List[Dict[str, Union[str, int]]]:
+        """Return the number of jobs with status per category (name)."""
+        filter_map: Dict[Callable, Optional[Union[str, bool]]] = {
+            JobFilter.FILTER_BY_STATUS: status,
+            JobFilter.FILTER_BY_SINCE_WHEN: since_when,
+        }
+        filter_functions: List[Callable] = [
+            function for function, supplied_arg in filter_map.items() if supplied_arg
+        ]
+        categories = apply_job_filter(
+            filter_functions=filter_functions,
+            jobs=self.get_job_query_with_name_and_count_labels(),
+            since_when=since_when,
+            status=status,
+        )
+        categories = categories.group_by(Job.name).all()
+        return [{"name": category.name, "count": category.count} for category in categories]
+
+    def get_analysis(self, case_name: str, started_at: datetime, status: str) -> Optional[Analysis]:
+        """Return the latest analysis for supplied parameters."""
+        return apply_analysis_filter(
+            filter_functions=[
+                AnalysisFilter.FILTER_BY_CASE_NAME,
+                AnalysisFilter.FILTER_BY_STARTED_AT,
+                AnalysisFilter.FILTER_BY_STATUS,
+            ],
+            analyses=self.get_query(table=Analysis),
+            case_name=case_name,
+            started_at=started_at,
+            status=status,
+        ).first()
+
+    def get_latest_analysis_for_case(self, case_name: str) -> Optional[Analysis]:
+        """Return the latest analysis for a case."""
+        return (
+            apply_analysis_filter(
+                filter_functions=[
+                    AnalysisFilter.FILTER_BY_CASE_NAME,
+                ],
+                analyses=self.get_query(table=Analysis),
+                case_name=case_name,
+            )
+            .order_by(desc(Analysis.started_at))
+            .first()
+        )
+
+    def get_analysis_with_id(self, analysis_id: int) -> Optional[Analysis]:
+        """Get a single analysis by id."""
+        return apply_analysis_filter(
+            filter_functions=[AnalysisFilter.FILTER_BY_ID],
+            analyses=self.get_query(table=Analysis),
+            analysis_id=analysis_id,
+        ).first()
+
+    def get_user(
+        self,
+        email: str = None,
+        exclude_archived: bool = True,
+    ) -> User:
+        """Return user from the database."""
+        filter_map: Dict[Callable, Optional[Union[str, bool]]] = {
+            UserFilter.FILTER_BY_CONTAINS_EMAIL: email,
+            UserFilter.FILTER_BY_IS_NOT_ARCHIVED: exclude_archived,
+        }
+        filter_functions: List[Callable] = [
+            function for function, supplied_arg in filter_map.items() if supplied_arg
+        ]
+        return apply_user_filter(
+            filter_functions=filter_functions,
+            users=self.get_query(table=User),
+            email=email,
+        ).first()
+
+    def get_users(
+        self,
+        name: str = None,
+        email: str = None,
+        exclude_archived: bool = True,
+    ) -> List[User]:
+        """Return users from the database."""
+        filter_map: Dict[Callable, Optional[Union[str, bool]]] = {
+            UserFilter.FILTER_BY_CONTAINS_EMAIL: email,
+            UserFilter.FILTER_BY_CONTAINS_NAME: name,
+            UserFilter.FILTER_BY_IS_NOT_ARCHIVED: exclude_archived,
+        }
+        filter_functions: List[Callable] = [
+            function for function, supplied_arg in filter_map.items() if supplied_arg
+        ]
+        return apply_user_filter(
+            filter_functions=filter_functions,
+            users=self.get_query(table=User),
+            email=email,
+            name=name,
+        ).all()
