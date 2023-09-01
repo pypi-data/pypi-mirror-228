6491d0e6d395e1224f28358c47a1e3cf3ccafa05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:12:14 +0000

    Add package eliasliu/0.1.30/eliasliu-0.1.30-py3-none-any.whl

---
+++ packages/eliasliu/eliasliu-0.1.30-py3-none-any.whl/elias/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  7 10:45:26 2023
+
+@author: Administrator
+"""
+
+# import usual
+# import wechat
+
+blog = 'https://note.youdao.com/s/6TBmbe1q'
+tip = f'函数应用详细教程见作者(Elias)笔记：\n{blog}'
+print(tip)
