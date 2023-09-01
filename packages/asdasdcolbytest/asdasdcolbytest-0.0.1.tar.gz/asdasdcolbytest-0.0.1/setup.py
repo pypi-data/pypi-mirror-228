265c2ebfd5d5a7576355906c7cffdf4b3be97f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:08:35 +0000

    Add package asdasdcolbytest/0.0.1/asdasdcolbytest-0.0.1.tar.gz

---
+++ packages/asdasdcolbytest/asdasdcolbytest-0.0.1.tar.gz/asdasdcolbytest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='asdasdcolbytest',
+      version='0.0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/',
+      author='zzz',
+      author_email='zzz@example.com',
+      license='MIT',
+      packages=['asdasdcolbytest'],
+      zip_safe=False)
