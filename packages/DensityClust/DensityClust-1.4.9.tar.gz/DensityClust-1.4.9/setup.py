9f7e6016e94fc06954f6f5d10f0f81455ae0d4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 15:08:15 +0000

    Add package DensityClust/1.4.9/DensityClust-1.4.9.tar.gz

---
+++ packages/DensityClust/DensityClust-1.4.9.tar.gz/DensityClust-1.4.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='DensityClust',
+      version='1.4.9',
+      author='Luo Xiaoyu',
+      description='Molecular Clump extraction algorithm based on Local Density Clustering*',
+      author_email='vastlxy@163.com',
+      packages=['DensityClust', 't_match', 'tools', 'fit_clump_function', 'Generate', 'LDC_MGM_main'],
+      url="https://github.com/Luoxiaoyu828/LDC-MGM",
+      project_urls={
+        "Bug Tracker": "https://github.com/Luoxiaoyu828/LDC_MGM/issues",
+    })
