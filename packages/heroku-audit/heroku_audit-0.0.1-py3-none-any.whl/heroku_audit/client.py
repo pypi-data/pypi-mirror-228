f1ca0946937e9ba10e56c8013f2e61935ed19f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:13:06 +0000

    Add package heroku-audit/0.0.1/heroku_audit-0.0.1-py3-none-any.whl

---
+++ packages/heroku-audit/heroku_audit-0.0.1-py3-none-any.whl/heroku_audit/client.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+import heroku3
+from rich import print
+from rich.text import Text
+
+try:
+    heroku = heroku3.from_key(os.environ["HEROKU_API_KEY"])
+except KeyError:
+    print(Text("Please set $HEROKU_API_KEY to a valid Heroku API key.", style="red"))
+    sys.exit(1)
