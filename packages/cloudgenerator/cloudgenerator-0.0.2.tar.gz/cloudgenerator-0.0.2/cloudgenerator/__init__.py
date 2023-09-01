bdc87c7209084f3bfbdda6837135bd4e6d04e04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 06:14:13 +0000

    Add package cloudgenerator/0.0.2/cloudgenerator-0.0.2.tar.gz

---
+++ packages/cloudgenerator/cloudgenerator-0.0.2.tar.gz/cloudgenerator-0.0.2/cloudgenerator/__init__.py
@@ -0,0 +1,5 @@
+from .cloudgenerator import generate_password
+from .cloudgenerator import calculate_complexity
+from .cloudgenerator import setlenght
+from .cloudgenerator import complexity_word
+from .cloudgenerator import generate_token
