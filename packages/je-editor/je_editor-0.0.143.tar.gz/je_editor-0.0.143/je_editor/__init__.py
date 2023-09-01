827adfaf2129951f539f0f2c84d92ef39291c6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 15:09:05 +0000

    Add package je-editor/0.0.143/je_editor-0.0.143.tar.gz

---
+++ packages/je-editor/je_editor-0.0.143.tar.gz/je_editor-0.0.143/je_editor/__init__.py
@@ -0,0 +1,41 @@
+# start editor
+from je_editor.start_editor import start_editor
+# Editor
+from je_editor.pyside_ui.main_ui.main_editor import EditorMain
+from je_editor.pyside_ui.main_ui.main_editor import EDITOR_EXTEND_TAB
+from je_editor.pyside_ui.main_ui.editor.editor_widget import EditorWidget
+from je_editor.pyside_ui.main_ui.editor.editor_widget_full import FullEditorWidget
+# Exceptions
+from je_editor.utils.exception.exceptions import JEditorException
+from je_editor.utils.exception.exceptions import JEditorExecException
+from je_editor.utils.exception.exceptions import JEditorRunOnShellException
+from je_editor.utils.exception.exceptions import JEditorSaveFileException
+from je_editor.utils.exception.exceptions import JEditorOpenFileException
+from je_editor.utils.exception.exceptions import JEditorContentFileException
+from je_editor.utils.exception.exceptions import JEditorCantFindLanguageException
+from je_editor.utils.exception.exceptions import JEditorJsonException
+# Color
+from je_editor.pyside_ui.colors.global_color import error_color
+from je_editor.pyside_ui.colors.global_color import output_color
+# Exec and shell
+from je_editor.pyside_ui.code.code_process.code_exec import exec_manage
+from je_editor.pyside_ui.code.code_process.code_exec import ExecManager
+from je_editor.pyside_ui.code.shell_process.shell_exec import default_shell_manager
+from je_editor.pyside_ui.code.shell_process.shell_exec import ShellManager
+# Browser
+from je_editor.pyside_ui.browser.je_broser import JEBrowser
+# Highlight
+from je_editor.pyside_ui.code.complete_list.total_complete_list import complete_list
+from je_editor.pyside_ui.code.syntax.python_syntax import PythonHighlighter
+from je_editor.pyside_ui.code.syntax.syntax_setting import syntax_word_setting_dict, syntax_rule_setting_dict
+
+
+__all__ = [
+    "start_editor", "EditorMain", "EDITOR_EXTEND_TAB",
+    "JEditorException", "JEditorExecException", "FullEditorWidget",
+    "JEditorRunOnShellException", "JEditorSaveFileException", "syntax_rule_setting_dict",
+    "JEditorOpenFileException", "JEditorContentFileException", "syntax_word_setting_dict",
+    "JEditorCantFindLanguageException", "JEditorJsonException", "PythonHighlighter",
+    "error_color", "output_color", "EditorWidget", "JEBrowser", "complete_list",
+    "exec_manage", "default_shell_manager", "ExecManager", "ShellManager"
+]
