8d02399e6ae680d3819de3799c0654449bfdff08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:52 +0000

    Add package hypervise/0.1.0a0/hypervise-0.1.0a0.tar.gz

---
+++ packages/hypervise/hypervise-0.1.0a0.tar.gz/hypervise-0.1.0a0/README.md
@@ -0,0 +1,2 @@
+# hypervise
+A user-friendly virtual environment manager for Python.
---
+++ packages/hypervise/hypervise-0.1.0a0.tar.gz/hypervise-0.1.0a0/hypervise/base.py
@@ -0,0 +1,9 @@
+class New():
+    """
+    Description for class.
+
+    :ivar _varname: List of arguments for mapping
+    """
+
+    def __init__(self, params_list, params_dict):
+        """ Assign parameter list and parameter dictionary """
