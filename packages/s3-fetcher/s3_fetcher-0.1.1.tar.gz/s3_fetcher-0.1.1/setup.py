3a26dec48866edf1528fd6467bdb78307eec07cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:12:35 +0000

    Add package s3-fetcher/0.1.1/s3_fetcher-0.1.1.tar.gz

---
+++ packages/s3-fetcher/s3_fetcher-0.1.1.tar.gz/s3_fetcher-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='s3_fetcher',
+      version='0.1.1',
+      description='Simplified Command-Line Tool for Fetching Files from Amazon S3',
+      author='Kristian Marlowe Ole',
+      author_email='kristian.ole@done-data-solutions.com',
+      license='GPLv3',
+      install_requires=["boto3"],
+      entry_points={
+            'console_scripts': [
+                  's3fetcher = s3_fetcher.cli:main',
+            ],
+      },
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      python_requires='>=3.5'
+)
