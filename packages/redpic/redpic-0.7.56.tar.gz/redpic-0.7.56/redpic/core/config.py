30f5abb2fae4b99155c21520f26ecb02a0bfa0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:11:27 +0000

    Add package redpic/0.7.56/redpic-0.7.56.tar.gz

---
+++ packages/redpic/redpic-0.7.56.tar.gz/redpic-0.7.56/redpic/core/config.py
@@ -0,0 +1,29 @@
+from logging import config as logging_config
+
+from redpic.core.logger import LOGGING
+
+logging_config.dictConfig(LOGGING)
+
+NAME = "redpic"
+
+VERSION = "0.7.56"
+
+AUTHOR = "Vyacheslav Fedorov"
+
+AUTHOR_EMAIL = "slava@fuodorov.ru"
+
+DOC = "Relativistic Difference Scheme Particle-in-Cell code (REDPIC)"
+
+URL = "https://github.com/fuodorov/redpic"
+
+DISABLE_JIT = False
+
+DISABLE_NOPYTHON = False
+
+DISABLE_PARALLEL = False
+
+DISABLE_FASTMATH = False
+
+DISABLE_CACHE = False
+
+DISABLE_NOGIL = False
