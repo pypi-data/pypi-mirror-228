e32dd466b311ea4c7e08d331ee766f604f43e5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:51 +0000

    Add package pyguidesktop/0.3/pyguidesktop-0.3-py3-none-any.whl

---
+++ packages/pyguidesktop/pyguidesktop-0.3-py3-none-any.whl/pyguidesktop/logger.py
@@ -0,0 +1,40 @@
+import os
+import logging
+import traceback
+from datetime import datetime
+
+TIME_DIFF_SECONDS = 10
+
+package_dir = os.path.dirname(os.path.abspath(__file__))
+main_folder = os.path.dirname(package_dir)
+
+screenshots_dir = os.path.join(main_folder, "logs")
+
+os.makedirs(screenshots_dir, exist_ok=True)
+
+now = datetime.now()
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+folder_path = os.path.join(screenshots_dir, timestamp)
+os.makedirs(folder_path, exist_ok=True)
+
+log_file = os.path.join(folder_path, f"{timestamp}.log")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filename=log_file,
+    filemode='w'
+)
+
+def is_same_folder(timestamp_folder):
+    now = datetime.now()
+    folder_time = datetime.strptime(timestamp_folder, "%Y-%m-%d_%H-%M-%S")
+    time_difference = now - folder_time
+    return abs(time_difference.total_seconds()) <= TIME_DIFF_SECONDS
+
+def log_debug(message):
+    logging.debug(message)
+    
+def log_error(exception):
+    exception_info = traceback.format_exc()
+    logging.error(f"An error occurred:\n{exception_info}")
