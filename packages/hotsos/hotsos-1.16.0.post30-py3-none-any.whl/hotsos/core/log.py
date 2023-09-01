1824da1fc4a3623b202b48db13aef4d1dcbdf2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:11:44 +0000

    Add package hotsos/1.16.0.post30/hotsos-1.16.0.post30-py3-none-any.whl

---
+++ packages/hotsos/hotsos-1.16.0.post30-py3-none-any.whl/hotsos/core/log.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import logging
+import tempfile
+
+from hotsos.core.config import HotSOSConfig
+
+log = logging.getLogger('hotsos')
+
+
+def setup_logging():
+    fmt = ("%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] "
+           "%(message)s")
+    log.name = 'hotsos.plugin.{}'.format(HotSOSConfig.plugin_name)
+    if not HotSOSConfig.debug_mode:
+        logging.basicConfig(format=fmt, level=logging.DEBUG,
+                            filename=tempfile.mktemp(suffix='hotsos.log'))
+    else:
+        logging.basicConfig(format=fmt, level=logging.DEBUG)
