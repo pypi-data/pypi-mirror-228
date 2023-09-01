dcc4f665930df98cb4b09c1a168b0fb8c6a18ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:59 +0000

    Add package yxdump/1.0.1/yxdump-1.0.1.tar.gz

---
+++ packages/yxdump/yxdump-1.0.1.tar.gz/yxdump-1.0.1/README.md
@@ -0,0 +1,208 @@
+# yxd
+
+yxd is a hex dump tool similar to xxd, but with features that I wanted. It's written in python3, and doesn't have any requirements outside of the python3 standard library (sys, argparse, re). The script itself is pretty simple, and should be easy to add features to if needed.
+
+## Install
+
+You can install the `yxdump` package with pip
+```
+python3 -m pip install yxdump
+```
+
+## Usage
+
+There are two ways to use yxd: from the command line, and as a library.
+
+## Command Line
+
+```
+$ yxd -h
+usage: yxd [-h] [-f INFILE] [-o STARTOFFSET] [-s BUFFERSIZE] [-r] [--plain] [--xx] [--ps] [--py] [--sc] [--style] [-v] [input]
+
+yxd - Yuu's heX Dumper
+
+positional arguments:
+  input           File to open
+
+options:
+  -h, --help      show this help message and exit
+  -f INFILE       File to open
+  -o STARTOFFSET  Offset to start within file
+  -s BUFFERSIZE   Size of buffer to dump
+  -r              Do a reverse hex dump
+  --plain         Print in xxd style plain text, compatible with xxd
+  --xx            Print in xx format, a modified xxd-style dump for use with xx
+  --ps, -ps       output in postscript plain hexdump style.
+  --py            Create a python script to generate the buffer
+  --sc            Create a C shellcode loader from buffer
+  --style         Show Current Hex Style
+  -v              Print Version Info
+```
+
+### Reading Files
+
+Read a file with command line argument
+```
+$ yxd file.bin
+```
+You can also read from stdin
+```
+$ cat file.bin | yxd
+```
+
+### Specifying offsets and sizes
+
+Specify the beginning offset with the `-o` flag, and the size of the buffer with the `-s` flag. You can use decimal or hex (prefixed with 0x) to represent these numbers.
+
+```
+$ yxd base.bin -o 0x40 -s 0x38
+00000040│0100 0000 0500 0000│0000 0000 0000 0000│................
+00000050│0000 4000 0000 0000│0000 4000 0000 0000│..@.......@.....
+00000060│0000 0000 0100 0000│0000 0000 0100 0000│................
+00000070│0000 2000 0000 0000│                   │.. .....
+```
+
+### xxd-style Output
+
+yxd is also capable of producing xxd-style hex output with the `--plain` flag.
+
+```
+$ yxd base.bin --plain
+00000000: 7f45 4c46 0201 0100 0000 0000 0000 0000  .ELF............
+00000010: 0200 3e00 0100 0000 7800 4000 0000 0000  ..>.....x.@.....
+00000020: 4000 0000 0000 0000 0000 0000 0000 0000  @...............
+00000030: 0000 0000 4000 3800 0100 0000 0000 0000  ....@.8.........
+00000040: 0100 0000 0500 0000 0000 0000 0000 0000  ................
+00000050: 0000 4000 0000 0000 0000 4000 0000 0000  ..@.......@.....
+00000060: 0000 0000 0100 0000 0000 0000 0100 0000  ................
+00000070: 0000 2000 0000 0000 b03c 66bf 0600 0f05  .. ......<f.....
+```
+
+### xx Compatible Hex Dump 
+
+yxd can create .xx files compatible with the [xx project](https://github.com/netspooky/xx) by using the flag `--xx`. These are modified xxd style hex dumps with the offset on the side within an `xx` comment, to allow for editing and markup while retaining offset data and the ASCII dump of the file.
+
+```
+$ yxd png.5e86c4ab.bin --xx
+8950 4e47 0d0a 1a0a 0000 000d 4948 4452  ; 00000000: .PNG........IHDR
+0000 0001 0000 0001 0100 0000 0037 6ef9  ; 00000010: .............7n.
+2400 0000 1049 4441 5478 9c62 6001 0000  ; 00000020: $....IDATx.b`...
+00ff ff03 0000 0600 0557 bfab d400 0000  ; 00000030: .........W......
+0049 454e 44ae 4260 82                   ; 00000040: .IEND.B`.
+```
+
+### Plain Hex Dump
+
+This is a plain hex dump with all the hexbytes as one long line. This is equivalent to the xxd flag `-ps`. You can use `-ps` or `--ps` to produce this.
+
+```
+$ yxd png.5e86c4ab.bin -ps 
+89504e470d0a1a0a0000000d4948445200000001000000010100000000376ef9240000001049444154789c626001000000ffff03000006000557bfabd40000000049454e44ae426082
+```
+
+### Reverse Hex Dump
+
+yxd does reverse hex dumps and supports both yxd and xxd style output.
+
+```
+$ yxd base.bin > base.yxd
+$ yxd base.yxd -r
+ELF>x@@@8@@ �<f�
+```
+
+### Script Generation
+
+One of my main use cases for this tool is to create buffers from files to manipulate them.
+
+```
+$ yxd base.bin --py
+```
+
+This dumps the following python script that willwrite the input file and give it a name based on the hash of the file. This is useful if you are doing file format research and need to help track minor changes in files as you edit them. A script form can also make it easier to comment on specific sections, and add your own calculations as needed.
+
+```python
+import struct
+import sys
+import hashlib
+
+def writeBin(b,h):
+    outfile = h + ".bin"
+    f = open(outfile,'wb')
+    f.write(b)
+    f.close()
+    print(outfile)
+
+b =  b""
+b += b"\x7f\x45\x4c\x46\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00" # 00000000 .ELF............
+b += b"\x02\x00\x3e\x00\x01\x00\x00\x00\x78\x00\x40\x00\x00\x00\x00\x00" # 00000010 ..>.....x.@.....
+b += b"\x40\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" # 00000020 @...............
+b += b"\x00\x00\x00\x00\x40\x00\x38\x00\x01\x00\x00\x00\x00\x00\x00\x00" # 00000030 ....@.8.........
+b += b"\x01\x00\x00\x00\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" # 00000040 ................
+b += b"\x00\x00\x40\x00\x00\x00\x00\x00\x00\x00\x40\x00\x00\x00\x00\x00" # 00000050 ..@.......@.....
+b += b"\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00" # 00000060 ................
+b += b"\x00\x00\x20\x00\x00\x00\x00\x00\xb0\x3c\x66\xbf\x06\x00\x0f\x05" # 00000070 .. ......<f.....
+
+m = hashlib.sha256()
+m.update(b)
+shorthash = m.digest().hex()[0:8]
+writeBin(b,shorthash)
+```
+
+Similarly, the `--sc` option can turn your file buffer into a C program that runs it as shellcode
+```
+$ yxd base.bin -o 0x78 --sc
+```
+
+This grabs the shellcode of this specific binary and turns it into a dropper.
+
+```c
+#include <stdio.h>
+#include <string.h>
+
+char code[] = "\xb0\x3c\x66\xbf\x06\x00\x0f\x05";
+
+int main() {
+    printf("len:%d bytes\n", strlen(code));
+    (*(void(*)()) code)();
+    return 0;
+}
+```
+
+## As a Library
+
+The yxd library can do a hex dump with the same style options as in the command line tool, from a library.
+
+Example of basic yxd API usage:
+```
+>>> import yxd
+>>> a = b"\x41"*32
+>>> a += b"\x42"*32
+>>> yxd.yxd(a)
+00000000: 4141 4141 4141 4141 4141 4141 4141 4141  AAAAAAAAAAAAAAAA
+00000010: 4141 4141 4141 4141 4141 4141 4141 4141  AAAAAAAAAAAAAAAA
+00000020: 4242 4242 4242 4242 4242 4242 4242 4242  BBBBBBBBBBBBBBBB
+00000030: 4242 4242 4242 4242 4242 4242 4242 4242  BBBBBBBBBBBBBBBB
+```
+
+By default the yxd library does hex dumps in the xxd format. To output in a different format, you can set the `outFormat` variable to any of the available formats.
+```python
+import yxd
+a = b"\x41"*32
+a += b"\x42"*32
+a += b"\xfa\xde\xdd\xdd\xec\xc5\xde\xee"
+yxd.yxd(a, baseAddr=0x1000, outFormat="yxd")
+```
+
+## Styling
+
+The yxdconfig.py file contains the style information for each byte, as well as templates for scripts. You can use ANSI escape codes in each, to enable everything from foreground and background colors, to blinking, underline, and more. These are all contained in a big dictionary called `bytez`
+
+Use the `--style` flag to see what the current styling looks like.
+
+## Contributing
+
+PRs are welcome. There are still some features I'd like to add, and I would love to see other people's ideas. There are a lot of hex editing tools out there, but this one fits my usecase for simple, portable hex manipulation, that is also pretty looking.
+
+Mastodon: https://haunted.computer/@netspooky
+
+Twitter: [@netspooky](https://twitter.com/netspooky)
---
+++ packages/yxdump/yxdump-1.0.1.tar.gz/yxdump-1.0.1/yxd.py
@@ -0,0 +1,374 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import re
+import yxdconfig as yc
+
+parser = argparse.ArgumentParser(description="yxd - Yuu's heX Dumper")
+parser.add_argument('-f', help='File to open', dest='inFile')
+parser.add_argument('input', help='File to open', nargs='?')
+parser.add_argument('-o', help='Offset to start within file', dest='startOffset', type=lambda x: int(x,0) )
+parser.add_argument('-s', help='Size of buffer to dump', dest='bufferSize', type=lambda x: int(x,0) )
+parser.add_argument('-r', help='Do a reverse hex dump',  dest='reverseDump', action="store_true")
+parser.add_argument('--plain', help='Print in xxd style plain text, compatible with xxd', dest='plainText', action="store_true")
+parser.add_argument('--xx', help='Print in xx format, a modified xxd-style dump for use with xx', dest='xxFormat', action="store_true")
+parser.add_argument('--ps','-ps', help='output in postscript plain hexdump style.', dest='psFormat', action="store_true")
+parser.add_argument('--py', help='Create a python script to generate the buffer', dest='genPythonScript', action="store_true")
+parser.add_argument('--sc', help='Create a C shellcode loader from buffer', dest='genShellcode', action="store_true")
+parser.add_argument('--style', help='Show Current Hex Style', dest='dumpStyle', action="store_true")
+parser.add_argument('-v', help='Print Version Info', dest='printVersion', action="store_true")
+
+versionInfo="yxd - Yuu's heX Dumper Version 20230831.0"""
+
+def styleDump():
+    """
+    Dump all styles.
+
+    Dump the color and style information from the yxdconfig
+    """
+    for i in range(0,256):
+        print(f"{yc.bytez[i]}{i:02X}{yc.EOA} ",end="")
+        if (i+1) % 16 == 0:
+            if i == 0:
+                continue
+            else:
+                print()
+
+def dump(inBytes,baseAddr=0,dataLen=0,blockSize=16,outFormat="xxd",quiet=False):
+    """
+    Dump hex.
+
+    This function performs a hex dump on the provided buffer with
+    the given parameters
+
+    Parameters
+    ----------
+    inBytes : bytes
+        The hex text buffer to work with.
+    baseAddr : int
+        The base address of the buffer
+    dataLen : int
+        The length of data to dump from the buffer. Default 0 = All
+    blockSize : int
+        The number of bytes per line
+    outFormat : str
+        The format of hex dump format to do
+    quiet : bool
+        If true, don't print the output to the terminal
+    Returns
+    -------
+    hexDumpOut : str
+        The text form of the hex dump
+    """
+    dataLen = len(inBytes) if ( dataLen == 0 ) or ( dataLen > len(inBytes) ) else dataLen # Sanity check
+    offs = 0
+    hexDumpOut = "" 
+    # Style
+    dumpBytez = yc.bytez
+    dumpOffstyle = yc.OFFSTYLE
+    dumpSEP0 = yc.SEP0
+    dumpSEP1 = yc.SEP1
+    dumpSEP2 = yc.SEP2
+    dumpEOA = yc.EOA
+    if ( outFormat == "xxd" ) or ( outFormat == "xx" ) or ( outFormat == "ps" ):
+        dumpBytez = {}
+        dumpOffstyle = ""
+        dumpSEP0  = ": "
+        dumpSEP1  = " "
+        dumpSEP2  = "  "
+        dumpEOA  = ""
+    while offs < dataLen:
+        try:
+            hb = []
+            bAsc = ""
+            bChunk = inBytes[offs:offs+blockSize]
+            chunkBytes = 0
+            for b in bChunk:
+                bFmt = f"{dumpBytez[b]}" if b in dumpBytez.keys() else ""
+                bAsc += f"{bFmt}{chr(b)}{dumpEOA}" if chr(b).isprintable() and b < 0x7F else f"{bFmt}.{dumpEOA}"
+                hb.append(f"{bFmt}{b:02x}{dumpEOA}")
+                chunkBytes = chunkBytes + 1
+            if chunkBytes < blockSize:
+                neededChunks = blockSize - chunkBytes
+                for nullChunk in range(neededChunks):
+                    hb.append("  ")
+            realOffs = offs+baseAddr
+            # This is where the buffer to print is built
+            offsetOut = f"{dumpOffstyle}{realOffs:08x}{dumpEOA}{dumpSEP0}"
+            hexOut =  f"{hb[0]}{hb[1]} "
+            hexOut += f"{hb[2]}{hb[3]} "
+            hexOut += f"{hb[4]}{hb[5]} "
+            hexOut += f"{hb[6]}{hb[7]}{dumpSEP1}"
+            hexOut += f"{hb[8]}{hb[9]} "
+            hexOut += f"{hb[10]}{hb[11]} "
+            hexOut += f"{hb[12]}{hb[13]} "
+            hexOut += f"{hb[14]}{hb[15]}{dumpSEP2}"
+            if outFormat == "xx":
+                if quiet == False:
+                    print(f"{hexOut}; {offsetOut}{bAsc}")
+                hexDumpOut += f"{hexOut}; {offsetOut}{bAsc}\n"
+            elif outFormat == "ps":
+                if quiet == False:
+                    print("".join(hexOut.split()),end="")
+                hexDumpOut += "".join(hexOut.split())
+            else:
+                if quiet == False:
+                    print(f"{offsetOut}{hexOut}{bAsc}")
+                hexDumpOut += f"{offsetOut}{hexOut}{bAsc}\n"
+            offs = offs + blockSize
+        except Exception as e:
+            print(f"yxd.dump: {e}")
+    if outFormat == "ps":
+        if quiet == False:
+            print() # Avoid annoying terminal behavior with a new line
+    return hexDumpOut
+
+def hexString(bChunk):
+    """
+    Create an escaped hex string.
+
+    This function performs turns a buffer of binary data into a
+    hex string in the style of "\x41\x41\x41\x41"
+
+    Parameters
+    ----------
+    inBytes : bytes
+        The hex text buffer to work with.
+
+    Returns
+    -------
+    bHex : str
+        the hex string with double quotes around it
+    bAsc : str
+        printable characters in this buffer, if any
+    cSum : int
+        checksum, used to determine if all bytes were 0
+    """
+    bHex = ""
+    bAsc = ""
+    cSum = 0
+    try:
+        for b in bChunk:
+            bAsc += chr(b) if chr(b).isprintable() and b < 0x7F else '.'
+            bHex += f"\\x{b:02x}"
+            cSum = cSum + b
+        bHex = f"\"{bHex}\""
+        return bHex, bAsc, cSum
+    except Exception as e:
+        print(f"yxd.hexString: {e}") # Maybe don't need
+        return bHex, bAsc, cSum
+
+def genPythonScript(inBytes):
+    """
+    Create a Python script loader.
+
+    This function performs turns a buffer of binary data into a
+    python script that creates a copy of your binary data.
+
+    Parameters
+    ----------
+    inBytes : bytes
+        The hex text buffer to work with.
+    """
+    print(yc.template1)
+    offs = 0
+    zeroCount = 0
+    savedOffset = 0
+    while offs < len(inBytes):
+        bChunk = inBytes[offs:offs+16]
+        chunkLen = len(bChunk)
+        bHex, bAsc, cSum = hexString(bChunk)
+        bHex = "b" + bHex
+        if cSum == 0:
+            if savedOffset == 0:
+                savedOffset = offs
+            zeroCount = zeroCount + chunkLen
+        else:
+            if zeroCount != 0:
+                print(f'b += b"\\x00"*{zeroCount} # {savedOffset:08X}')
+                zeroCount = 0
+                savedOffset = 0
+            print(f"b += {bHex} # {offs:08X} {bAsc}")
+        offs = offs + 16
+    if zeroCount != 0:
+        print(f'b += b"\\x00"*{zeroCount} # {savedOffset:08X}')
+    print(yc.template2)
+
+def genShellcode(inBytes):
+    """
+    Create a C shellcode loader.
+
+    This function performs turns a buffer of binary data into a
+    C-style shellcode loader.
+
+    Parameters
+    ----------
+    inBytes : bytes
+        The hex text buffer to work with.
+    """
+    print(yc.cTemplate1)
+    print("char code[] = ",end="")
+    scBuff, scAsc, scSum = hexString(inBytes)
+    print(f"{scBuff};")
+    print(yc.cTemplate2)
+
+def reverseDump(inText):
+    """
+    Reverse hex dump.
+
+    This function performs a reverse hex dump from a text buffer.
+    It can detect both xxd and yxd style hex dumps and convert them
+    back into binary buffers.
+
+    Parameters
+    ----------
+    inText : bytes
+        The hex text buffer to work with.
+    """
+    hexBuf = b""
+    if inText[0] == 0x1b:
+        # yxd Ansi output
+        linebuf = inText.decode("utf-8")
+        lines = linebuf.split("\n")
+        for l in lines:
+            try:
+                l = l.split("│")
+                l = l[1] + l[2]
+                l = l.replace(" ", "")
+                escAnsi = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+                l = escAnsi.sub('', l)
+                l = bytes.fromhex(l)
+                hexBuf += l
+            except:
+                break
+    elif inText[0:8] == b"00000000":
+        # xxd style output
+        linebuf = inText.decode("latin-1")
+        lines = linebuf.split("\n")
+        for l in lines:
+            try:
+                l = l.split("  ") # Shave off the last part
+                l.pop()
+                l = l[0].split(": ")
+                l = l[1]
+                l = l.replace(" ", "")
+                l = bytes.fromhex(l)
+                hexBuf += l
+            except:
+                break
+    else:
+        print("yxd.reverseDump: Wrong Format!")
+    sys.stdout.buffer.write(hexBuf)
+    return
+
+class yxd:
+    """
+    A class to represent a buffer of binary data.
+
+    The yxd class creates a reusable object that can access the features of the yxd library.
+
+    ...
+
+    Attributes
+    ----------
+    binData : bytes
+        a buffer of binary data
+    amount : int
+        the amount of data to output, 0 = all
+    baseAddr : int
+        the base address of the binary data
+    outFormat : str
+        output format, xx, xxd, yxd, python, shellcode, ps etc
+    color : str
+        the color scheme from the config options
+    blockSize : int
+        how many bytes per line
+    offset : int
+        offset within the buffer to do the hexdump from
+    quiet : bool
+        whether to dump to the terminal or not
+
+    Methods
+    -------
+    dump():
+        do hex dump on the binary data with configured settings
+    genPythonScript():
+        generate a python script that creates a copy of your binary data
+    genShellcode():
+        create a C shellcode loader from binary data
+    reverseDump():
+        do a reverse hex dump
+    """
+    def __init__(self, binData, amount=0, baseAddr=0, outFormat="xxd", color="default", blockSize=16, offset=0,  quiet=False ):
+        self.binData = binData
+        self.dataLen = len(binData)
+        self.baseAddr = baseAddr
+        self.outFormat = outFormat
+        self.color = color
+        self.offset = offset
+        self.blockSize = blockSize
+        self.amount = amount
+        self.quiet = quiet
+        if quiet != True:
+            self.dump()
+    def styleDump(self):
+        for i in range(0,256):
+            print(f"{yc.bytez[i]}{i:02X}{yc.EOA} ",end="")
+            if (i+1) % 16 == 0:
+                if i == 0:
+                    continue
+                else:
+                    print()
+    def dump(self):
+        return dump(self.binData,self.baseAddr,self.dataLen,self.blockSize,self.outFormat)
+    def genPythonScript(self):
+        genPythonScript(self.binData)
+    def genShellcode(self):
+        genShellcode(self.binData)
+    def reverseDump(self):
+        reverseDump(self.binData)
+
+def main():
+    args = parser.parse_args()
+
+    if args.printVersion:
+        print(versionInfo)
+        sys.exit(0)
+    if args.dumpStyle:
+        styleDump()
+        sys.exit(0)
+
+    hexStyle = "yxd"
+    if args.plainText:
+        hexStyle = "xxd"
+    if args.xxFormat:
+        hexStyle = "xx"
+    if args.psFormat:
+        hexStyle = "ps"
+
+    bufferSize  = args.bufferSize  if args.bufferSize else 0
+    startOffset = args.startOffset if args.startOffset else 0
+    if args.inFile == None:
+        args.inFile = args.input
+    if args.inFile and args.inFile != "-":
+        inFile = args.inFile
+        with open(inFile,"rb") as f:
+            f.seek(startOffset)
+            binData = f.read(bufferSize) if bufferSize !=0 else f.read()
+            binSize = len(binData)
+    else:
+        binData = sys.stdin.buffer.read()
+        binSize = len(binData)
+
+    yxdd = yxd(binData, baseAddr=startOffset, outFormat=hexStyle, quiet=True)
+    if args.genPythonScript:
+        yxdd.genPythonScript()
+    elif args.genShellcode:
+        yxdd.genShellcode()
+    elif args.reverseDump:
+        yxdd.reverseDump()
+    else:
+        yxdd.dump()
+
+if __name__ == '__main__':
+    main()
