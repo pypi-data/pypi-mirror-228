302fd4b4c52fad70d79c8ed02ff88ae3ba60e5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:13:25 +0000

    Add package element-python-package/0.1.1/element_python_package-0.1.1.tar.gz

---
+++ packages/element-python-package/element_python_package-0.1.1.tar.gz/element_python_package-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='element_python_package',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        # add any third-party packages that your package depends on here
+    ],
+    author='Sharadind Peddiraju',
+    author_email='sharadind.pv@gmail.com',
+    description='A simple example package',
+)
