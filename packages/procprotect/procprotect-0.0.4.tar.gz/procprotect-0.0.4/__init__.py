85ca3d30ec4069e4ec3963ef9ce24343b827cb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:11:41 +0000

    Add package procprotect/0.0.4/procprotect-0.0.4.tar.gz

---
+++ packages/procprotect/procprotect-0.0.4.tar.gz/procprotect-0.0.4/__init__.py
@@ -0,0 +1,7 @@
+from procprotect import *
+from procprotect.debugCheck import debugChecks_main
+from procprotect.vmCheck import vmChecks_main
+
+# lists
+from procprotect.lists.default_vmCheck import *
+from procprotect.lists.default_debugCheck import *
