8c1219f37a85ef69ff9f8943877823631f6a8853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:13:16 +0000

    Add package PyQt6-Material-Widgets/0.9.10/PyQt6_Material_Widgets-0.9.10-py3-none-any.whl

---
+++ packages/PyQt6-Material-Widgets/PyQt6_Material_Widgets-0.9.10-py3-none-any.whl/qmaterialwidgets/__init__.py
@@ -0,0 +1,20 @@
+"""
+PyQt6-Material-Widgets
+========================
+A material design widgets library based on PyQt6.
+
+Documentation is available in the docstrings and
+online at https://qmaterialwidgets.readthedocs.io.
+
+Examples are available at https://github.com/zhiyiYo/QMaterialWidgets/tree/PyQt6/examples.
+
+:copyright: (c) 2023 by zhiyiYo.
+:license: PyQt6-Material-Widgets is licensed under [GPLv3](./LICENSE).
+"""
+
+__version__ = "0.9.10"
+
+from .components import *
+from .common import *
+from .window import *
+from ._rc import resource
