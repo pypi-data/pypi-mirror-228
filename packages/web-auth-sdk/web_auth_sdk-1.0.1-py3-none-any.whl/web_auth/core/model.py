c0d51fceb73fd2c757a97e4464f1735fc537d703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:11:41 +0000

    Add package web-auth-sdk/1.0.1/web_auth_sdk-1.0.1-py3-none-any.whl

---
+++ packages/web-auth-sdk/web_auth_sdk-1.0.1-py3-none-any.whl/web_auth/core/model.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import pydantic
+
+
+@dataclass
+class PermissionModel:
+    bitmask_idx: int
+    codename: str
+    name: Optional[str]
+    service: Optional[str]
+
+
+class ErrorMessageModel(pydantic.BaseModel):
+    code: str
+    message: str
+
+
+class Consumer(pydantic.BaseModel):
+    """Authenticated client base class that developers can derive its attribute and declare it as a parameter
+    in view functions to retrieve consumer information.
+    """
+
+
+class JWTConsumer(Consumer):
+    """Authenticated client data structure by JWT"""
+
+    user_id: int
+    permission_bitmask: str
+    iat: int
+    exp: int
