6a56407b387cd5357be7a044a66dce1ded6a30b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:11:10 +0000

    Add package g3/0.5.0/g3-0.5.0.tar.gz

---
+++ packages/g3/g3-0.5.0.tar.gz/g3-0.5.0/g3/services/generate/pr/prompts/template.py
@@ -0,0 +1,15 @@
+pr_template = """
+"Use the following format in your response:
+# Title
+
+[The title of the pull request]
+
+## Description
+
+[A short description of the changes made in 1 to 4 sentences.]
+
+## Changes
+
+[Bulleted list of changes made. Don't exceed 5 items. Each item should start with -]
+
+"""
