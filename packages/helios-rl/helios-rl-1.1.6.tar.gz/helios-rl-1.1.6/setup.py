3bc827d91830036e9717be7d10321415ee240b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:13:09 +0000

    Add package helios-rl/1.1.6/helios-rl-1.1.6.tar.gz

---
+++ packages/helios-rl/helios-rl-1.1.6.tar.gz/helios-rl-1.1.6/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name='helios-rl',
+    version='1.1.6',
+    packages=[
+        'helios_rl', 
+        'helios_rl.adapters', 
+        'helios_rl.agents',
+        'helios_rl.benchmark',
+        'helios_rl.encoders', 
+        'helios_rl.environment_setup', 
+        'helios_rl.evaluation', 
+        'helios_rl.experiments'
+        ],
+    # TODO: Add benchmark to exclusion of wheel
+    url='https://github.com/pdfosborne/HELIOS-RL',
+    license='GNU Public License v3',
+    author='Philip Osborne',
+    author_email='pdfosborne@gmail.com',
+    description='Applying the HELIOS architecture to Reinforcement Learning problems.',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scipy>=1.10.1',
+        'torch',
+        'sentence-transformers'
+    ] 
+)
