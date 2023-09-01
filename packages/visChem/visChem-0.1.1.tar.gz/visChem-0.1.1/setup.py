cceaef2647815665f401467c33323b7b348fa1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 15:07:57 +0000

    Add package visChem/0.1.1/visChem-0.1.1.tar.gz

---
+++ packages/visChem/visChem-0.1.1.tar.gz/visChem-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='visChem',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        line.strip() for line in open('requirements.txt').readlines()
+    ],
+    author='Steve Niu',
+    author_email='stevexniu@gmail.com',
+    description='A tool for chemical space visualization and clustering.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
---
+++ packages/visChem/visChem-0.1.1.tar.gz/visChem-0.1.1/visChem/__init__.py
@@ -0,0 +1,3 @@
+from .core import get_smiles, smiles_to_mol, mol_to_fp, fp_to_np
+from .utils import compute_tanimoto, tanimoto_similarity, reduce_dimensionality, perform_clustering
+from .visualization import plot_chemical_structures, mol_to_image, repel_positions
