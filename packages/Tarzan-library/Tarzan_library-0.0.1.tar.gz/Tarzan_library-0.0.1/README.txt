975456bd258fb5d71fb5021dc121a9cebfe4c752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:08:07 +0000

    Add package Tarzan-library/0.0.1/Tarzan_library-0.0.1.tar.gz

---
+++ packages/Tarzan-library/Tarzan_library-0.0.1.tar.gz/Tarzan_library-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a simple basic mathematics operations
---
+++ packages/Tarzan-library/Tarzan_library-0.0.1.tar.gz/Tarzan_library-0.0.1/__init__.py
@@ -0,0 +1,11 @@
+def add_nums(num1, num2):
+    return num1+num2
+
+def sub_nums(num1, num2):
+    return num1-num2
+
+def mul_nums(num1, num2):
+    return num1*num2
+
+def div_nums(num1, num2):
+    return num1/num2
