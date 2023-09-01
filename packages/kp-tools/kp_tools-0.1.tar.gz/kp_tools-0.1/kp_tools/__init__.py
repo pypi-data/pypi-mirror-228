6e96c33f869892d61d2cffa3ba1055ec3f2027b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:54 +0000

    Add package kp-tools/0.1/kp_tools-0.1.tar.gz

---
+++ packages/kp-tools/kp_tools-0.1.tar.gz/kp_tools-0.1/kp_tools/__init__.py
@@ -0,0 +1,16 @@
+import sys
+
+# Basic Information
+__name__ = "kp_tools"
+__version__ = "0.1"
+__summary__ = "Kocher Pipeline Tools"
+__url__ = "https://github.com/kocherlab/kocher_pipeline_tools"
+__code__ = "https://github.com/kocherlab/kocher_pipeline_tools"
+__issue__ = "https://github.com/kocherlab/kocher_pipeline_tools/issues"
+__docs__ = ""
+__license__ = "MIT"
+__copyright__ = "2023"
+
+# Author Information
+__authors__ = "Andrew Webb and Sarah Kocher"
+__email__ = "19213578+aewebb80@users.noreply.github.com"
