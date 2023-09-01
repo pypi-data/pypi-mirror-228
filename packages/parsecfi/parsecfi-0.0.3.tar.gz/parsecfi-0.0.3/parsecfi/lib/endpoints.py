1b9e9d33c2b5262f3f250f641b3b16ebfe2e1731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:13:25 +0000

    Add package parsecfi/0.0.3/parsecfi-0.0.3.tar.gz

---
+++ packages/parsecfi/parsecfi-0.0.3.tar.gz/parsecfi-0.0.3/parsecfi/lib/endpoints.py
@@ -0,0 +1,18 @@
+from parsecfi.lib.endpoints_list import (
+	lending_market, 
+	contract_logs, 
+	token_holders, 
+	token_acc, 
+	trending_contracts,
+	transfers
+)
+
+def load():
+	return {
+		'lending_market': lending_market,
+		'contract_logs': contract_logs,
+		'token_holders': token_holders,
+		'token_acc': token_acc,
+		'trending_contracts': trending_contracts,
+		'transfers': transfers
+	}
---
+++ packages/parsecfi/parsecfi-0.0.3.tar.gz/parsecfi-0.0.3/parsecfi/lib/endpoints_list/contract_logs.py
@@ -0,0 +1,37 @@
+import json
+import pandas as pd
+
+query_string = """
+	query ContractLogs(
+		$address: String!, 
+		$chain: String!, 
+		$log: String!,
+		$since: Int,
+		$limit: Int
+	) {
+	  contract(address: $address, chain: $chain) {
+	  	logs(limit: $limit, log: $log, since: $since) {
+	      timestamp
+	      txId
+	      data
+	    }
+	  }
+	}
+"""
+
+def query(variables):
+	return {
+		'query': query_string,
+		'variables': variables
+	}
+
+def parser(json_data):
+	data = json_data['contract']['logs']
+
+	for item in data:
+		d = json.loads(item['data'])
+		for key, val in d.items():
+			item[key] = val
+		del item['data']
+
+	return pd.DataFrame(data)
