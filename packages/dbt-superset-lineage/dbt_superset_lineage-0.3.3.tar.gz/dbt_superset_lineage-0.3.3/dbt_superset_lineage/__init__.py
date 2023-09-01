3a14f20d831df947c442d54e69bea12bce739bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:15:10 +0000

    Add package dbt-superset-lineage/0.3.3/dbt_superset_lineage-0.3.3.tar.gz

---
+++ packages/dbt-superset-lineage/dbt_superset_lineage-0.3.3.tar.gz/dbt_superset_lineage-0.3.3/dbt_superset_lineage/__init__.py
@@ -0,0 +1,64 @@
+import typer
+from .pull_dashboards import main as pull_dashboards_main
+from .push_descriptions import main as push_descriptions_main
+
+app = typer.Typer()
+
+
+@app.command()
+def pull_dashboards(dbt_project_dir: str = typer.Option('.', help="Directory path to dbt project."),
+                    exposures_path: str = typer.Option('/models/exposures/superset_dashboards.yml',
+                                                       help="Where within PROJECT_DIR the exposure file should "
+                                                            "be stored. If you set this to go outside /models, it then "
+                                                            "needs to be added to source-paths in dbt_project.yml."),
+                    dbt_db_name: str = typer.Option(None, help="Name of your database within dbt towards which "
+                                                               "the pull should be reduced to run."),
+                    superset_url: str = typer.Argument(..., help="URL of your Superset, e.g. "
+                                                                 "https://mysuperset.mycompany.com"),
+                    superset_db_id: int = typer.Option(None, help="ID of your database within Superset towards which "
+                                                                  "the pull should be reduced to run."),
+                    sql_dialect: str = typer.Option('ansi', help="Database SQL dialect; used for parsing queries. "
+                                                                 "Consult docs of SQLFluff for details: "
+                                                                 "https://docs.sqlfluff.com/en/stable/dialects.html"),
+                    superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN",
+                                                              help="Access token to Superset API. "
+                                                                   "Can be automatically generated if "
+                                                                   "SUPERSET_REFRESH_TOKEN is provided."),
+                    superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN",
+                                                               help="Refresh token to Superset API.")):
+
+    pull_dashboards_main(dbt_project_dir, exposures_path, dbt_db_name,
+                         superset_url, superset_db_id, sql_dialect,
+                         superset_access_token, superset_refresh_token)
+
+
+@app.command()
+def push_descriptions(dbt_project_dir: str = typer.Option('.', help="Directory path to dbt project."),
+                      dbt_db_name: str = typer.Option(None, help="Name of your database within dbt to which the script "
+                                                                 "should be reduced to run."),
+                      superset_url: str = typer.Argument(..., help="URL of your Superset, e.g. "
+                                                                   "https://mysuperset.mycompany.com"),
+                      superset_db_id: int = typer.Option(None, help="ID of your database within Superset towards which "
+                                                                    "the push should be reduced to run."),
+                      superset_refresh_columns: bool = typer.Option(False, help="Whether columns in Superset should be "
+                                                                                "refreshed from database before "
+                                                                                "the push."),
+                      superset_pause_after_update: int = typer.Option(None, help="Number of seconds for which the "
+                                                                                 "script pauses after any update of "
+                                                                                 "Superset columns. This is to allow "
+                                                                                 "databases to catch up in "
+                                                                                 "the meantime."),
+                      superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN",
+                                                                help="Access token to Superset API."
+                                                                     "Can be automatically generated if "
+                                                                     "SUPERSET_REFRESH_TOKEN is provided."),
+                      superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN",
+                                                                 help="Refresh token to Superset API.")):
+
+    push_descriptions_main(dbt_project_dir, dbt_db_name,
+                           superset_url, superset_db_id, superset_refresh_columns, superset_pause_after_update,
+                           superset_access_token, superset_refresh_token)
+
+
+if __name__ == '__main__':
+    app()
---
+++ packages/dbt-superset-lineage/dbt_superset_lineage-0.3.3.tar.gz/dbt_superset_lineage-0.3.3/dbt_superset_lineage/push_descriptions.py
@@ -0,0 +1,271 @@
+import json
+import logging
+import re
+import time
+
+from bs4 import BeautifulSoup
+from markdown import markdown
+from requests import HTTPError
+
+from .superset_api import Superset
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+def get_datasets_from_superset(superset, superset_db_id):
+    logging.info("Getting physical datasets from Superset.")
+
+    page_number = 0
+    datasets = []
+    datasets_keys = set()
+    while True:
+        logging.info("Getting page %d.", page_number + 1)
+
+        payload = {
+            'q': json.dumps({
+                'page': page_number,
+                'page_size': 100
+            })
+        }
+        res = superset.request('GET', '/dataset/', params=payload)
+
+        result = res['result']
+        if result:
+            for r in result:
+                kind = r['kind']
+                database_id = r['database']['id']
+
+                if kind == 'physical' \
+                        and (superset_db_id is None or database_id == superset_db_id):
+
+                    dataset_id = r['id']
+
+                    name = r['table_name']
+                    schema = r['schema']
+                    dataset_key = f'{schema}.{name}'  # used as unique identifier
+
+                    dataset_dict = {
+                        'id': dataset_id,
+                        'key': dataset_key
+                    }
+
+                    # fail if it breaks uniqueness constraint
+                    assert dataset_key not in datasets_keys, \
+                        f"Dataset {dataset_key} is a duplicate name (schema + table) " \
+                        "across databases. " \
+                        "This would result in incorrect matching between Superset and dbt. " \
+                        "To fix this, remove duplicates or add the ``superset_db_id`` argument."
+
+                    datasets_keys.add(dataset_key)
+                    datasets.append(dataset_dict)
+            page_number += 1
+        else:
+            break
+
+    assert datasets, "There are no datasets in Superset!"
+
+    return datasets
+
+
+def get_tables_from_dbt(dbt_manifest, dbt_db_name):
+    tables = {}
+    for table_type in ['nodes', 'sources']:
+        manifest_subset = dbt_manifest[table_type]
+
+        for table_key_long in manifest_subset:
+            table = manifest_subset[table_key_long]
+            name = table['name']
+            schema = table['schema']
+            database = table['database']
+
+            table_key_short = schema + '.' + name
+            columns = table['columns']
+            description = table['description']
+
+            if dbt_db_name is None or database == dbt_db_name:
+                # fail if it breaks uniqueness constraint
+                assert table_key_short not in tables, \
+                    f"Table {table_key_short} is a duplicate name (schema + table) " \
+                    f"across databases. " \
+                    "This would result in incorrect matching between Superset and dbt. " \
+                    "To fix this, remove duplicates or add the ``dbt_db_name`` argument."
+
+                tables[table_key_short] = {'columns': columns, 'description': description}
+
+    assert tables, "Manifest is empty!"
+
+    return tables
+
+
+def refresh_columns_in_superset(superset, dataset_id):
+    logging.info("Refreshing columns in Superset.")
+    superset.request('PUT', f'/dataset/{dataset_id}/refresh')
+
+
+def add_superset_columns(superset, dataset):
+    logging.info("Pulling fresh columns info from Superset.")
+
+    res = superset.request('GET', f"/dataset/{dataset['id']}")
+    result = res['result']
+
+    dataset['columns'] = result['columns']
+    dataset['description'] = result['description']
+    dataset['owners'] = result['owners']
+
+    return dataset
+
+
+def convert_markdown_to_plain_text(md_string):
+    """Converts a markdown string to plaintext.
+
+    The following solution is used:
+    https://gist.github.com/lorey/eb15a7f3338f959a78cc3661fbc255fe
+    """
+
+    # md -> html -> text since BeautifulSoup can extract text cleanly
+    html = markdown(md_string)
+
+    # remove code snippets
+    html = re.sub(r'<pre>(.*?)</pre>', ' ', html)
+    html = re.sub(r'<code>(.*?)</code >', ' ', html)
+
+    # extract text
+    soup = BeautifulSoup(html, 'html.parser')
+    text = ''.join(soup.findAll(text=True))
+
+    # make one line
+    single_line = re.sub(r'\s+', ' ', text)
+
+    # make fixes
+    single_line = re.sub('→', '->', single_line)
+    single_line = re.sub('<null>', '"null"', single_line)
+
+    return single_line
+
+
+def merge_columns_info(dataset, tables):
+    logging.info("Merging columns info from Superset and manifest.json file.")
+
+    key = dataset['key']
+    sst_columns = dataset['columns']
+    dbt_columns = tables.get(key, {}).get('columns', {})
+
+    sst_description = dataset['description']
+    dbt_description = tables.get(key, {}).get('description')
+
+    sst_owners = dataset['owners']
+
+    columns_new = []
+    for sst_column in sst_columns:
+
+        column_name = sst_column['column_name']
+
+        # add the mandatory fields
+        column_new = {
+            'column_name': column_name,
+            'id': sst_column['id']
+        }
+
+        # add column descriptions
+        if column_name in dbt_columns \
+                and 'description' in dbt_columns[column_name] \
+                and (sst_column['expression'] is None  # database columns
+                     or sst_column['expression'] == ''):
+            description = dbt_columns[column_name]['description']
+            description = convert_markdown_to_plain_text(description)
+        else:
+            description = sst_column['description']
+        column_new['description'] = description
+
+        columns_new.append(column_new)
+
+    dataset['columns_new'] = columns_new
+
+    # add dataset description
+    if dbt_description is None:
+        dataset['description_new'] = sst_description
+    else:
+        dataset['description_new'] = convert_markdown_to_plain_text(dbt_description)
+
+    # add dataset owner IDs (otherwise Superset empties the owners list)
+    dataset['owners_new'] = [owner['id'] for owner in sst_owners]
+
+    return dataset
+
+
+def check_columns_equal(lst1, lst2):
+    return sorted(lst1, key=lambda c: c["id"]) == sorted(lst2, key=lambda c: c["id"])
+
+
+def pause_after_update(superset_pause_after_update):
+    if superset_pause_after_update:
+        logging.info("Pausing the script for %d seconds to allow for databases to catch up with the update.",
+                     superset_pause_after_update)
+        time.sleep(superset_pause_after_update)
+        logging.info("Resuming the script again.")
+
+
+def put_descriptions_to_superset(superset, dataset, superset_pause_after_update):
+    logging.info("Putting model and column descriptions into Superset.")
+
+    description_new = dataset['description_new']
+    columns_new = dataset['columns_new']
+    owners_new = dataset['owners_new']
+
+    description_old = dataset['description']
+    columns_old = [{
+        'column_name': col['column_name'],
+        'id': col['id'],
+        'description': col['description']
+    } for col in dataset['columns']]
+
+    if description_new != description_old or \
+       not check_columns_equal(columns_new, columns_old):
+        payload = {'description': description_new, 'columns': columns_new, 'owners': owners_new}
+        superset.request('PUT', f"/dataset/{dataset['id']}?override_columns=false", json=payload)
+        pause_after_update(superset_pause_after_update)
+    else:
+        logging.info("Skipping PUT execute request as nothing would be updated.")
+
+
+def main(dbt_project_dir, dbt_db_name,
+         superset_url, superset_db_id, superset_refresh_columns, superset_pause_after_update,
+         superset_access_token, superset_refresh_token):
+
+    # require at least one token for Superset
+    assert superset_access_token is not None or superset_refresh_token is not None, \
+           "Add ``SUPERSET_ACCESS_TOKEN`` or ``SUPERSET_REFRESH_TOKEN`` " \
+           "to your environment variables or provide in CLI " \
+           "via ``superset-access-token`` or ``superset-refresh-token``."
+
+    superset = Superset(superset_url + '/api/v1',
+                        access_token=superset_access_token, refresh_token=superset_refresh_token)
+
+    logging.info("Starting the script!")
+
+    sst_datasets = get_datasets_from_superset(superset, superset_db_id)
+    logging.info("There are %d physical datasets in Superset overall.", len(sst_datasets))
+
+    with open(f'{dbt_project_dir}/target/manifest.json') as f:
+        dbt_manifest = json.load(f)
+
+    dbt_tables = get_tables_from_dbt(dbt_manifest, dbt_db_name)
+
+    sst_datasets_dbt_filtered = [d for d in sst_datasets if d["key"] in dbt_tables]
+    logging.info("There are %d physical datasets in Superset with a match in dbt.", len(sst_datasets_dbt_filtered))
+
+    for i, sst_dataset in enumerate(sst_datasets_dbt_filtered):
+        logging.info("Processing dataset %d/%d.", i + 1, len(sst_datasets_dbt_filtered))
+        sst_dataset_id = sst_dataset['id']
+        try:
+            if superset_refresh_columns:
+                refresh_columns_in_superset(superset, sst_dataset_id)
+                pause_after_update(superset_pause_after_update)
+            sst_dataset_w_cols = add_superset_columns(superset, sst_dataset)
+            sst_dataset_w_cols_new = merge_columns_info(sst_dataset_w_cols, dbt_tables)
+            put_descriptions_to_superset(superset, sst_dataset_w_cols_new, superset_pause_after_update)
+        except HTTPError as e:
+            logging.error("The dataset with ID=%d wasn't updated. Check the error below.",
+                          sst_dataset_id, exc_info=e)
+
+    logging.info("All done!")
