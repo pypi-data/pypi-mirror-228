e2a8337c1a131945bed7d9c83a1554410c203f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:14:04 +0000

    Add package dalec/0.2.3/dalec-0.2.3.tar.gz

---
+++ packages/dalec/dalec-0.2.3.tar.gz/dalec-0.2.3/dalec/static/dalec/js/main.js
@@ -0,0 +1,37 @@
+
+
+function dalec_fetch_content(container){
+    const data = container.dataset ;
+    container.classList.add("dalec-loading");
+    if (data.channelObjects !== undefined){
+        data.channelObjects = JSON.parse(data.channelObjects)
+    }
+    fetch(
+        data.url,
+        {
+            method: "POST",
+            headers: {
+              'Accept': 'text/html',
+              'Content-Type': 'application/json'
+            },
+            // cache: "no-cache",
+            body: JSON.stringify(
+                {"channelObjects": data.channelObjects, "orderedBy": data.orderedBy}
+            ),
+            keepalive: true,
+        }
+    ).then(function(response){
+        if (!response.ok) {
+            console.error(`HTTP error ${response.status} while fetching ${data.url}`);
+            return ;
+        }
+        if (response.status === 204) {
+            container.classList.remove("dalec-loading");
+            return ;
+        }
+        response.text().then(function (html) {
+            container.innerHTML = html ;
+            container.classList.remove("dalec-loading");
+        });
+    });
+}
