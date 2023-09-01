43e60a4ce534f77bef62c4f9c895542989e5de8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:13:06 +0000

    Add package qontract-reconcile/0.10.1rc279/qontract_reconcile-0.10.1rc279-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc279-py3-none-any.whl/reconcile/ocm/types.py
@@ -0,0 +1,140 @@
+from __future__ import annotations
+
+from typing import (
+    Optional,
+    Union,
+)
+
+from pydantic import (
+    BaseModel,
+    Extra,
+    Field,
+)
+
+
+class OCMClusterAutoscale(BaseModel):
+    min_replicas: int
+    max_replicas: int
+
+
+class OCMClusterNetwork(BaseModel):
+    type: Optional[str]
+    vpc: str
+    service: str
+    pod: str
+
+
+class OCMClusterSpec(BaseModel):
+    autoscale: Optional[OCMClusterAutoscale]
+    channel: str
+    disable_user_workload_monitoring: Optional[bool]
+    external_id: Optional[str]
+    id: Optional[str]
+    instance_type: Optional[str]
+    multi_az: Optional[bool]
+    nodes: Optional[int]
+    private: bool
+    product: str
+    provider: str
+    provision_shard_id: Optional[str]
+    region: str
+    initial_version: Optional[str]
+    version: str
+    hypershift: Optional[bool]
+
+    class Config:
+        extra = Extra.forbid
+
+
+class OSDClusterSpec(OCMClusterSpec):
+    load_balancers: int
+    storage: int
+
+    class Config:
+        extra = Extra.forbid
+
+
+class ROSAOcmAwsStsAttrs(BaseModel):
+    installer_role_arn: str
+    support_role_arn: str
+    controlplane_role_arn: Optional[str]
+    worker_role_arn: str
+
+    class Config:
+        extra = Extra.forbid
+
+
+class ROSAOcmAwsAttrs(BaseModel):
+    creator_role_arn: str
+    sts: Optional[ROSAOcmAwsStsAttrs]
+
+    class Config:
+        extra = Extra.forbid
+
+
+class ROSAClusterAWSAccount(BaseModel):
+    uid: str
+    rosa: ROSAOcmAwsAttrs
+
+    class Config:
+        extra = Extra.forbid
+
+
+class ROSAClusterSpec(OCMClusterSpec):
+    account: ROSAClusterAWSAccount
+    subnet_ids: Optional[list[str]]
+    availability_zones: Optional[list[str]]
+
+    class Config:
+        extra = Extra.forbid
+
+
+class OCMSpec(BaseModel):
+    path: Optional[str]
+    spec: Union[OSDClusterSpec, ROSAClusterSpec, OCMClusterSpec]
+    network: OCMClusterNetwork
+    domain: Optional[str]
+    server_url: str = Field("", alias="serverUrl")
+    console_url: str = Field("", alias="consoleUrl")
+    elb_fqdn: str = Field("", alias="elbFQDN")
+
+    class Config:
+        smart_union = True
+        # This is need to populate by either console_url or consoleUrl, for instance
+        allow_population_by_field_name = True
+
+
+class OCMOidcIdp(BaseModel):
+    id: Optional[str] = None
+    cluster: str
+    name: str
+    client_id: str
+    client_secret: Optional[str] = None
+    issuer: str
+    email_claims: list[str]
+    name_claims: list[str]
+    username_claims: list[str]
+    groups_claims: list[str]
+    mapping_method: str = "add"
+
+    def __lt__(self, other: OCMOidcIdp) -> bool:
+        return self.cluster < other.cluster
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, OCMOidcIdp):
+            raise NotImplementedError("Cannot compare to non OCMOidcIdp objects.")
+        return self.cluster == other.cluster and self.name == other.name
+
+    def __hash__(self) -> int:
+        return hash(self.cluster + self.name + self.client_id)
+
+    def differ(self, other: OCMOidcIdp) -> bool:
+        return (
+            self.client_id != other.client_id
+            or self.issuer != other.issuer
+            or self.email_claims != other.email_claims
+            or self.username_claims != other.username_claims
+            or self.name_claims != other.name_claims
+            or self.groups_claims != other.groups_claims
+            or self.mapping_method != other.mapping_method
+        )
---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc279-py3-none-any.whl/reconcile/rhidp/common.py
@@ -0,0 +1,184 @@
+from collections import (
+    Counter,
+    defaultdict,
+)
+from collections.abc import Callable
+from enum import Enum
+from typing import (
+    Iterable,
+    Optional,
+    Sequence,
+)
+
+from reconcile.gql_definitions.fragments.ocm_environment import OCMEnvironment
+from reconcile.gql_definitions.fragments.vault_secret import VaultSecret
+from reconcile.gql_definitions.rhidp.clusters import (
+    ClusterAuthOIDCV1,
+    ClusterV1,
+    OpenShiftClusterManagerV1,
+)
+from reconcile.gql_definitions.rhidp.clusters import query as cluster_query
+from reconcile.rhidp.metrics import RhIdpClusterCounter
+from reconcile.utils.disabled_integrations import integration_is_enabled
+from reconcile.utils.metrics import MetricsContainer
+from reconcile.utils.ocm.clusters import (
+    ClusterDetails,
+    discover_clusters_by_labels,
+)
+from reconcile.utils.ocm.labels import subscription_label_filter
+from reconcile.utils.ocm.sre_capability_labels import sre_capability_label_key
+from reconcile.utils.ocm_base_client import OCMBaseClient
+
+# Generates label keys for rhidp, compliant with the naming schema defined in
+# https://service.pages.redhat.com/dev-guidelines/docs/sre-capabilities/framework/ocm-labels/
+RHIDP_LABEL_KEY = sre_capability_label_key("rhidp")
+
+
+class RhidpLabelValue(Enum):
+    ENABLED = "enabled"
+    DISABLED = "disabled"
+
+
+def discover_clusters(
+    ocm_api: OCMBaseClient,
+    org_ids: Optional[set[str]] = None,
+    label_value: RhidpLabelValue = RhidpLabelValue.ENABLED,
+) -> dict[str, list[ClusterDetails]]:
+    """Discover all clusters that are part of the RHIDP service."""
+    clusters = discover_clusters_by_labels(
+        ocm_api=ocm_api,
+        label_filter=subscription_label_filter()
+        .eq("key", RHIDP_LABEL_KEY)
+        .eq("value", label_value.value),
+    )
+
+    # group by org and filter if org_id is specified
+    clusters_by_org: dict[str, list[ClusterDetails]] = defaultdict(list)
+    for c in clusters:
+        passed_ocm_filters = org_ids is None or c.organization_id in org_ids
+        if passed_ocm_filters:
+            clusters_by_org[c.organization_id].append(c)
+
+    return clusters_by_org
+
+
+def build_cluster_auths(name: str, issuer_url: str) -> list[ClusterAuthOIDCV1]:
+    return [
+        ClusterAuthOIDCV1(
+            service="oidc",
+            name=name,
+            issuer=issuer_url,
+            # stick with the defaults
+            claims=None,
+        )
+    ]
+
+
+def build_cluster_obj(
+    ocm_env: OCMEnvironment, cluster: ClusterDetails, auth: Sequence[ClusterAuthOIDCV1]
+) -> ClusterV1:
+    return ClusterV1(
+        name=cluster.ocm_cluster.name,
+        consoleUrl=cluster.ocm_cluster.console.url
+        if cluster.ocm_cluster.console
+        else "",
+        ocm=OpenShiftClusterManagerV1(
+            name=cluster.organization_id,
+            environment=ocm_env,
+            orgId=cluster.organization_id,
+            # unused values
+            accessTokenClientId=None,
+            accessTokenClientSecret=None,
+            accessTokenUrl=None,
+            blockedVersions=None,
+            sectors=None,
+        ),
+        auth=auth,
+        # unused values
+        upgradePolicy=None,
+        disable=None,
+    )
+
+
+def get_clusters(
+    integration_name: str,
+    query_func: Callable,
+    default_issuer_url: str,
+    exclude_clusters_without_ocm: bool = True,
+) -> list[ClusterV1]:
+    """Get all clusters from AppInterface."""
+    data = cluster_query(query_func, variables={})
+    clusters: list[ClusterV1] = []
+
+    for c in data.clusters or []:
+        if not c.console_url:
+            # without a console url we can't calculate the redirect url ... skip it for a moment
+            continue
+        if not integration_is_enabled(integration_name, c):
+            # integration disabled for this particular cluster
+            continue
+        if c.ocm is None and exclude_clusters_without_ocm:
+            # no ocm relation
+            continue
+        if not [auth for auth in c.auth if isinstance(auth, ClusterAuthOIDCV1)]:
+            # no OIDC auth
+            continue
+        for auth in c.auth:
+            if isinstance(auth, ClusterAuthOIDCV1) and not auth.issuer:
+                auth.issuer = default_issuer_url
+        clusters.append(c)
+    return clusters
+
+
+def cluster_vault_secret_id(org_id: str, cluster_name: str, auth_name: str) -> str:
+    """Returns the vault secret id for the given cluster."""
+    return f"{cluster_name}-{org_id}-{auth_name}"
+
+
+def cluster_vault_secret(
+    vault_input_path: str,
+    org_id: Optional[str] = None,
+    cluster_name: Optional[str] = None,
+    auth_name: Optional[str] = None,
+    vault_secret_id: Optional[str] = None,
+) -> VaultSecret:
+    """Returns the vault secret path for the given cluster."""
+    if not vault_secret_id and (org_id and cluster_name and auth_name):
+        cid = cluster_vault_secret_id(org_id, cluster_name, auth_name)
+    elif vault_secret_id:
+        cid = vault_secret_id
+    else:
+        raise ValueError(
+            "vault_secret_id or org_id, cluster_name and auth_name must be provided"
+        )
+    return VaultSecret(
+        path=f"{vault_input_path.rstrip('/')}/{cid}",
+        field="",
+        version=None,
+        format=None,
+    )
+
+
+def expose_base_metrics(
+    metrics_container: MetricsContainer,
+    integration_name: str,
+    ocm_environment: str,
+    clusters: Iterable[ClusterV1],
+) -> None:
+    clusters_per_org: Counter[str] = Counter()
+    for cluster in clusters:
+        if cluster.ocm:
+            clusters_per_org[cluster.ocm.org_id] += 1
+        else:
+            clusters_per_org["unknown-org-id"] += 1
+
+    # clusters per org counter
+    for org_id, count in clusters_per_org.items():
+        metrics_container.set_gauge(
+            RhIdpClusterCounter(
+                integration=integration_name,
+                ocm_environment=ocm_environment,
+                org_id=org_id,
+            ),
+            value=count,
+        )
