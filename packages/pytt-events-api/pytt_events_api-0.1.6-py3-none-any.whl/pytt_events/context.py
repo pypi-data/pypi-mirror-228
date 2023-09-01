488bf8e8cbd5b7ae87547374b775724c850dc323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:50 +0000

    Add package pytt-events-api/0.1.6/pytt_events_api-0.1.6-py3-none-any.whl

---
+++ packages/pytt-events-api/pytt_events_api-0.1.6-py3-none-any.whl/pytt_events/context.py
@@ -0,0 +1,102 @@
+from typing import Optional, List, Dict, Any
+import phonenumbers
+import ipaddress
+from pydantic import BaseModel, ValidationError, validator, root_validator, \
+    AnyUrl, constr, EmailStr
+
+
+class ContextFormatError(Exception):
+    def __init__(self, value, message) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(value, message)
+
+
+class Ad(BaseModel):
+    # ttclid
+    callback: Optional[constr(
+        strip_whitespace=True, max_length=501
+    )]
+
+    @validator('callback', pre=True)
+    @classmethod
+    def callback_is_valid(cls, value):
+        """
+        Check if the callback value is a valid ttclid.
+        """
+        if value:
+            ttclid_error = ContextFormatError(
+                value=value,
+                message="Callback must be a valid ttclid please check "
+                        "TikTok's documentation for more information on "
+                        "ttclid: "
+                        "https://ads.tiktok.com/marketing_api/docs?id"
+                        "=1739584860883969"
+            )
+
+            value_list = value.split('.')
+
+            if value[:6] != 'E.C.P.':
+                raise ttclid_error
+
+            if len(value_list) != 4:
+                raise ttclid_error
+
+            if any(len(value) != 1 for value in value_list[:3]):
+                raise ttclid_error
+
+            if len(value_list[3]) <= 1:
+                raise ttclid_error
+
+        return value
+
+
+class Page(BaseModel):
+    url: AnyUrl
+    referrer: Optional[AnyUrl]
+
+
+class User(BaseModel):
+    external_id: Optional[constr(min_length=1, strip_whitespace=True)]
+    email: Optional[EmailStr]
+    phone_number: Optional[str]
+    ttp: Optional[constr(min_length=1, strip_whitespace=True)]
+
+    @validator('phone_number')
+    @classmethod
+    def validate_phone_number(cls, field_value):
+        if field_value:
+            number = phonenumbers.parse(field_value, None)
+            phonenumbers.format_number(number,
+                                       phonenumbers.PhoneNumberFormat.E164)
+
+            is_possible = phonenumbers.is_possible_number(number)
+
+            if not phonenumbers.is_possible_number(number):
+                raise ContextFormatError(
+                    value=field_value,
+                    message='Must be a valid phone number: +{country code}{'
+                            'local code}{phone number} / +001199999999'
+                )
+
+        return field_value
+
+    @root_validator(pre=True)
+    @classmethod
+    def validate_user(cls, values):
+        if not any(values.values()):
+            raise ContextFormatError(
+                value=values,
+                message='User must have at least one of the following: '
+                        'external_id, email, phone_number, ttp'
+            )
+        return values
+
+
+# Context object parameters
+class Context(BaseModel):
+    user_agent: constr(min_length=1)  # Client user agent
+    ip: ipaddress.IPv4Address  # User IP address
+    ad: Optional[Ad]  # ttclid
+    page: Page
+    user: User
---
+++ packages/pytt-events-api/pytt_events_api-0.1.6-py3-none-any.whl/pytt_events/properties.py
@@ -0,0 +1,43 @@
+from pydantic import (
+    BaseModel,
+    ValidationError,
+    conlist,
+    constr,
+    PositiveFloat,
+    PositiveInt
+)
+from typing import Optional
+from enum import Enum
+
+class ContentType(str, Enum):
+    """
+    For individual products.
+    """
+    PRODUCT = 'product'
+    """
+    For product groups (Items and variants).
+    """
+    PRODUCT_GROUP = 'product_group'
+
+    def __str__(self):
+        return self.value
+
+class Content(BaseModel):
+    content_id: constr(strip_whitespace=True, min_length=1)
+    quantity: PositiveInt
+    price: PositiveFloat
+    content_category: constr(min_length=1)
+    content_name: constr(min_length=1)
+
+class Properties(BaseModel):
+    currency: Optional[constr(strip_whitespace=True, min_length=3, max_length=3)]
+    value: Optional[PositiveFloat]
+    description: Optional[constr(min_length=1)]
+    query: Optional[constr(min_length=1)]
+    status: Optional[constr(min_length=1)]
+    content_type: ContentType
+    contents: Optional[conlist(
+        item_type=Content, unique_items=True, min_items=1
+    )]
+
+
