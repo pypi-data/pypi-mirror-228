86ae31cacb67edc617244b0f9152c30b42dccb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 15:09:11 +0000

    Add package jacksung/0.0.2.3/jacksung-0.0.2.3-py3-none-any.whl

---
+++ packages/jacksung/jacksung-0.0.2.3-py3-none-any.whl/jacksung/utils/hash.py
@@ -0,0 +1,28 @@
+import hashlib
+
+
+def calculate_file_hash(file_path, hash_algorithm="md5", chunk_size=4096):
+    hash_obj = hashlib.new(hash_algorithm)
+    with open(file_path, "rb") as file:
+        while True:
+            data = file.read(chunk_size)
+            if not data:
+                break
+            hash_obj.update(data)
+    return hash_obj.hexdigest()
+
+
+def hash_files(file_paths, hash_algorithm="md5"):
+    hash_list = []
+    for filepath in file_paths:
+        hash_list.append(calculate_file_hash(filepath, hash_algorithm))
+    return hash_list_of_hashes(hash_list, hash_algorithm)
+
+
+def hash_list_of_hashes(hash_list, hash_algorithm="md5"):
+    hash_obj = hashlib.new(hash_algorithm)
+    # 将哈希值列表连接成一个字符串
+    hash_str = ''.join(hash_list)
+    # 对连接后的字符串进行哈希
+    hash_obj.update(hash_str.encode())
+    return hash_obj.hexdigest()
