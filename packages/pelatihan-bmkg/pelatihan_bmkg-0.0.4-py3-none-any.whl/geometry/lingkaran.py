6e80a92a6808c5f47de0104c016455c407a4ac5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:08:13 +0000

    Add package pelatihan-bmkg/0.0.4/pelatihan_bmkg-0.0.4-py3-none-any.whl

---
+++ packages/pelatihan-bmkg/pelatihan_bmkg-0.0.4-py3-none-any.whl/geometry/lingkaran.py
@@ -0,0 +1,12 @@
+class Lingkaran:
+    def __init__(self, radius):
+        self.radius = radius  # Variabel objek
+    
+    def hitung_luas(self):
+        return 3.14 * self.radius * self.radius
+    
+    def hitung_keliling(self):
+        return 2 * 3.14 * self.radius 
+    
+    def menghitung_luas():
+        return "ini fungsi menghitung luas"
