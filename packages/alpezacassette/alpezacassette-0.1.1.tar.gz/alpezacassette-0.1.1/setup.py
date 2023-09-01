47aee94a00171319fe19b9302a0994220a0cf735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:13:38 +0000

    Add package alpezacassette/0.1.1/alpezacassette-0.1.1.tar.gz

---
+++ packages/alpezacassette/alpezacassette-0.1.1.tar.gz/alpezacassette-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+install_requires = ""
+
+
+def getRequirements():
+    with open("./requirements.txt", "r", encoding="utf-8") as requirements_file:
+        install_requires = [line.strip()
+                            for line in requirements_file if line.strip()]
+
+
+setup(
+    name="alpezacassette",
+    version="0.1.1",
+    description="Renderizador de dialogos .fountain a audio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="alpeza",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    url="https://github.com/alpeza/cassette",
+    entry_points={
+        "console_scripts": [
+            "cassette = cassette.main:main",
+        ],
+    },
+    package_data={
+        "": ["utils/.env"],
+    },
+)
