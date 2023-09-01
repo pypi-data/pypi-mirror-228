dc04ce17a75cdf25ea47a54ea83a55b2bc94d86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 15:09:08 +0000

    Add package jsonly/2.0.2/jsonly-2.0.2-py3-none-any.whl

---
+++ packages/jsonly/jsonly-2.0.2-py3-none-any.whl/jsonly/error.py
@@ -0,0 +1,22 @@
+class PathException(Exception):
+    pass
+
+
+class GetExcetion(Exception):
+    pass
+
+
+class UpdateExcetion(Exception):
+    pass
+
+
+class ConvertTypeExcetion(Exception):
+    pass
+
+
+class ConverException(Exception):
+    pass
+
+
+class SaveException(Exception):
+    pass
