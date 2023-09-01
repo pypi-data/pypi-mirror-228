0d5f9fb4316462f5a623b0aa5b792277c7eda466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:07:49 +0000

    Add package automation-editor/0.0.25/automation_editor-0.0.25-py3-none-any.whl

---
+++ packages/automation-editor/automation_editor-0.0.25-py3-none-any.whl/automation_editor/extend/process_executor/load_density/load_density_process.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Union
+
+from je_editor import EditorWidget
+
+from automation_editor.extend.process_executor.process_executor_utils import build_process
+
+if TYPE_CHECKING:
+    from automation_editor.automation_editor_ui.editor_main.main_ui import AutomationEditor
+import json
+import sys
+
+from automation_editor.automation_editor_ui.show_code_window.code_window import CodeWindow
+from automation_editor.extend.mail_thunder_extend.mail_thunder_setting import send_after_test
+from automation_editor.extend.process_executor.task_process_manager import TaskProcessManager
+from automation_editor.utils.exception.exception_tags import wrong_test_data_format_exception_tag
+from automation_editor.utils.exception.exceptions import ITETestExecutorException
+from automation_editor.utils.file_process.get_dir_file_list import ask_and_get_dir_files_as_list
+
+
+def call_load_density(
+        main_window: AutomationEditor,
+        exec_str: Union[str, None] = None,
+        program_buffer: int = 1024000
+):
+    build_process(main_window, "je_load_density", exec_str, False, program_buffer)
+
+
+def call_load_density_with_send(
+        main_window: AutomationEditor,
+        exec_str: Union[str, None] = None,
+        program_buffer: int = 1024000
+):
+    build_process(main_window, "je_load_density", exec_str, True, program_buffer)
+
+
+def call_load_density_multi_file(
+        main_window: AutomationEditor,
+        program_buffer: int = 1024000
+):
+    try:
+        need_to_execute_list: list = ask_and_get_dir_files_as_list(main_window)
+        if need_to_execute_list is not None and isinstance(need_to_execute_list, list) and len(
+                need_to_execute_list) > 0:
+            for execute_file in need_to_execute_list:
+                with open(execute_file, "r+") as test_script_json:
+                    call_load_density(
+                        main_window,
+                        test_script_json.read(),
+                        program_buffer
+                    )
+    except Exception as error:
+        print(repr(error), file=sys.stderr)
+
+
+def call_load_density_multi_file_and_send(
+        main_window: AutomationEditor,
+        program_buffer: int = 1024000
+):
+    try:
+
+        need_to_execute_list: list = ask_and_get_dir_files_as_list(main_window)
+        if need_to_execute_list is not None and isinstance(need_to_execute_list, list) and len(
+                need_to_execute_list) > 0:
+            for execute_file in need_to_execute_list:
+                with open(execute_file, "r+") as test_script_json:
+                    call_load_density_with_send(
+                        main_window,
+                        test_script_json.read(),
+                        program_buffer
+                    )
+    except Exception as error:
+        print(repr(error), file=sys.stderr)
