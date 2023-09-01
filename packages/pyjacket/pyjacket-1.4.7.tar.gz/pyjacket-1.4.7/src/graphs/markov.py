7e6f8f56afc472336c62bcd5681e776b603f599d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:34 +0000

    Add package pyjacket/1.4.7/pyjacket-1.4.7.tar.gz

---
+++ packages/pyjacket/pyjacket-1.4.7.tar.gz/pyjacket-1.4.7/src/graphs/markov.py
@@ -0,0 +1,8 @@
+
+
+"""Consider a graph between with n observables (nodes) and k connections
+
+We are given the values of the nodes in time
+
+we want to figure out the rate constants of each connection
+"""
