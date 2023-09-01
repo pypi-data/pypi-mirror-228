e217dceea5c27b20d729007da638f3f048febcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:08:31 +0000

    Add package sqlmesh/0.29.1/sqlmesh-0.29.1-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.29.1-py3-none-any.whl/web/server/api/endpoints/environments.py
@@ -0,0 +1,34 @@
+import typing as t
+
+from fastapi import APIRouter, Depends
+
+from sqlmesh.core import constants as c
+from sqlmesh.core.context import Context
+from sqlmesh.core.environment import Environment
+from web.server.exceptions import ApiException
+from web.server.settings import get_loaded_context
+
+router = APIRouter()
+
+
+@router.get("")
+async def get_environments(
+    context: Context = Depends(get_loaded_context),
+) -> t.Dict[str, Environment]:
+    """Get the environments"""
+    try:
+        environments = {env.name: env for env in context.state_reader.get_environments()}
+    except Exception:
+        raise ApiException(
+            message="Unable to get environments",
+            origin="API -> environments -> get_environments",
+        )
+
+    if c.PROD not in environments:
+        environments[c.PROD] = Environment(
+            name=c.PROD,
+            snapshots=[],
+            start_at=c.EPOCH,
+            plan_id="",
+        )
+    return environments
