b55b2392c1d5d1b19c2d7c5355e761130c73f309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:11:19 +0000

    Add package retryer/1.0.2/retryer-1.0.2.tar.gz

---
+++ packages/retryer/retryer-1.0.2.tar.gz/retryer-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+setup(
+    name='retryer',
+    version='1.0.2',
+    description='A retry decorator for error handling',
+    author='Vishwanath Kannan',
+    packages=['retryer'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'time',
+        'functools'
+    ],
+)
