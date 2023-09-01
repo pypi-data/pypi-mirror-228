adb09826a7eb092c9af18704d08ef6f88b948e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 06:13:24 +0000

    Add package lexset/4.1.7/lexset-4.1.7-py3-none-any.whl

---
+++ packages/lexset/lexset-4.1.7-py3-none-any.whl/lexset/test_ssim.py
@@ -0,0 +1,8 @@
+from review import analysis
+
+dir1 = "D:/github/coco_analysis/5869/"
+dir2 = "D:/github/medtronic/real_img/"
+
+# Create an instance of the 'analysis' class
+sample_data = analysis(dir1)
+sample_data.compare_ssim_distributions(compare_dir=dir2,target_size=(256, 256))
