1299889e278c5b54224bdd73232435a098e45c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:11:10 +0000

    Add package dlt/0.3.15/dlt-0.3.15.tar.gz

---
+++ packages/dlt/dlt-0.3.15.tar.gz/dlt-0.3.15/dlt/common/configuration/providers/airflow.py
@@ -0,0 +1,19 @@
+from .toml import VaultTomlProvider
+
+
+class AirflowSecretsTomlProvider(VaultTomlProvider):
+    def __init__(self, only_secrets: bool = False, only_toml_fragments: bool = False) -> None:
+        super().__init__(only_secrets, only_toml_fragments)
+
+    @property
+    def name(self) -> str:
+        return 'Airflow Secrets TOML Provider'
+
+    def _look_vault(self, full_key: str, hint: type) -> str:
+        """Get Airflow Variable with given `full_key`, return None if not found"""
+        from airflow.models import Variable
+        return Variable.get(full_key, default_var=None)  # type: ignore
+
+    @property
+    def supports_secrets(self) -> bool:
+        return True
