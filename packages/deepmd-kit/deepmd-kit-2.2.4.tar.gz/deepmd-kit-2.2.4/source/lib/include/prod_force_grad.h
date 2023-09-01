6c55b55d5353235acc8871a5227147915f64b7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 06:14:12 +0000

    Add package deepmd-kit/2.2.4/deepmd-kit-2.2.4.tar.gz

---
+++ packages/deepmd-kit/deepmd-kit-2.2.4.tar.gz/deepmd-kit-2.2.4/source/lib/include/prod_force_grad.h
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
+
+namespace deepmd {
+
+template <typename FPTYPE>
+void prod_force_grad_a_cpu(FPTYPE* grad_net,
+                           const FPTYPE* grad,
+                           const FPTYPE* env_deriv,
+                           const int* nlist,
+                           const int nloc,
+                           const int nnei,
+                           const int nframes);
+
+template <typename FPTYPE>
+void prod_force_grad_r_cpu(FPTYPE* grad_net,
+                           const FPTYPE* grad,
+                           const FPTYPE* env_deriv,
+                           const int* nlist,
+                           const int nloc,
+                           const int nnei,
+                           const int nframes);
+
+#if GOOGLE_CUDA
+template <typename FPTYPE>
+void prod_force_grad_a_gpu_cuda(FPTYPE* grad_net,
+                                const FPTYPE* grad,
+                                const FPTYPE* env_deriv,
+                                const int* nlist,
+                                const int nloc,
+                                const int nnei,
+                                const int nframes);
+
+template <typename FPTYPE>
+void prod_force_grad_r_gpu_cuda(FPTYPE* grad_net,
+                                const FPTYPE* grad,
+                                const FPTYPE* env_deriv,
+                                const int* nlist,
+                                const int nloc,
+                                const int nnei,
+                                const int nframes);
+#endif  // GOOGLE_CUDA
+
+#if TENSORFLOW_USE_ROCM
+template <typename FPTYPE>
+void prod_force_grad_a_gpu_rocm(FPTYPE* grad_net,
+                                const FPTYPE* grad,
+                                const FPTYPE* env_deriv,
+                                const int* nlist,
+                                const int nloc,
+                                const int nnei,
+                                const int nframes);
+
+template <typename FPTYPE>
+void prod_force_grad_r_gpu_rocm(FPTYPE* grad_net,
+                                const FPTYPE* grad,
+                                const FPTYPE* env_deriv,
+                                const int* nlist,
+                                const int nloc,
+                                const int nnei,
+                                const int nframes);
+#endif  // TENSORFLOW_USE_ROCM
+}  // namespace deepmd
