a18030c19e1efc5a2af9f6467d7af8f4b3b2aa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:15:39 +0000

    Add package ghostcoder/0.0.2/ghostcoder-0.0.2.tar.gz

---
+++ packages/ghostcoder/ghostcoder-0.0.2.tar.gz/ghostcoder-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ghostcoder",
+    version="0.0.2",
+    packages=find_packages(),
+    #entry_points={
+    #    'console_scripts': [
+    #        'app=ghostcoder.main:run',
+    #    ],
+    #},
+    #install_requires=[
+    #    'fastapi', 'uvicorn'
+    #],
+)
