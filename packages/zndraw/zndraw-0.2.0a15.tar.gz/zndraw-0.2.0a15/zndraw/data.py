0800b6a977e4ff6e7fd050f98eeec41d0a9ef0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:12:34 +0000

    Add package zndraw/0.2.0a15/zndraw-0.2.0a15.tar.gz

---
+++ packages/zndraw/zndraw-0.2.0a15.tar.gz/zndraw-0.2.0a15/zndraw/data.py
@@ -0,0 +1,170 @@
+import pathlib
+import typing
+
+import ase.io
+import networkx as nx
+import numpy as np
+import tqdm
+import znh5md
+from ase.calculators.singlepoint import SinglePointCalculator
+from ase.data.colors import jmol_colors
+from ase.neighborlist import natural_cutoffs
+from networkx.exception import NetworkXError
+from pydantic import BaseModel, Field
+
+
+def _rgb2hex(value):
+    r, g, b = np.array(value * 255, dtype=int)
+    return "#%02x%02x%02x" % (r, g, b)
+
+
+def _get_radius(value):
+    return (0.25 * (2 - np.exp(-0.2 * value)),)
+
+
+class ASEComputeBonds(BaseModel):
+    single_bond_multiplier: float = Field(1.2, le=2, ge=0)
+    double_bond_multiplier: float = Field(0.9, le=1, ge=0)
+    triple_bond_multiplier: float = Field(0.0, le=1, ge=0)
+
+    def build_graph(self, atoms: ase.Atoms):
+        cutoffs = [
+            self.single_bond_multiplier,
+            self.double_bond_multiplier,
+            self.triple_bond_multiplier,
+        ]
+        connectivity_matrix = np.zeros((len(atoms), len(atoms)), dtype=int)
+        atoms.pbc = False
+        distance_matrix = atoms.get_all_distances(mic=False)
+        np.fill_diagonal(distance_matrix, np.inf)
+        for cutoff in cutoffs:
+            cutoffs = np.array(natural_cutoffs(atoms, mult=cutoff))
+            cutoffs = cutoffs[:, None] + cutoffs[None, :]
+            connectivity_matrix[distance_matrix <= cutoffs] += 1
+        G = nx.from_numpy_array(connectivity_matrix)
+        return G
+
+    def update_graph_using_modifications(self, atoms: ase.Atoms):
+        modifications = atoms.info.get("modifications", {})
+        graph = atoms.connectivity
+        for key in modifications:
+            atom_1, atom_2 = key
+            weight = modifications[key]
+            if weight == 0:
+                self.remove_edge(graph, atom_1, atom_2)
+            else:
+                graph.add_edge(atom_1, atom_2, weight=weight)
+
+    @staticmethod
+    def remove_edge(graph, atom_1, atom_2):
+        try:
+            graph.remove_edge(atom_1, atom_2)
+        except NetworkXError:
+            pass
+
+    def get_bonds(self, atoms: ase.Atoms):
+        graph = atoms.connectivity
+        bonds = []
+        for edge in graph.edges:
+            bonds.append((edge[0], edge[1], graph.edges[edge]["weight"]))
+        return bonds
+
+    def update_bond_order(self, atoms: ase.Atoms, particles: list[int], order: int):
+        if len(particles) != 2:
+            raise ValueError("Exactly two particles must be selected")
+        modifications = atoms.info.get("modifications", {})
+        sorted_particles = tuple(sorted(particles))
+        modifications[sorted_particles] = order
+        atoms.info["modifications"] = modifications
+
+
+def get_atomsdict_list(filename, stride) -> typing.Generator[typing.Dict, None, None]:
+    """Read atoms from file and return a list of atoms dicts.
+
+    Parameters
+    ----------
+    filename : str
+        Path to the file which should be read.
+    stride : int
+        Stride for the frames to be visualized. If set to 1, all frames will be visualized.
+    """
+
+    frame_idx = 0
+
+    if pathlib.Path(filename).suffix == ".h5":
+        # Read file using znh5md and convert to list[ase.Atoms]
+        atoms_list = znh5md.ASEH5MD(filename).get_atoms_list()
+        for idx, atoms in tqdm.tqdm(
+            enumerate(atoms_list), ncols=100, total=len(atoms_list)
+        ):
+            if idx % stride == 0:
+                yield {frame_idx: atoms_to_json(atoms)}
+                frame_idx += 1
+    else:
+        for idx, atoms in tqdm.tqdm(enumerate(ase.io.iread(filename)), ncols=100):
+            if idx % stride == 0:
+                yield {frame_idx: atoms_to_json(atoms)}
+                frame_idx += 1
+
+
+def atoms_to_json(atoms: ase.Atoms) -> dict:
+    ase_bond_calculator = ASEComputeBonds()
+    atoms.connectivity = ase_bond_calculator.build_graph(atoms)
+
+    atoms_dict = atoms.todict()
+    for key in list(atoms_dict):
+        # includes ['numbers', 'positions', 'cell', 'pbc']
+        if isinstance(atoms_dict[key], np.ndarray):
+            atoms_dict[key] = atoms_dict[key].tolist()
+        elif isinstance(atoms_dict[key], np.generic):
+            atoms_dict[key] = atoms_dict[key].item()
+
+    # remove info if available # currently not used
+    atoms_dict.pop("info", None)
+
+    atoms_dict["colors"] = [
+        _rgb2hex(jmol_colors[number]) for number in atoms_dict["numbers"]
+    ]
+    atoms_dict["radii"] = [_get_radius(number) for number in atoms_dict["numbers"]]
+
+    try:
+        calc_data = {}
+        for key in atoms.calc.results:
+            value = atoms.calc.results[key]
+            if isinstance(value, np.ndarray):
+                value = value.tolist()
+            calc_data[key] = value
+
+        atoms_dict["calc"] = calc_data
+    except (RuntimeError, AttributeError):
+        pass
+
+    try:
+        atoms_dict["connectivity"] = ase_bond_calculator.get_bonds(atoms)
+    except AttributeError:
+        atoms_dict["connectivity"] = []
+
+    return atoms_dict
+
+
+def atoms_from_json(data: dict) -> ase.Atoms:
+    atoms = ase.Atoms(
+        numbers=data["numbers"],
+        cell=data["cell"],
+        pbc=True,
+        positions=data["positions"],
+    )
+
+    if "calc" in data:
+        atoms.calc = SinglePointCalculator(atoms)
+        atoms.calc.results = {
+            key: np.array(val) if isinstance(val, list) else val
+            for key, val in data["calc"].items()
+        }
+
+    if "connectivity" in data:
+        atoms.connectivity = nx.Graph()
+        for edge in data["connectivity"]:
+            atoms.connectivity.add_edge(edge[0], edge[1], weight=edge[2])
+
+    return atoms
---
+++ packages/zndraw/zndraw-0.2.0a15.tar.gz/zndraw-0.2.0a15/zndraw/static/main.js
@@ -0,0 +1,127 @@
+import { Cache } from "./pycom/Cache.js";
+import { World } from "./World/World.js";
+import { setUIEvents } from "./UI/UI.js";
+import { initJSONEditor } from "./UI/json_editor.js";
+
+function setupSocket() {
+  const socket = io();
+  socket.on("connect", () => {
+    socket.emit("connection", { data: "I'm connected!" });
+  });
+  return socket;
+}
+
+function setupUpload(socket) {
+  const upload_form = document.getElementById("uploadForm");
+  const upload_input = document.getElementById("fileInput");
+  const submit_button = document.getElementById("uploadBtn");
+
+  const file = {
+    dom: document.getElementById("fileInput"),
+    binary: null,
+  };
+
+  const reader = new FileReader();
+
+  // Because FileReader is asynchronous, store its
+  // result when it finishes reading the file
+  reader.addEventListener("load", () => {
+    file.binary = reader.result;
+    socket.emit("upload", {
+      content: reader.result,
+      filename: file.dom.files[0].name,
+    });
+  });
+
+  // At page load, if a file is already selected, read it.
+  if (file.dom.files[0]) {
+    reader.readAsBinaryString(file.dom.files[0]);
+  }
+
+  // If not, read the file once the user selects it.
+  file.dom.addEventListener("change", () => {
+    if (reader.readyState === FileReader.LOADING) {
+      reader.abort();
+    }
+
+    reader.readAsBinaryString(file.dom.files[0]);
+  });
+}
+
+function setupSlider(socket, world) {}
+
+function main() {
+  const socket = setupSocket();
+  const cache = new Cache(socket);
+  const container = document.querySelector("#scene-container");
+  const world = new World(container, cache, socket);
+
+  setUIEvents(socket, world);
+  initJSONEditor(socket, cache, world);
+  world.start();
+
+  // socket dicsconnect event
+  socket.on("disconnect", () => {
+    window.close();
+  });
+
+  document.getElementById("ExitBtn").addEventListener("click", () => {
+    fetch("/exit", { method: "GET" });
+  });
+
+  document.getElementById("downloadBtn").addEventListener("click", () => {
+    socket.emit("download", { atoms_list: cache.getAllAtoms() }, (data) => {
+      const blob = new Blob([data], { type: "text/csv" });
+      const elem = window.document.createElement("a");
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = "trajectory.xyz";
+      document.body.appendChild(elem);
+      elem.click();
+      document.body.removeChild(elem);
+    });
+  });
+
+  document
+    .getElementById("downloadSelectedBtn")
+    .addEventListener("click", () => {
+      socket.emit(
+        "download",
+        {
+          atoms_list: cache.getAllAtoms(),
+          selection: world.selection.selection,
+        },
+        (data) => {
+          const blob = new Blob([data], { type: "text/csv" });
+          const elem = window.document.createElement("a");
+          elem.href = window.URL.createObjectURL(blob);
+          elem.download = "trajectory.xyz";
+          document.body.appendChild(elem);
+          elem.click();
+          document.body.removeChild(elem);
+        },
+      );
+    });
+
+  const helpBtn = document.getElementById("HelpBtn");
+
+  helpBtn.addEventListener("mouseover", () => {
+    new bootstrap.Collapse(document.getElementById("helpBoxCollapse"), {
+      toggle: false,
+    }).show();
+  });
+  helpBtn.addEventListener("mouseout", () => {
+    new bootstrap.Collapse(document.getElementById("helpBoxCollapse"), {
+      toggle: false,
+    }).hide();
+  });
+
+  socket.emit("atoms:request", null, () => {
+    world.setStep(0);
+    // disable loading screen
+    document.getElementById("atom-spinner").style.display = "none";
+  });
+
+  setupUpload(socket);
+}
+
+main();
