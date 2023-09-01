5aee92cd448ad68d5bd5531f05274becead0c8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 15:08:13 +0000

    Add package polymaps/0.0.108/polymaps-0.0.108.tar.gz

---
+++ packages/polymaps/polymaps-0.0.108.tar.gz/polymaps-0.0.108/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='polymaps',
+    version='0.0.108',
+    description='A simple Python package to preprocess and postprocess LAMMPS simulations for polymers and small molecules',
+    url='https://gitlab.com/doublylinkedlist/polymaps/',
+    author='Xiaoli Yan',
+    author_email='xyan11@uic.edu',
+    license='MIT',
+    packages=['polymaps'],
+    install_requires=['jupyter==1.0.0',
+                      'jupyterlab==4.0.4',
+                      'numpy==1.25.0',
+                      'scipy==1.11.1',
+                      'pandas==2.0.2',
+                      'matplotlib==3.7.1',
+                      'plotly==5.15.0',
+                      'scikit-learn==1.3.0'
+                      ],
+
+    
+)
+
