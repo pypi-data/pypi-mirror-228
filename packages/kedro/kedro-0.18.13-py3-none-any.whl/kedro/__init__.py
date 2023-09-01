2199d5c1cd59d6db247346a3b298f1f95b3184b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:11:22 +0000

    Add package kedro/0.18.13/kedro-0.18.13-py3-none-any.whl

---
+++ packages/kedro/kedro-0.18.13-py3-none-any.whl/kedro/__init__.py
@@ -0,0 +1,28 @@
+"""Kedro is a framework that makes it easy to build robust and scalable
+data pipelines by providing uniform project templates, data abstraction,
+configuration and pipeline assembly.
+"""
+
+import sys
+import warnings
+
+__version__ = "0.18.13"
+
+
+class KedroPythonVersionWarning(UserWarning):
+    """Custom class for warnings about incompatibilities with Python versions."""
+
+    pass
+
+
+if not sys.warnoptions:
+    warnings.simplefilter("error", KedroPythonVersionWarning)
+
+if sys.version_info >= (3, 12):
+    warnings.warn(
+        """Kedro is not yet fully compatible with this Python version.
+To proceed at your own risk and ignore this warning,
+run Kedro with `python -W "default:Kedro is not yet fully compatible" -m kedro ...`
+or set the PYTHONWARNINGS environment variable accordingly.""",
+        KedroPythonVersionWarning,
+    )
