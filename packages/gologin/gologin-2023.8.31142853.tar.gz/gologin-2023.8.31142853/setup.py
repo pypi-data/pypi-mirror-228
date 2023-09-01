167be1bf8ae8c9a62cb9877982c621af86562502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:12:37 +0000

    Add package gologin/2023.8.31142853/gologin-2023.8.31142853.tar.gz

---
+++ packages/gologin/gologin-2023.8.31142853.tar.gz/gologin-2023.8.31142853/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gologin',
+    version='2023.08.31142853',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'psutil'
+    ],
+    author='GoLogin',
+    author_email='yuri@gologin.com',
+    description='Official GoLogin python package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gologinapp/pygologin',
+    python_requires='>=3.5'
+)
