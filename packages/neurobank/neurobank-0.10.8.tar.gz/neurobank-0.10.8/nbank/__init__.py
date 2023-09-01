f652e4fb51030712a83d269d30102a2f23b917da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:12:38 +0000

    Add package neurobank/0.10.8/neurobank-0.10.8.tar.gz

---
+++ packages/neurobank/neurobank-0.10.8.tar.gz/neurobank-0.10.8/nbank/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""neurobank data management system
+
+Copyright (C) 2013--2023 Dan Meliza <dan@meliza.org>
+"""
+
+__version__ = "0.10.8"
+
+from nbank.core import deposit, describe, fetch, find, get, search, verify
+
+# Variables:
+# End:
