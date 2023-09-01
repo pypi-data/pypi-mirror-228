ecfce654104ed4ff6389a461cf36599a5a26e3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:15:07 +0000

    Add package django-delayed-notifications/0.7.3/django_delayed_notifications-0.7.3-py3-none-any.whl

---
+++ packages/django-delayed-notifications/django_delayed_notifications-0.7.3-py3-none-any.whl/django_notifications/__init__.py
@@ -0,0 +1,11 @@
+"""
+Application to manage notifications.
+
+:copyright: (c) 2022 by François GUÉRIN
+:license: mit, see LICENSE for more details.
+:creationdate: 06/01/2022 11:31
+:moduleauthor: François GUÉRIN <fguerin@ville-tourcoing.fr>
+:modulename: django_notifications
+
+"""
+__version__ = VERSION = "0.7.3"
