9f044455e38d27b4b8ea49de52532fe1ef21b738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:12:43 +0000

    Add package streamlit-table-update-create-scenario/1.0.0/streamlit-table-update-create-scenario-1.0.0.tar.gz

---
+++ packages/streamlit-table-update-create-scenario/streamlit-table-update-create-scenario-1.0.0.tar.gz/streamlit-table-update-create-scenario-1.0.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+ 
+
+setuptools.setup(
+
+    name="streamlit-table-update-create-scenario",
+
+    version="1.0.0",
+
+    author="",
+
+    author_email="",
+
+    description="",
+
+    long_description="",
+
+    long_description_content_type="text/plain",
+
+    url="",
+
+    packages=setuptools.find_packages(),
+
+    include_package_data=True,
+
+    classifiers=[],
+
+    python_requires=">=3.6",
+
+    install_requires=[
+
+        "streamlit >= 0.66",
+
+    ],
+
+)
