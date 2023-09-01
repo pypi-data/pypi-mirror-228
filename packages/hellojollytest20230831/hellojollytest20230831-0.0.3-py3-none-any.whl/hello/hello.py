73c8eb5644c8a0084f5415eed415b7a3bf84ddc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:11:50 +0000

    Add package hellojollytest20230831/0.0.3/hellojollytest20230831-0.0.3-py3-none-any.whl

---
+++ packages/hellojollytest20230831/hellojollytest20230831-0.0.3-py3-none-any.whl/hello/hello.py
@@ -0,0 +1,17 @@
+#hello 模块
+
+def say_hello():
+    print("Hello~")
+
+def say_hi():
+    print("Hi~")
+
+class C:
+    def __init__(self,name):
+        self.name = name
+    def sayC(self):
+        print("我的类是C")
+
+c1 = C("Jolly")
+
+
