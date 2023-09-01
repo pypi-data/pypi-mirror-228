ce352c4badc9a006ec6c5cc692d2b492f3b93586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:12:14 +0000

    Add package ailp/0.3/ailp-0.3.tar.gz

---
+++ packages/ailp/ailp-0.3.tar.gz/ailp-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ailp',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[],  # Put any dependencies here
+    author="Aswin v v",
+    author_email="aswinjayakrishnan9423@gmail.com",
+)
