9bf67a88ffd4949590986cc6188d1fa818950cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:08:17 +0000

    Add package element-python-package/0.2.3/element_python_package-0.2.3.tar.gz

---
+++ packages/element-python-package/element_python_package-0.2.3.tar.gz/element_python_package-0.2.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='element_python_package',
+    version='0.2.3',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'seaborn',
+        'matplotlib',
+    ],
+    author='Sharadind Peddiraju',
+    author_email='sharadind.pv@gmail.com',
+    description='A simple example package',
+)
