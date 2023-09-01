9ca6373726b719106844699c4bac14770bccbf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:07:48 +0000

    Add package funcyou/1.0.5/funcyou-1.0.5.tar.gz

---
+++ packages/funcyou/funcyou-1.0.5.tar.gz/funcyou-1.0.5/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='funcyou',
+    version='1.0.5',
+    author='Tikendra sahu',
+    author_email='tikendraksahu1029@gmail.com',
+    description='useful function to save time',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'sklearn',
+        'pandas',
+        'numpy',
+        'kaggle'
+    ],
+)
