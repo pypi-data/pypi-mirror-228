62bd4cc4048b936594195b81d4d319cc2952d2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:15:39 +0000

    Add package spinesTS/0.1.5/spinesTS-0.1.5.tar.gz

---
+++ packages/spinesTS/spinesTS-0.1.5.tar.gz/spinesTS-0.1.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import Extension, dist, find_packages, setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+    name='spinesTS',
+    version="0.1.5",
+    description='spinesTS, a powerful timeseries toolsets.',
+    keywords='computer vision',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    url='https://github.com/BirchKwok/spinesTS',
+    author='Birch Kwok',
+    author_email='birchkwok@gmail.com',
+    install_requires=[
+        'scikit-learn>=1.0.2',
+        'torch>=1.4',
+        'scipy>=1.7.0',
+        'numpy>=1.17.0',
+        'pandas>=1.0.0',
+        'tabulate>=0.8',
+        'matplotlib>=3.5.1'
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
