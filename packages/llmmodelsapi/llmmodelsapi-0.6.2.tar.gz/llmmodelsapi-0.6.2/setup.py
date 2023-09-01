3506bbd7b8e28549143b2429508c2f55203ddee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 15:07:51 +0000

    Add package llmmodelsapi/0.6.2/llmmodelsapi-0.6.2.tar.gz

---
+++ packages/llmmodelsapi/llmmodelsapi-0.6.2.tar.gz/llmmodelsapi-0.6.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'llmmodelsapi',
+  packages = ['llmmodelsapi'],
+  version = '0.6.2',
+  license='MIT',
+  description = 'SDK for interfacing with core models hosted by LLM Solutions',
+  long_description_content_type="text/markdown",
+  author = 'Matthieu Moullec',
+  author_email = 'matthieu.moullec.perso@gmail.com',
+  url = 'https://github.com/llm-solutions/LLM-API-Express-SDK-Python',
+  download_url = 'https://github.com/llm-solutions/LLM-API-Express-SDK-Python/archive/refs/tags/v0.6.2.tar.gz',
+  keywords = ['Large Language Model', 'LLM', 'API'],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
