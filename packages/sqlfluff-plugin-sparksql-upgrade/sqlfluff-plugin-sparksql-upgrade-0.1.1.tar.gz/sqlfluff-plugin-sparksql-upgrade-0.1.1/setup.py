bff200da7c6ca4ec66d3a49ae14bf5f59e21cdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:48 +0000

    Add package sqlfluff-plugin-sparksql-upgrade/0.1.1/sqlfluff-plugin-sparksql-upgrade-0.1.1.tar.gz

---
+++ packages/sqlfluff-plugin-sparksql-upgrade/sqlfluff-plugin-sparksql-upgrade-0.1.1.tar.gz/sqlfluff-plugin-sparksql-upgrade-0.1.1/setup.py
@@ -0,0 +1,32 @@
+"""Setup file for example plugin."""
+from setuptools import find_packages, setup
+
+# Change these names in your plugin, e.g. company name or plugin purpose.
+PLUGIN_LOGICAL_NAME = "sparksql-upgrade"
+PLUGIN_ROOT_MODULE = "sparksql_upgrade"
+
+setup(
+    name="sqlfluff-plugin-{plugin_logical_name}".format(
+        plugin_logical_name=PLUGIN_LOGICAL_NAME
+    ),
+    version="0.1.1",
+    author="Holden Karau",
+    author_email="holden@pigscanfly.ca",
+    url="https://github.com/holdenk/spark-upgrade",
+    description="SQLFluff rules to help migrate your Spark SQL from 2.X to 3.X",
+    long_description="SQLFluff rules to help migrate your Spark SQL from 2.X to 3.X",
+    test_requires=["nose", "coverage", "unittest2"],
+    license="../LICENSE",
+    include_package_data=True,
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires="sqlfluff>=1.0.0",
+    entry_points={
+        "sqlfluff": [
+            "{plugin_logical_name} = {plugin_root_module}.rules".format(
+                plugin_logical_name=PLUGIN_LOGICAL_NAME,
+                plugin_root_module=PLUGIN_ROOT_MODULE,
+            )
+        ]
+    },
+)
