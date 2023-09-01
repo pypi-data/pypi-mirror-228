c1f86bf9457ec06f8ac704d43653ac10e84f7580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:12:37 +0000

    Add package weaviate-filter/0.0.2/weaviate_filter-0.0.2.tar.gz

---
+++ packages/weaviate-filter/weaviate_filter-0.0.2.tar.gz/weaviate_filter-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='weaviate_filter',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'weaviate-client',
+    ],
+    url='https://github.com/wjbmattingly/weaviate-filter',
+    license='MIT',
+    author='WJB Mattingly',
+    description='A package for creating GraphQL filters for Weaviate',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+)
