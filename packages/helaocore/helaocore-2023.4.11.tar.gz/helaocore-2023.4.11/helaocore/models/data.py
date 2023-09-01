215bce87565ac76a6b1f1c5e0639451e78a21c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:13:55 +0000

    Add package helaocore/2023.4.11/helaocore-2023.4.11.tar.gz

---
+++ packages/helaocore/helaocore-2023.4.11.tar.gz/helaocore-2023.4.11/helaocore/models/data.py
@@ -0,0 +1,24 @@
+__all__ = ["DataModel", "DataPackageModel"]
+
+from typing import List, Dict, Optional
+from uuid import UUID
+from pydantic import BaseModel, Field
+
+from helaocore.models.hlostatus import HloStatus
+from helaocore.helaodict import HelaoDict
+from helaocore.error import ErrorCodes
+
+
+class DataModel(BaseModel, HelaoDict):
+    # data is contained in a dict and keyed by file_conn_key
+    data: Dict[UUID, dict] = Field(default={})
+    errors: List[ErrorCodes] = Field(default=[])
+    status: Optional[HloStatus] = HloStatus.active
+
+
+class DataPackageModel(BaseModel, HelaoDict):
+    action_uuid: UUID
+    action_name: str
+    datamodel: DataModel
+    errors: List[ErrorCodes] = Field(default=[])
+    # status: Optional[HloStatus] = None
