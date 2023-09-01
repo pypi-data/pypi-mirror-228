8694f16d215ee3d8c7d036e4b0a90d297ffa633c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:07:58 +0000

    Add package pelatihan-bmkg/0.0.1/pelatihan-bmkg-0.0.1.tar.gz

---
+++ packages/pelatihan-bmkg/pelatihan-bmkg-0.0.1.tar.gz/pelatihan-bmkg-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "pelatihan-bmkg",
+    version = "0.0.1",
+    author = "ummu",
+    author_email = "author@example.com",
+    description = "aplikasi pertama",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://gitlab.com/ummumf/pelatihan_bmkg",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.9",
+    install_requires = [
+       ]
+)
