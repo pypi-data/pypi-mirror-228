7ce5b5804a1aa43204c513e0071c2985fdecbaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:13:41 +0000

    Add package oarepo-requests/1.0.2/oarepo_requests-1.0.2-py3-none-any.whl

---
+++ packages/oarepo-requests/oarepo_requests-1.0.2-py3-none-any.whl/oarepo_requests/components/__init__.py
@@ -0,0 +1,11 @@
+from .requests import (
+    AllowedRequestsComponent,
+    OAICreateRequestsComponent,
+    PublishDraftComponent,
+)
+
+__all__ = [
+    "AllowedRequestsComponent",
+    "PublishDraftComponent",
+    "OAICreateRequestsComponent",
+]
