9a3947c3f6f35cdc23f7db15bd6f912bd3baee2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:07:21 +0000

    Add package hivedca/0.0.30/hivedca-0.0.30-py3-none-any.whl

---
+++ packages/hivedca/hivedca-0.0.30-py3-none-any.whl/dca/dataloader.py
@@ -0,0 +1,320 @@
+import os
+from enum import Enum
+
+import PIL
+import torch
+from torchvision import transforms
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+class DatasetSplit(Enum):
+    TRAIN = "train"
+    VAL = "val"
+    TEST = "test"
+
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        source,
+        classname,
+        resize=(256,256),
+        split=DatasetSplit.TRAIN,
+        train_val_split=1.0,
+        **kwargs,
+    ):
+        """
+        Args:
+            source: [str]. Path to the MVTec data folder.
+            classname: [str or None]. Name of MVTec class that should be
+                       provided in this dataset. If None, the datasets
+                       iterates over all available images.
+            resize: [int]. (Square) Size the loaded image initially gets
+                    resized to.
+            imagesize: [int]. (Square) Size the resized loaded image gets
+                       (center-)cropped to.
+            split: [enum-option]. Indicates if training or test split of the
+                   data should be used. Has to be an option taken from
+                   DatasetSplit, e.g. mvtec.DatasetSplit.TRAIN. Note that
+                   mvtec.DatasetSplit.TEST will also load mask data.
+        """
+        super().__init__()
+        self.source = source
+        self.split = split
+        self.classnames_to_use = [classname]
+        self.train_val_split = train_val_split
+
+        self.imgpaths_per_class, self.data_to_iterate = self.get_image_data()
+
+        self.transform_img = [
+            transforms.Resize(resize),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+        ]
+        self.transform_img = transforms.Compose(self.transform_img)
+
+        self.transform_mask = [
+            transforms.Resize(resize),
+            transforms.ToTensor(),
+        ]
+        self.transform_mask = transforms.Compose(self.transform_mask)
+
+        self.imagesize = (3, resize[0], resize[1])
+
+    def __getitem__(self, idx):
+        classname, anomaly, image_path, mask_path = self.data_to_iterate[idx]
+        image = PIL.Image.open(image_path).convert("RGB")
+        image = self.transform_img(image)
+
+        if self.split == DatasetSplit.TEST and mask_path is not None:
+            mask = PIL.Image.open(mask_path)
+            mask = self.transform_mask(mask)
+        else:
+            mask = torch.zeros([1, *image.size()[1:]])
+
+        return {
+            "image": image,
+            "mask": mask,
+            "classname": classname,
+            "anomaly": anomaly,
+            "is_anomaly": int(anomaly != "good"),
+            "image_name": "/".join(image_path.split("/")[-4:]),
+            "image_path": image_path,
+        }
+
+    def __len__(self):
+        return len(self.data_to_iterate)
+
+    def get_image_data(self):
+        imgpaths_per_class = {}
+        maskpaths_per_class = {}
+
+        for classname in self.classnames_to_use:
+            classpath = os.path.join(self.source, classname, self.split.value)
+            maskpath = os.path.join(self.source, classname, "ground_truth")
+            print(classpath)
+            anomaly_types = os.listdir(classpath)
+
+            imgpaths_per_class[classname] = {}
+            maskpaths_per_class[classname] = {}
+
+            for anomaly in anomaly_types:
+                anomaly_path = os.path.join(classpath, anomaly)
+                anomaly_files = sorted(os.listdir(anomaly_path))
+                imgpaths_per_class[classname][anomaly] = [
+                    os.path.join(anomaly_path, x) for x in anomaly_files
+                ]
+
+                if self.train_val_split < 1.0:
+                    n_images = len(imgpaths_per_class[classname][anomaly])
+                    train_val_split_idx = int(n_images * self.train_val_split)
+                    if self.split == DatasetSplit.TRAIN:
+                        imgpaths_per_class[classname][anomaly] = imgpaths_per_class[
+                            classname
+                        ][anomaly][:train_val_split_idx]
+                    elif self.split == DatasetSplit.VAL:
+                        imgpaths_per_class[classname][anomaly] = imgpaths_per_class[
+                            classname
+                        ][anomaly][train_val_split_idx:]
+
+                if self.split == DatasetSplit.TEST and anomaly != "good":
+                    anomaly_mask_path = os.path.join(maskpath, anomaly)
+                    anomaly_mask_files = sorted(os.listdir(anomaly_mask_path))
+                    maskpaths_per_class[classname][anomaly] = [
+                        os.path.join(anomaly_mask_path, x) for x in anomaly_mask_files
+                    ]
+                else:
+                    maskpaths_per_class[classname]["good"] = None
+
+        # Unrolls the data dictionary to an easy-to-iterate list.
+        data_to_iterate = []
+        for classname in sorted(imgpaths_per_class.keys()):
+            for anomaly in sorted(imgpaths_per_class[classname].keys()):
+                for i, image_path in enumerate(imgpaths_per_class[classname][anomaly]):
+                    data_tuple = [classname, anomaly, image_path]
+                    if self.split == DatasetSplit.TEST and anomaly != "good":
+                        data_tuple.append(maskpaths_per_class[classname][anomaly][i])
+                    else:
+                        data_tuple.append(None)
+                    data_to_iterate.append(data_tuple)
+
+        return imgpaths_per_class, data_to_iterate
+
+
+def get_dataloaders(data_path,batch_size,resize,num_workers,augment=True):
+    train_val_split = 1.0
+    train_dataset = CustomDataset(data_path,classname=".",resize=resize,train_val_split=train_val_split,
+                                 split=DatasetSplit.TRAIN,
+                                 seed=0,augment=augment)
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                batch_size=batch_size,
+                shuffle=False,
+                num_workers=num_workers,
+                pin_memory=True,
+                )
+    
+
+    dataloader_dict = {
+                "training": train_dataloader,
+            }
+
+    
+    return dataloader_dict
+
+
+class TestDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        source,
+        classname,
+        resize=(256,256),
+        split=DatasetSplit.TRAIN,
+        **kwargs,
+    ):
+        """
+        Args:
+            source: [str]. Path to the MVTec data folder.
+            classname: [str or None]. Name of MVTec class that should be
+                       provided in this dataset. If None, the datasets
+                       iterates over all available images.
+            resize: [int]. (Square) Size the loaded image initially gets
+                    resized to.
+            imagesize: [int]. (Square) Size the resized loaded image gets
+                       (center-)cropped to.
+            split: [enum-option]. Indicates if training or test split of the
+                   data should be used. Has to be an option taken from
+                   DatasetSplit, e.g. mvtec.DatasetSplit.TRAIN. Note that
+                   mvtec.DatasetSplit.TEST will also load mask data.
+        """
+        super().__init__()
+        self.source = source
+        self.split = split
+        self.classnames_to_use = [classname]
+
+        self.imgpaths_per_class, self.data_to_iterate = self.get_image_data()
+
+        self.transform_img = [
+            transforms.Resize(resize),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+        ]
+        self.transform_img = transforms.Compose(self.transform_img)
+
+        self.transform_mask = [
+            transforms.Resize(resize),
+            transforms.ToTensor(),
+        ]
+        self.transform_mask = transforms.Compose(self.transform_mask)
+
+        self.imagesize = (3, resize[0], resize[1])
+
+    def __getitem__(self, idx):
+        classname, anomaly, image_path, mask_path = self.data_to_iterate[idx]
+        image = PIL.Image.open(image_path).convert("RGB")
+        image = self.transform_img(image)
+
+        if self.split == DatasetSplit.TEST and mask_path is not None:
+            mask = PIL.Image.open(mask_path)
+            mask = self.transform_mask(mask)
+        else:
+            mask = torch.zeros([1, *image.size()[1:]])
+
+        return {
+            "image": image,
+            "mask": mask,
+            "classname": classname,
+            "anomaly": anomaly,
+            "is_anomaly": int(anomaly != "good"),
+            "image_name": "/".join(image_path.split("/")[-4:]),
+            "image_path": image_path,
+        }
+
+    def __len__(self):
+        return len(self.data_to_iterate)
+
+    def get_image_data(self):
+        imgpaths_per_class = {}
+        maskpaths_per_class = {}
+
+        for classname in self.classnames_to_use:
+            classpath = os.path.join(self.source, classname, self.split.value)
+            maskpath = os.path.join(self.source, classname, "ground_truth")
+            anomaly_types = os.listdir(classpath)
+
+            imgpaths_per_class[classname] = {}
+            maskpaths_per_class[classname] = {}
+
+            for anomaly in anomaly_types:
+                anomaly_path = os.path.join(classpath, anomaly)
+                anomaly_files = sorted(os.listdir(anomaly_path))
+                imgpaths_per_class[classname][anomaly] = [
+                    os.path.join(anomaly_path, x) for x in anomaly_files
+                ]
+
+                if self.train_val_split < 1.0:
+                    n_images = len(imgpaths_per_class[classname][anomaly])
+                    train_val_split_idx = int(n_images * self.train_val_split)
+                    if self.split == DatasetSplit.TRAIN:
+                        imgpaths_per_class[classname][anomaly] = imgpaths_per_class[
+                            classname
+                        ][anomaly][:train_val_split_idx]
+                    elif self.split == DatasetSplit.VAL:
+                        imgpaths_per_class[classname][anomaly] = imgpaths_per_class[
+                            classname
+                        ][anomaly][train_val_split_idx:]
+
+                if self.split == DatasetSplit.TEST and anomaly != "good":
+                    anomaly_mask_path = os.path.join(maskpath, anomaly)
+                    anomaly_mask_files = sorted(os.listdir(anomaly_mask_path))
+                    maskpaths_per_class[classname][anomaly] = [
+                        os.path.join(anomaly_mask_path, x) for x in anomaly_mask_files
+                    ]
+                else:
+                    maskpaths_per_class[classname]["good"] = None
+
+        # Unrolls the data dictionary to an easy-to-iterate list.
+        data_to_iterate = []
+        for classname in sorted(imgpaths_per_class.keys()):
+            for anomaly in sorted(imgpaths_per_class[classname].keys()):
+                for i, image_path in enumerate(imgpaths_per_class[classname][anomaly]):
+                    data_tuple = [classname, anomaly, image_path]
+                    if self.split == DatasetSplit.TEST and anomaly != "good":
+                        data_tuple.append(maskpaths_per_class[classname][anomaly][i])
+                    else:
+                        data_tuple.append(None)
+                    data_to_iterate.append(data_tuple)
+
+        return imgpaths_per_class, data_to_iterate
+
+def get_test_dataloaders(data_path,batch_size,resize,num_workers):
+    test_dataset = CustomDataset(data_path,classname=".",resize=resize,train_val_split=1.0,
+                                 split=DatasetSplit.TEST,
+                                 seed=0,augment=False)
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+
+    dataloader_dict = {
+        "testing": test_dataloader,
+    }
+
+    return dataloader_dict
+
+
+
+
+
+
+
+def main():
+    a = get_dataloaders(name="Data",data_path="./mvtec",batch_size=2,image_size=256,num_workers=8,augment=True)
+    print(a)
+        
+if __name__ == "__main__":
+    main()
