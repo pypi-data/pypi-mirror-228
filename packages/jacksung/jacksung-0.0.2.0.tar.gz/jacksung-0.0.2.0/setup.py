3cc78e9651030963a6c2f810e8ad63e06a1954c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:11:50 +0000

    Add package jacksung/0.0.2.0/jacksung-0.0.2.0.tar.gz

---
+++ packages/jacksung/jacksung-0.0.2.0.tar.gz/jacksung-0.0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jacksung',
+    version='0.0.2.0',
+    author='Zijiang Song',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm',
+        'pymysql'
+    ],
+)
