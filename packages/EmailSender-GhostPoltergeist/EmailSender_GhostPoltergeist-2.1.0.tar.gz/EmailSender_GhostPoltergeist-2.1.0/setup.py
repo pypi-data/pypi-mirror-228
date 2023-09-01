8c465530eda4adee79e65e3bcae81da8b17703bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:12:43 +0000

    Add package EmailSender-GhostPoltergeist/2.1.0/EmailSender_GhostPoltergeist-2.1.0.tar.gz

---
+++ packages/EmailSender-GhostPoltergeist/EmailSender_GhostPoltergeist-2.1.0.tar.gz/EmailSender_GhostPoltergeist-2.1.0/setup.py
@@ -0,0 +1,17 @@
+
+#  █▀▀▀  █▀▄▀█  █▀▀█ ▀█▀  █    　  █▀▀▀█  █▀▀▀  █▄  █  █▀▀▄  █▀▀▀  █▀▀█
+#  █▀▀▀  █ █ █  █▄▄█  █   █    　  ▀▀▀▄▄  █▀▀▀  █ █ █  █  █  █▀▀▀  █▄▄▀
+#  █▄▄▄  █   █  █  █ ▄█▄  █▄▄█ 　  █▄▄▄█  █▄▄▄  █  ▀█  █▄▄▀  █▄▄▄  █  █
+
+from setuptools import setup
+
+setup(
+    name='EmailSender_GhostPoltergeist',
+    version='2.1.0',
+    description='A python script program to send emails using SMTP',
+    author='Harold Edsel F. Cabaluna',
+    author_email='EdselCabaluna21@gmail.com',
+    license='MIT',
+    keywords='email sender smtp',
+    packages=['EmailSender_GhostPoltergeist'],
+)
