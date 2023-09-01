f1f14691c3fb45b47952098405e59f3ae42c674d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 04:13:41 +0000

    Add package swheel/0.1.1/swheel-0.1.1-py3-none-any.whl

---
+++ packages/swheel/swheel-0.1.1-py3-none-any.whl/swheel/__init__.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+def verifica_projeto_com_src():
+    diretorio_atual = os.getcwd()
+    caminho_src = os.path.join(diretorio_atual, 'src')
+    return os.path.exists(caminho_src) and os.path.isdir(caminho_src)
+
+def main():
+    if verifica_projeto_com_src():
+        print("Projeto Python encontrado com a pasta 'src'.")
+        subprocess.run(['python', 'setup.py', 'sdist', 'bdist_wheel'])
+        print("Projeto construído usando sbuild.")
+    else:
+        print("Este não parece ser um projeto Python com a pasta 'src'.")
+
+if __name__ == "__main__":
+    main()
