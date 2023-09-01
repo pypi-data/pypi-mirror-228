e2dd0f685c5941718ec3498e1a4535bb83c42418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:50 +0000

    Add package arena-py/0.6.0/arena-py-0.6.0.tar.gz

---
+++ packages/arena-py/arena-py-0.6.0.tar.gz/arena-py-0.6.0/arena/objects/__init__.py
@@ -0,0 +1,53 @@
+from .arena_object import Object
+from .box import Box
+from .camera import Camera
+from .circle import Circle
+from .cone import Cone
+from .cylinder import Cylinder
+from .dodecahedron import Dodecahedron
+from .entity import Entity
+from .gltf import GLTF
+from .icosahedron import Icosahedron
+from .image import Image
+from .light import Light
+from .line import Line
+from .octahedron import Octahedron
+from .particle import Particle
+from .plane import Plane
+from .ring import Ring
+from .sphere import Sphere
+from .tetrahedron import Tetrahedron
+from .text import Text
+from .thickline import ThickLine
+from .torus import Torus
+from .torus_knot import TorusKnot
+from .triangle import Triangle
+from .ui import *
+
+OBJECT_TYPE_MAP = {
+    "box": Box,
+    "camera": Camera,
+    "circle": Circle,
+    "cone": Cone,
+    "cube": Box,
+    "cylinder": Cylinder,
+    "dodecahedron": Dodecahedron,
+    "entity": Entity,
+    "gltf-model": GLTF,
+    "icosahedron": Icosahedron,
+    "image": Image,
+    "light": Light,
+    "line": Line,
+    "object": Object,
+    "octahedron": Octahedron,
+    "particle": Particle,
+    "plane": Plane,
+    "ring": Ring,
+    "sphere": Sphere,
+    "tetrahedron": Tetrahedron,
+    "text": Text,
+    "thickline": ThickLine,
+    "torus": Torus,
+    "torusKnot": TorusKnot,
+    "triangle": Triangle,
+}
