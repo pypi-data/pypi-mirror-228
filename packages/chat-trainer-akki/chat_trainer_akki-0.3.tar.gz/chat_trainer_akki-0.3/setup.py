c817bf1217a643bf62de4df5b5905e4352d1ec75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:07:50 +0000

    Add package chat-trainer-akki/0.3/chat_trainer_akki-0.3.tar.gz

---
+++ packages/chat-trainer-akki/chat_trainer_akki-0.3.tar.gz/chat_trainer_akki-0.3/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+setup(name="chat_trainer_akki",
+version="0.3",
+description="code trainer package",
+long_description = "code trainer package",
+author="Akshat",
+packages=['chat_trainer_akki'],
+install_requires=['nltk','tensorflow','numpy'])
