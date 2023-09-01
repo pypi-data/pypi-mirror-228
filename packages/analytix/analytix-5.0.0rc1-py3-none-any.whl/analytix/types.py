34e1ef93eae58f999fb2fe4175e3a619953e04b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:10:13 +0000

    Add package analytix/5.0.0rc1/analytix-5.0.0rc1-py3-none-any.whl

---
+++ packages/analytix/analytix-5.0.0rc1-py3-none-any.whl/analytix/types.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2021-present, Ethan Henderson
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import annotations
+
+__all__ = (
+    "ResponseT",
+    "PathLikeT",
+    "OptionalPathLikeT",
+    "RequestParamsT",
+    "SecretT",
+    "TokenT",
+    "ReportRowT",
+    "AuthUriT",
+    "TokenUriT",
+)
+
+import typing as t
+from pathlib import Path
+from typing import Dict, Tuple, Union
+
+# Old
+ResponseT = t.Dict[str, t.Any]
+PathLikeT = t.Union[str, Path]
+OptionalPathLikeT = t.Union[str, Path, None]
+
+RequestParamsT = t.Tuple[str, t.Dict[str, str], t.Dict[str, str]]
+SecretT = t.Union[str, t.List[str]]
+TokenT = t.Union[str, int]
+ReportRowT = t.List[t.List[t.Union[str, int, float]]]
+
+AuthUriT = t.Tuple[str, t.Dict[str, str]]
+TokenUriT = t.Tuple[str, t.Dict[str, str], t.Dict[str, str]]
+
+# New
+PathLike = Union[str, Path]
+UriParams = Tuple[str, Dict[str, str], Dict[str, str]]
