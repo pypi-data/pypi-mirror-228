8ec2c989147b950c24f738ca70cdaf67d18a9071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 06:14:11 +0000

    Add package Remilia/1.3/remilia-1.3-py3-none-any.whl

---
+++ packages/Remilia/remilia-1.3-py3-none-any.whl/Remilia/__init__.py
@@ -0,0 +1,27 @@
+from . import (
+    base,
+    log,
+    res,
+    shadow,
+    utils,
+    mixin,
+    sdb,
+    fancy,
+    impl,
+    expression,
+    cutils,
+)
+
+__all__ = [
+    "impl",
+    "mixin",
+    "log",
+    "res",
+    "shadow",
+    "utils",
+    "base",
+    "sdb",
+    "fancy",
+    "expression",
+    "cutils"
+]
---
+++ packages/Remilia/remilia-1.3-py3-none-any.whl/Remilia/base/typings.py
@@ -0,0 +1,12 @@
+from typing import Generic, TypeVar
+
+RT = TypeVar("RT")  # Return Type
+T = TypeVar("T")
+
+NT = TypeVar("NT")  # the first arg of a pair
+VT = TypeVar("VT")  # the second arg of a pair
+
+
+class PairType(Generic[NT, VT]):
+    ...
+
