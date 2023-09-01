26be23765be3b372e569007220ca20c11a61d50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:08:26 +0000

    Add package aidios-sdk/0.1/aidios_sdk-0.1.tar.gz

---
+++ packages/aidios-sdk/aidios_sdk-0.1.tar.gz/aidios_sdk-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aidios_sdk',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'json'
+    ],
+    author='Your Name',
+    author_email='jamie.gilchrist@tunestamp.com',
+    description='SDK for the Aidios API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://aidios.co.uk',  
+)
+from setuptools import setup, find_packages
+
+setup(
+    name='aidios_sdk',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    author='Jamie Gilchrist',
+    author_email='jamie.gilchrist@tunestamp.com',
+    description='SDK for the Aidios API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://aidios.co.uk',  
+)
