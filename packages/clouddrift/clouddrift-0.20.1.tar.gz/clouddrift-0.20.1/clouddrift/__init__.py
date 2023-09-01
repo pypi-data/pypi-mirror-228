f44cdba9c6b38c665b0b9b5502962bca460b1cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 05:09:26 +0000

    Add package clouddrift/0.20.1/clouddrift-0.20.1.tar.gz

---
+++ packages/clouddrift/clouddrift-0.20.1.tar.gz/clouddrift-0.20.1/clouddrift/__init__.py
@@ -0,0 +1,10 @@
+from ._version import version
+
+__version__ = version
+
+from clouddrift.raggedarray import RaggedArray
+import clouddrift.adapters
+import clouddrift.analysis
+import clouddrift.datasets
+import clouddrift.sphere
+import clouddrift.signal
