6a7fd1f653c7a7dbb9c07a78f51a2fe420ee22a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 14:07:30 +0000

    Add package dicergirl/3.2.13/dicergirl-3.2.13.tar.gz

---
+++ packages/dicergirl/dicergirl-3.2.13.tar.gz/dicergirl-3.2.13/setup.py
@@ -0,0 +1,41 @@
+from dicergirl.utils.utils import version
+
+import setuptools
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = "dicergirl",
+    version = version,
+    author = "Night Resurgent <fu050409@163.com>",
+    author_email = "fu050409@163.com",
+    description = "跑团骰娘机器人欧若可, 支持 QQ频道 及 Nonebot2 部署.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://gitee.com/unvisitor/dicer",
+    project_urls = {
+        "Bug Tracker": "https://gitee.com/unvisitor/dicer/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    license = "Apache-2.0",
+    packages = setuptools.find_packages(),
+    install_requires = [
+        'nonebot2',
+        'nonebot-adapter-onebot',
+        'loguru',
+        'pyyaml',
+        'openai',
+        'multilogging',
+        'psutil',
+        'nonebot_plugin_apscheduler',
+        'pypi-simple',
+        'httpx',
+        'dicergirl-plugin-scp',
+    ],
+    python_requires=">=3",
+)
