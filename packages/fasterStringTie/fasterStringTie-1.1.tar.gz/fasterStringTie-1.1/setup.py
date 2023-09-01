80ce1c883893734df17d877bb85a4c3b71eb41ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:11:51 +0000

    Add package fasterStringTie/1.1/fasterStringTie-1.1.tar.gz

---
+++ packages/fasterStringTie/fasterStringTie-1.1.tar.gz/fasterStringTie-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fasterStringTie',
+    author="Waqar Hanif",
+    description="A three times faster version of StringTie.",
+    version='1.1',
+    packages=find_packages()
+)
