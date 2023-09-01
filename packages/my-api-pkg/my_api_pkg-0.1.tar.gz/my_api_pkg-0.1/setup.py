fdbd686435255f846ab4ffb25565e2c20eeb5d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-01 03:12:37 +0000

    Add package my-api-pkg/0.1/my_api_pkg-0.1.tar.gz

---
+++ packages/my-api-pkg/my_api_pkg-0.1.tar.gz/my_api_pkg-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='my_api_pkg',
+    version='0.1',
+    description='A package for IP generator, TCP, UDP, SSL and XML RPC socket modules',
+    author='Pa Pa',
+    author_email='htunpa2aung@gmail.com',
+    packages=['my_api_pkg'],
+)
