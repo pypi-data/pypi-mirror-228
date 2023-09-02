9da7935c9f856a844b220bdd6bd61bf379ff51e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:11:17 +0000

    Add package vision-datasets/1.0.4/vision_datasets-1.0.4.tar.gz

---
+++ packages/vision-datasets/vision_datasets-1.0.4.tar.gz/vision_datasets-1.0.4/vision_datasets/image_regression/operations.py
@@ -0,0 +1,18 @@
+from ..common import DatasetTypes, GenerateCocoDictBase, ImageLabelManifest, SampleByNumSamples, SampleStrategyType, SingleTaskMerge, Spawn, Split, CocoDictGeneratorFactory, \
+    ManifestMergeStrategyFactory, SampleStrategyFactory, SpawnFactory, SplitFactory
+
+_DATA_TYPE = DatasetTypes.IMAGE_REGRESSION
+
+
+@CocoDictGeneratorFactory.register(_DATA_TYPE)
+class ImageRegressionCocoDictGenerator(GenerateCocoDictBase):
+    def process_labels(self, coco_ann, label: ImageLabelManifest):
+        coco_ann['target'] = label.label_data
+
+
+ManifestMergeStrategyFactory.direct_register(SingleTaskMerge, _DATA_TYPE)
+
+SampleStrategyFactory.direct_register(SampleByNumSamples, _DATA_TYPE, SampleStrategyType.NumSamples)
+
+SpawnFactory.direct_register(Spawn, _DATA_TYPE)
+SplitFactory.direct_register(Split, _DATA_TYPE)
