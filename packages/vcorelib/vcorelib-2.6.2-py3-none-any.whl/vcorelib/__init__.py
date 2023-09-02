76eb29e6d6241294d7339b90a32b317c971b4953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:11:09 +0000

    Add package vcorelib/2.6.2/vcorelib-2.6.2-py3-none-any.whl

---
+++ packages/vcorelib/vcorelib-2.6.2-py3-none-any.whl/vcorelib/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.3
+# hash=2277ca86d71f3c89201b04db9e130324
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A collection of core Python utilities."
+PKG_NAME = "vcorelib"
+VERSION = "2.6.2"
+
+# vcorelib-specific content.
+DEFAULT_INDENT = 2
+DEFAULT_ENCODING = "utf-8"
