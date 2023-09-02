7ddcf40dcfd73d5722ae68b40b563e39c3941cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:11:01 +0000

    Add package pyfzf-obj/0.1.0/pyfzf-obj-0.1.0.tar.gz

---
+++ packages/pyfzf-obj/pyfzf-obj-0.1.0.tar.gz/pyfzf-obj-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="pyfzf-obj",
+        version="0.1.0",
+        packages=find_packages(),
+        install_requires=[
+            ],
+        author="Max de Hoyos",
+        description="Get actual python objects back from `pyfzf-iter`, not strings",
+        distribution=''
+        )
