07b631f854977a3f3714fc3115c3e33fb9b81c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 05:10:46 +0000

    Add package LatticePy/0.1.1/LatticePy-0.1.1.tar.gz

---
+++ packages/LatticePy/LatticePy-0.1.1.tar.gz/LatticePy-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from LatticePy import __version__
+
+def parse_requirements(file):
+    with open('requirements.txt', 'rb') as f:
+        return f.read().decode().splitlines()
+
+requirements = parse_requirements("requirements.txt")
+print("REQUIREMENTS: ", requirements)
+
+DESCRIPTION = 'A package for simulating molecules on a lattice.'
+LONG_DESCRIPTION = 'A package that simulates multiple types of micromolecules such as proteins on a simple cubic or FCC lattice.'
+
+setup(
+    name="LatticePy",
+    version=__version__,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Sohit Miglani",
+    author_email="sohitmiglani@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=requirements,
+    keywords='proteins,simulation,lattice',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
