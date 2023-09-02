415be411c55f30580749128a5bbfdcb40fe44fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:11:03 +0000

    Add package nixtlats/0.1.12/nixtlats-0.1.12.tar.gz

---
+++ packages/nixtlats/nixtlats-0.1.12.tar.gz/nixtlats-0.1.12/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nixtlats",
+    version="0.1.12",
+    description="Nixtla SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nixtla/nixtla",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=["requests", "pandas", "httpx", "pydantic<2"],
+    extras_require={
+        "dev": ["black", "nbdev", "plotly", "python-dotenv", "openbb", "statsforecast", "utilsforecast"],
+        "distributed": ["dask[complete]", "fugue[ray]", "pyspark"],
+    },
+)
