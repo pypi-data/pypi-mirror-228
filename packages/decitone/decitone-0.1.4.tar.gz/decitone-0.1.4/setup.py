fa7ed27b173c80a3add52bc6d3e952a2cd95480a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 14:06:43 +0000

    Add package decitone/0.1.4/decitone-0.1.4.tar.gz

---
+++ packages/decitone/decitone-0.1.4.tar.gz/decitone-0.1.4/setup.py
@@ -0,0 +1,15 @@
+# cromp/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='decitone',
+    version='0.1.4',
+    description='This package contains deep learning utilities files',
+    author='Asiedu Henry',
+    author_email='sensork82@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
+
+ 
