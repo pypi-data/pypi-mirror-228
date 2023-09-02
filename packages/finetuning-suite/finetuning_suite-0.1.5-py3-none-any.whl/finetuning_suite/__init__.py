80bcc5bcbc6f4396deb376b2671de294b87c1f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:12:12 +0000

    Add package finetuning-suite/0.1.5/finetuning_suite-0.1.5-py3-none-any.whl

---
+++ packages/finetuning-suite/finetuning_suite-0.1.5-py3-none-any.whl/finetuning_suite/__init__.py
@@ -0,0 +1,6 @@
+from finetuning_suite.finetuner import FineTuner
+from finetuning_suite.inference.hf_model import Inference
+
+from finetuning_suite.processing.base import Preprocessor, DefaultPreprocessor
+from finetuning_suite.trainer.base import TrainerConfiguration, DefaultTrainerConfig
+
