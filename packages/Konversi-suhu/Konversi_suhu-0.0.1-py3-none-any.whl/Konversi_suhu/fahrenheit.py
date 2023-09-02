070bd778c2503ccb678f3d48cf1fef0ffc9ddee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:12:15 +0000

    Add package Konversi-suhu/0.0.1/Konversi_suhu-0.0.1-py3-none-any.whl

---
+++ packages/Konversi-suhu/Konversi_suhu-0.0.1-py3-none-any.whl/Konversi_suhu/fahrenheit.py
@@ -0,0 +1,11 @@
+class Fahrenheit:
+    def __init__ (self,derajat):
+        print('Selamat Datang di konversi Fahrenheit')
+        self.derajat = derajat
+
+    def hitung_konv(self):
+        return (self.derajat *1.8)+32
+    
+  
+def menghitung_konversi_celcius_to_fahrenheit():
+    return "ini fungsi menghitung konversi Celcius ke Fahrenheit"
