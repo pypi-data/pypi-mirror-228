432bb923e46ab9a3d4515598f60fd59a2c9ca03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:09:52 +0000

    Add package WestJR/0.4.2/westjr-0.4.2.tar.gz

---
+++ packages/WestJR/westjr-0.4.2.tar.gz/westjr-0.4.2/westjr/response_types/stations.py
@@ -0,0 +1,49 @@
+# [/api/v3/{LINE}_st.json]
+from __future__ import annotations
+
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class TransferItem(BaseModel):
+    name: str
+    type: int
+    code: str
+    link: Optional[str]
+    linkCode: Optional[str]
+
+
+class Info(BaseModel):
+    name: str
+    code: str
+    stopTrains: Optional[List[int]]
+    typeNotice: Optional[str]
+    transfer: Optional[List[TransferItem]]
+    line: Optional[str]
+    pairDisplay: Optional[str]
+    lines: Optional[str]
+
+
+class SideItem(BaseModel):
+    type: int
+    side: Optional[int]
+    linkLine: Optional[str]
+    linkStationCode: Optional[str]
+    line: Optional[str]
+    linkDirection: Optional[int]
+
+
+class Design(BaseModel):
+    mark: Optional[str]
+    upside: Optional[List[SideItem]]
+    downside: Optional[List[SideItem]]
+
+
+class StationsItem(BaseModel):
+    info: Info
+    design: Design
+
+
+class Stations(BaseModel):
+    stations: List[StationsItem]
---
+++ packages/WestJR/westjr-0.4.2.tar.gz/westjr-0.4.2/westjr/response_types/train_pos.py
@@ -0,0 +1,31 @@
+# [/api/v3/{LINE}.json]
+from __future__ import annotations
+
+from typing import List
+
+from pydantic import BaseModel
+
+
+class Dest(BaseModel):
+    text: str
+    code: str
+    line: str
+
+
+class TrainsItem(BaseModel):
+    no: str
+    pos: str
+    direction: int
+    nickname: str
+    type: str
+    displayType: str
+    dest: Dest
+    via: str
+    delayMinutes: int
+    typeChange: str
+    numberOfCars: int
+
+
+class TrainPos(BaseModel):
+    update: str
+    trains: List[TrainsItem]
