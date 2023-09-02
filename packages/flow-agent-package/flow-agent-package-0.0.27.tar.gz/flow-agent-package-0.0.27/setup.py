7e9b557dc4f7860cfb06ebff33e87fc761099cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 05:10:43 +0000

    Add package flow-agent-package/0.0.27/flow-agent-package-0.0.27.tar.gz

---
+++ packages/flow-agent-package/flow-agent-package-0.0.27.tar.gz/flow-agent-package-0.0.27/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="flow-agent-package",
+    version="0.0.27",
+    description="This is my agent tools package",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": ["flow_agent = flow_agent_package.tools.utils:list_package_tools"],
+    },
+    install_requires=[
+        'semantic_kernel'
+    ],
+    include_package_data=True,   # This line tells setuptools to include files from MANIFEST.in
+)
