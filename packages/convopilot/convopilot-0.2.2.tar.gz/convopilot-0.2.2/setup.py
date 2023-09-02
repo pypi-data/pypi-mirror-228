9a621e54eda5a875b1c60cca642753ec26399e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 15:08:31 +0000

    Add package convopilot/0.2.2/convopilot-0.2.2.tar.gz

---
+++ packages/convopilot/convopilot-0.2.2.tar.gz/convopilot-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='convopilot',
+    version='0.2.2',
+    packages=find_packages(include=['convopilot']),
+    license="MIT",
+    author="Xiaomeng Zhang",
+    author_email="zhang123cnn@gmail.com",
+    description="An AI tool to help users better navigate conversations.",
+    install_requires=requirements,
+    url='https://github.com/zhang123cnn/convopilot',
+    readme='README.md',
+    entry_points={
+        'console_scripts': [
+            'convopilot=convopilot.record_audio:cli',
+            'convopilot-server=convopilot.server:main'
+        ]
+    },
+)
