69728e420c4c611fa5f51c1a12e463c0a5104aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 04:12:13 +0000

    Add package hrefs/0.11/hrefs-0.11-py3-none-any.whl

---
+++ packages/hrefs/hrefs-0.11-py3-none-any.whl/hrefs/__init__.py
@@ -0,0 +1,7 @@
+"""Hyperlinks for pydantic models"""
+
+__version__ = "0.11"
+
+from .href import Href, Referrable
+from .model import BaseReferrableModel, PrimaryKey
+from .errors import ReferrableModelError
