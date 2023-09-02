aa5766e42ff61e7bcf85d2ee21278fb7af5f1e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 14:07:10 +0000

    Add package homeassistant-stubs/2023.9.0b2/homeassistant_stubs-2023.9.0b2-py3-none-any.whl

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2023.9.0b2-py3-none-any.whl/homeassistant-stubs/components/filesize/__init__.pyi
@@ -0,0 +1,9 @@
+from .const import PLATFORMS as PLATFORMS
+from homeassistant.config_entries import ConfigEntry as ConfigEntry
+from homeassistant.const import CONF_FILE_PATH as CONF_FILE_PATH
+from homeassistant.core import HomeAssistant as HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady as ConfigEntryNotReady
+
+def _check_path(hass: HomeAssistant, path: str) -> None: ...
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool: ...
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool: ...
