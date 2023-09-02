f22a68f92f676cea3845d3f21682d00d9f31ef82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 04:12:24 +0000

    Add package PlexTraktSync/0.27.2/PlexTraktSync-0.27.2.tar.gz

---
+++ packages/PlexTraktSync/PlexTraktSync-0.27.2.tar.gz/PlexTraktSync-0.27.2/plextraktsync/config/PlexServerConfig.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import asdict, dataclass
+
+
+@dataclass
+class PlexServerConfig:
+    """
+    Class to hold single server config
+    """
+
+    name: str
+    token: str
+    urls: list[str]
+    # The machineIdentifier value of this server
+    id: str = None
+
+    def asdict(self):
+        data = asdict(self)
+        del data["name"]
+
+        return data
