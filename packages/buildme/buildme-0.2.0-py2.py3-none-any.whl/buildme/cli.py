d6ba48a154990baf1f8f86d0de410e8170fca08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 15:07:58 +0000

    Add package buildme/0.2.0/buildme-0.2.0-py2.py3-none-any.whl

---
+++ packages/buildme/buildme-0.2.0-py2.py3-none-any.whl/buildme/cli.py
@@ -0,0 +1,56 @@
+import argparse
+import sys
+from itertools import compress
+from typing import Any
+
+from buildme.core import _check_target_exists
+from buildme.core import _exec_target
+
+
+def _get_buildme_file_contents(filepath: str) -> str:
+    with open(filepath, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def _create_build_script_code(old_code: str, targets: list[str], usr_opt_var_name: str) -> str:
+    return old_code + ''.join(f'\n{t}({usr_opt_var_name})' for t in targets)
+
+
+def _check_target_exists_map(targets: list[str]) -> list[bool]:
+    return [_check_target_exists(name) for name in targets]
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    user_arg_parser = argparse.ArgumentParser()
+
+    parser.add_argument('path', type=str, help='path to the buildme script')
+    parser.add_argument('targets', type=str, nargs='*',
+                        help='The list of targets to run')
+
+    args, unknown = parser.parse_known_args()
+
+    for a in unknown:
+        if a.startswith(('-', '--')):
+            user_arg_parser.add_argument(a.split('=')[0], type=str)
+
+    usr_known_args, _ = user_arg_parser.parse_known_args()
+    # gets the var name as string
+    usr_known_args_var_name = f'{usr_known_args=}'.split('=')[0]  # noqa: F841
+
+    target_globals: dict[str, Any] = {}
+    exec(_get_buildme_file_contents(args.path), target_globals)
+
+    if not all(invalid_map := _check_target_exists_map(targets=args.targets)):
+        print('unknown target(s): ' + ' '.join(compress(args.targets, [not i for i in invalid_map])),
+              file=sys.stderr)
+        return 1
+
+    for t in args.targets:
+        _exec_target(t, usr_known_args, target_globals)
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
