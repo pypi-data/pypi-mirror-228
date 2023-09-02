8273799d2444f14bfe13608b8ce2e3dad2db0a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 05:09:19 +0000

    Add package metavision/0.0.3/metavision-0.0.3.tar.gz

---
+++ packages/metavision/metavision-0.0.3.tar.gz/metavision-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Basic ELA package'
+LONG_DESCRIPTION = 'A package that provides fast image processing tools.'
+
+# Setting up
+setup(
+    name="metavision",
+    version=VERSION,
+    author="Abhishek Kumar",
+    author_email="<ykeep21@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['python', 'image', 'error level', 'compression', 'JPEG', 'residuals'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
