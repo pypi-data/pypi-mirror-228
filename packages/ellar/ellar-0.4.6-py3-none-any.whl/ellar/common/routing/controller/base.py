fe5bb2e7ef0215be494cac6070446f3ce730c531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 15:08:26 +0000

    Add package ellar/0.4.6/ellar-0.4.6-py3-none-any.whl

---
+++ packages/ellar/ellar-0.4.6-py3-none-any.whl/ellar/common/routing/controller/base.py
@@ -0,0 +1,25 @@
+import typing as t
+
+from ellar.common.interfaces import IExecutionContext
+from ellar.common.models import ControllerBase
+
+
+class ControllerRouteOperationBase:
+    endpoint: t.Callable
+    get_controller_type: t.Callable
+
+    def _get_controller_instance(self, ctx: IExecutionContext) -> ControllerBase:
+        controller_type: t.Optional[t.Type[ControllerBase]] = self.get_controller_type()
+
+        service_provider = ctx.get_service_provider()
+
+        controller_instance: ControllerBase = service_provider.get(controller_type)
+        controller_instance.context = ctx
+        return controller_instance
+
+    @t.no_type_check
+    def __call__(
+        self, context: IExecutionContext, *args: t.Any, **kwargs: t.Any
+    ) -> t.Any:
+        controller_instance = self._get_controller_instance(ctx=context)
+        return self.endpoint(controller_instance, *args, **kwargs)
