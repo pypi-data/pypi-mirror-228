1e6c63bc00c9b1d8cd8a00bea7d5d7ec288f748b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:10:06 +0000

    Add package pbrm/0.1.5/pbrm-0.1.5-py3-none-any.whl

---
+++ packages/pbrm/pbrm-0.1.5-py3-none-any.whl/pbrm/spider/get_user_illusts.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+def get_user_illusts(pid: str, cookie: str):
+    header = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "referer": "https://www.pixiv.net/users/{}/artworks".format(pid),
+        "accept": "application/json",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "cookie": cookie
+    }
+
+    url = "https://www.pixiv.net/ajax/user/{}/profile/all?lang=zh".format(pid)
+
+    response = requests.get(url, headers=header)
+    response_json = json.loads(response.content.decode("utf-8"))
+    r = list(response_json["body"]["illusts"].keys()) + list(response_json["body"]["manga"].keys())
+
+    return r
+
