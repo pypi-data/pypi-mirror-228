7cf509d4fb2459720aaab41ca65054cafd254738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 04:10:50 +0000

    Add package ubcpdk/2.1.0/ubcpdk-2.1.0.tar.gz

---
+++ packages/ubcpdk/ubcpdk-2.1.0.tar.gz/ubcpdk-2.1.0/ubcpdk/simulation/modes_waveguide.py
@@ -0,0 +1,31 @@
+"""MPB mode simulations."""
+
+import gdsfactory as gf
+import gplugins.modes as gm
+
+from ubcpdk.config import PATH
+
+nm = 1e-3
+
+find_modes_waveguide = gf.partial(
+    gm.find_modes_waveguide,
+    wg_width=500 * nm,
+    wg_thickness=220 * nm,
+    slab_thickness=0 * nm,
+    resolution=20,
+    nmodes=4,
+    cache=PATH.modes,
+)
+
+
+find_neff_vs_width = gf.partial(
+    gm.find_neff_vs_width, cache=PATH.modes, filepath="find_neff_vs_width.csv"
+)
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    df = find_neff_vs_width()
+    gm.plot_neff_vs_width(df)
+    plt.show()
