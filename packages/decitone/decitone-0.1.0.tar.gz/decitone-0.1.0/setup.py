3863a30ed49fb4694cb591462b34644a9852dfbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:11:52 +0000

    Add package decitone/0.1.0/decitone-0.1.0.tar.gz

---
+++ packages/decitone/decitone-0.1.0.tar.gz/decitone-0.1.0/setup.py
@@ -0,0 +1,13 @@
+# decitone/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='decitone',
+    version='0.1.0',
+    description='A simple Python package',
+    author='Asiedu Henry',
+    author_email='sensork82@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
