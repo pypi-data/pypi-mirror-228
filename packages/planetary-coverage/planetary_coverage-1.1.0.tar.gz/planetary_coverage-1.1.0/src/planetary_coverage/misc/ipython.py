4cf0b539f415796c5aa80dd8a800e43ed224ff53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:12:49 +0000

    Add package planetary-coverage/1.1.0/planetary_coverage-1.1.0.tar.gz

---
+++ packages/planetary-coverage/planetary_coverage-1.1.0.tar.gz/planetary_coverage-1.1.0/src/planetary_coverage/misc/ipython.py
@@ -0,0 +1,43 @@
+"""IPython module."""
+
+from importlib.metadata import PackageNotFoundError, version
+from os import sys
+
+from .dotenv import print_kernels_dir
+
+
+def load_ipython_extension(ipython):  # pylint: disable=unused-argument
+    """Print the list of installed packages and kernels directories.
+
+    IPython/Jupyter magic line: ``%load_ext planetary_coverage``
+
+    """
+    print_package(
+        'planetary-coverage',
+        'esa-ptr',
+        'numpy',
+        'matplotlib',
+        'spiceypy',
+    )
+
+    sys.stdout.write('\n')
+
+    print_kernels_dir()
+
+
+def print_package(*packages):
+    """Print packages versions.
+
+    Note
+    ----
+    Only the available packages are printed in stdout.
+
+    """
+    sys.stdout.write('Installed packages:\n')
+
+    n = max(map(len, packages))
+    for package in packages:
+        try:
+            sys.stdout.write(f'- {package:{n}s}: {version(package.lower())}\n')
+        except PackageNotFoundError:
+            pass
