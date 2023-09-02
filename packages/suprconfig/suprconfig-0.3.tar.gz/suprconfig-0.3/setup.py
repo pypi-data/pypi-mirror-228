996b527fc41f76c7ee871b5a0eca14d253fad413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 14:07:00 +0000

    Add package suprconfig/0.3/suprconfig-0.3.tar.gz

---
+++ packages/suprconfig/suprconfig-0.3.tar.gz/suprconfig-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "suprconfig",
+    version = "0.3",
+    description = "This library created for comfortable using config",
+    packages = ['suprconfig'],
+    author_email="keneskay@vk.com",
+    author = 'h99hood',
+    zip_safe = False,
+    long_description_content_type = "text/markdown",
+    long_description = """
+    
+**Documentation**
+
+**```class Config(self, config_path: str)```**
+    """,
+)
