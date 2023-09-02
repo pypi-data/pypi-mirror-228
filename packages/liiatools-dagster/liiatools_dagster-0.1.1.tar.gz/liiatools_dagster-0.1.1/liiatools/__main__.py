d509feb8ec8dceefd6a60fae142424ce028a2434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:11:35 +0000

    Add package liiatools-dagster/0.1.1/liiatools_dagster-0.1.1.tar.gz

---
+++ packages/liiatools-dagster/liiatools_dagster-0.1.1.tar.gz/liiatools_dagster-0.1.1/liiatools/__main__.py
@@ -0,0 +1,20 @@
+import click as click
+
+from liiatools.datasets.annex_a.annex_a_cli import annex_a
+from liiatools.datasets.cin_census.cin_cli import cin_census
+from liiatools.datasets.social_work_workforce.csww_cli import csww
+from liiatools.ssda903_pipeline.cli import s903
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(annex_a)
+cli.add_command(cin_census)
+cli.add_command(s903)
+cli.add_command(csww)
+
+if __name__ == "__main__":
+    cli()
