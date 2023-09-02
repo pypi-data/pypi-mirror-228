bb2a04690e030039d3b42ca583f19f80a33a23d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:12:48 +0000

    Add package planetary-coverage/1.1.0/planetary_coverage-1.1.0-py3-none-any.whl

---
+++ packages/planetary-coverage/planetary_coverage-1.1.0-py3-none-any.whl/planetary_coverage/rois/__init__.py
@@ -0,0 +1,24 @@
+"""Region Of Interest module."""
+
+from .categories import (
+    CategoriesCollection, Category, SubCategoriesCollection, SubCategory
+)
+from .geojson import GeoJsonROI
+from .kml import KmlROIsCollection
+from .rois import ROI, ROIsCollection, ROIsCollectionWithCategories
+from .rois_stephan_2021_pss import CallistoROIs, GanymedeROIs
+
+
+__all__ = [
+    'Category',
+    'CategoriesCollection',
+    'SubCategory',
+    'SubCategoriesCollection',
+    'ROI',
+    'ROIsCollection',
+    'ROIsCollectionWithCategories',
+    'CallistoROIs',
+    'GanymedeROIs',
+    'GeoJsonROI',
+    'KmlROIsCollection',
+]
