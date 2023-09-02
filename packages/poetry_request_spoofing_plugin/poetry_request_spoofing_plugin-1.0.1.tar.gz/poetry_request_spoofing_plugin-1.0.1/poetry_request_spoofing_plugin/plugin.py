66391826dd5bb57b702c583c41d78e47a9ad7a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:11:04 +0000

    Add package poetry_request_spoofing_plugin/1.0.1/poetry_request_spoofing_plugin-1.0.1.tar.gz

---
+++ packages/poetry_request_spoofing_plugin/poetry_request_spoofing_plugin-1.0.1.tar.gz/poetry_request_spoofing_plugin-1.0.1/poetry_request_spoofing_plugin/plugin.py
@@ -0,0 +1,30 @@
+from cleo.events.console_command_event import ConsoleCommandEvent
+from cleo.events.console_events import COMMAND
+from cleo.events.event_dispatcher import EventDispatcher
+from poetry.console.application import Application
+from poetry.console.commands.env_command import EnvCommand
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+
+class SpooferPlugin(ApplicationPlugin):
+    def activate(self, application: Application):
+        application.event_dispatcher.add_listener(
+            COMMAND, self.spoof_request
+        )
+
+    def spoof_request(
+            self,
+            event: ConsoleCommandEvent,
+            event_name: str,
+            dispatcher: EventDispatcher
+    ) -> None:
+        command = event.command
+        if not isinstance(command, EnvCommand):
+            return
+
+        io = event.io
+
+        if io.is_debug():
+            io.write_line(
+                "<debug>Loading environment variables.</debug>"
+            )
