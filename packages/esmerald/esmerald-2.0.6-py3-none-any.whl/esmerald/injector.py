ce4c42769b5c34dd9bf6e37ec5190488bb437648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 04:11:17 +0000

    Add package esmerald/2.0.6/esmerald-2.0.6-py3-none-any.whl

---
+++ packages/esmerald/esmerald-2.0.6-py3-none-any.whl/esmerald/injector.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING, Any, Dict, Optional, Type
+
+from esmerald.parsers import ArbitraryHashableBaseModel
+from esmerald.transformers.datastructures import Signature
+from esmerald.typing import Void
+from esmerald.utils.helpers import is_async_callable
+
+if TYPE_CHECKING:  # pragma: no cover
+    from esmerald.typing import AnyCallable
+
+
+class Inject(ArbitraryHashableBaseModel):
+    def __init__(self, dependency: "AnyCallable", use_cache: bool = False, **kwargs: Any):
+        super().__init__(**kwargs)
+        self.dependency = dependency
+        self.signature_model: Optional["Type[Signature]"] = None
+        self.use_cache = use_cache
+        self.value: Any = Void
+
+    async def __call__(self, **kwargs: Dict[str, Any]) -> Any:
+        if self.use_cache and self.value is not Void:
+            return self.value
+
+        if is_async_callable(self.dependency):
+            value = await self.dependency(**kwargs)
+        else:
+            value = self.dependency(**kwargs)
+
+        if self.use_cache:
+            self.value = value
+
+        return value
+
+    def __eq__(self, other: Any) -> bool:
+        return other is self or (
+            isinstance(other, self.__class__)
+            and other.dependency == self.dependency
+            and other.use_cache == self.use_cache
+            and other.value == self.value
+        )
