f0233bb78c234eb452e85255668aff1fcc4fd2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 04:10:29 +0000

    Add package bitskins-v2/1.0.1/bitskins-v2-1.0.1.tar.gz

---
+++ packages/bitskins-v2/bitskins-v2-1.0.1.tar.gz/bitskins-v2-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="bitskins-v2",
+    version="1.0.1",
+    description="An unofficial wrapper for the Bitskins V2 API in Python.",
+    url="https://github.com/kasperlindau/bitskins-v2-wrapper",
+    packages=["bitskins"],
+    keywords="bitskins v2 api rest websockets"
+)
