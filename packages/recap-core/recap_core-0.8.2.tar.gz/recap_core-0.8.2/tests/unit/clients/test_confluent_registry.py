ea859d5df08c54003da6051aab6a256f3aaaa1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 05:10:20 +0000

    Add package recap-core/0.8.2/recap_core-0.8.2.tar.gz

---
+++ packages/recap-core/recap_core-0.8.2.tar.gz/recap_core-0.8.2/tests/unit/clients/test_confluent_registry.py
@@ -0,0 +1,124 @@
+from unittest.mock import MagicMock
+
+from confluent_kafka import schema_registry
+
+from recap.clients.confluent_registry import ConfluentRegistryClient
+from recap.types import IntType, StringType, StructType, UnionType
+
+
+def test_struct_avro():
+    mock_schema_registry_client = MagicMock(spec=schema_registry.SchemaRegistryClient)
+
+    class MockSchema:
+        schema_type = "AVRO"
+        schema_str = """
+        {
+          "type": "record",
+          "name": "User",
+          "fields": [
+            {"name": "name", "type": "string"},
+            {"name": "age", "type": "int"}
+          ]
+        }
+        """
+
+    class MockRegisteredSchema:
+        schema = MockSchema()
+
+    mock_schema_registry_client.get_latest_version.return_value = MockRegisteredSchema()
+
+    client = ConfluentRegistryClient(mock_schema_registry_client)
+    result = client.get_schema("dummy_topic-value")
+
+    # Check that the schema was converted correctly.
+    assert isinstance(result, StructType)
+    assert len(result.fields) == 2
+    assert result.fields[0].extra_attrs["name"] == "name"
+    assert isinstance(result.fields[0], StringType)
+    assert result.fields[1].extra_attrs["name"] == "age"
+    assert isinstance(result.fields[1], IntType)
+
+    # Check that the get_latest_version method was called with the correct subject.
+    mock_schema_registry_client.get_latest_version.assert_called_with(
+        "dummy_topic-value"
+    )
+
+
+def test_struct_protobuf():
+    mock_schema_registry_client = MagicMock(spec=schema_registry.SchemaRegistryClient)
+
+    class MockSchema:
+        schema_type = "PROTOBUF"
+        schema_str = """
+        syntax = "proto3";
+
+        message Person {
+          string name = 1;
+          int32 age = 2;
+        }
+        """
+
+    class MockRegisteredSchema:
+        schema = MockSchema()
+
+    mock_schema_registry_client.get_latest_version.return_value = MockRegisteredSchema()
+
+    client = ConfluentRegistryClient(mock_schema_registry_client)
+    result = client.get_schema("dummy_topic-value")
+
+    # Check that the schema was converted correctly.
+    assert isinstance(result, StructType)
+    assert len(result.fields) == 2
+    assert isinstance(result.fields[0], UnionType)
+    assert isinstance(result.fields[0].types[1], StringType)
+    assert isinstance(result.fields[1], UnionType)
+    assert isinstance(result.fields[1].types[1], IntType)
+
+    # Check that the get_latest_version method was called with the correct subject.
+    mock_schema_registry_client.get_latest_version.assert_called_with(
+        "dummy_topic-value"
+    )
+
+
+def test_struct_json_schema():
+    mock_schema_registry_client = MagicMock(spec=schema_registry.SchemaRegistryClient)
+
+    class MockSchema:
+        schema_type = "JSON"
+        schema_str = """
+        {
+          "$schema": "http://json-schema.org/draft-07/schema#",
+          "title": "User",
+          "type": "object",
+          "properties": {
+            "name": {
+                "type": "string"
+            },
+            "age": {
+                "type": "integer"
+            }
+          },
+          "required": ["name", "age"]
+        }
+        """
+
+    class MockRegisteredSchema:
+        schema = MockSchema()
+
+    mock_schema_registry_client.get_latest_version.return_value = MockRegisteredSchema()
+
+    client = ConfluentRegistryClient(mock_schema_registry_client)
+    result = client.get_schema("dummy_topic-value")
+
+    # Check that the schema was converted correctly.
+    assert isinstance(result, StructType)
+    assert len(result.fields) == 2
+    assert result.fields[0].extra_attrs["name"] == "name"
+    assert isinstance(result.fields[0], StringType)
+    assert result.fields[1].extra_attrs["name"] == "age"
+    assert isinstance(result.fields[1], IntType)
+
+    # Check that the get_latest_version method was called with the correct subject.
+    mock_schema_registry_client.get_latest_version.assert_called_with(
+        "dummy_topic-value"
+    )
