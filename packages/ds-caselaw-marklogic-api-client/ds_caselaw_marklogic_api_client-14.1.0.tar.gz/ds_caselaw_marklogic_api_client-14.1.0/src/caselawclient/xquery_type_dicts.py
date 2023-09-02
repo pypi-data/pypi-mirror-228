5af5e4beacd378ed3356a74723dc7f09f1a6701f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 04:12:09 +0000

    Add package ds-caselaw-marklogic-api-client/14.1.0/ds_caselaw_marklogic_api_client-14.1.0.tar.gz

---
+++ packages/ds-caselaw-marklogic-api-client/ds_caselaw_marklogic_api_client-14.1.0.tar.gz/ds_caselaw_marklogic_api_client-14.1.0/src/caselawclient/xquery_type_dicts.py
@@ -0,0 +1,183 @@
+# This file is built automatically with script/build_xquery_type_dicts.
+# DO NOT CHANGE IT MANUALLY!
+
+"""
+These classes are automatically generated using the `script/build_xquery_type_dicts` script, as part of pre-commit
+checks. They are used to enforce appropriately typed variables being passed in to MarkLogic XQuery functions.
+"""
+
+from typing import Any, Optional, TypedDict
+
+
+class MarkLogicAPIDict(TypedDict):
+    pass
+
+
+# break_judgment_checkout.xqy
+class BreakJudgmentCheckoutDict(MarkLogicAPIDict):
+    uri: str
+
+
+# checkin_judgment.xqy
+class CheckinJudgmentDict(MarkLogicAPIDict):
+    uri: str
+
+
+# checkout_judgment.xqy
+class CheckoutJudgmentDict(MarkLogicAPIDict):
+    annotation: str
+    timeout: int
+    uri: str
+
+
+# copy_document.xqy
+class CopyDocumentDict(MarkLogicAPIDict):
+    new_uri: str
+    old_uri: str
+
+
+# delete_judgment.xqy
+class DeleteJudgmentDict(MarkLogicAPIDict):
+    uri: str
+
+
+# document_collections.xqy
+class DocumentCollectionsDict(MarkLogicAPIDict):
+    uri: str
+
+
+# document_exists.xqy
+class DocumentExistsDict(MarkLogicAPIDict):
+    uri: str
+
+
+# get_judgment.xqy
+class GetJudgmentDict(MarkLogicAPIDict):
+    show_unpublished: Optional[bool]
+    uri: str
+    version_uri: Optional[str]
+
+
+# get_judgment_checkout_status.xqy
+class GetJudgmentCheckoutStatusDict(MarkLogicAPIDict):
+    uri: str
+
+
+# get_judgment_version.xqy
+class GetJudgmentVersionDict(MarkLogicAPIDict):
+    uri: str
+    version: str
+
+
+# get_last_modified.xqy
+class GetLastModifiedDict(MarkLogicAPIDict):
+    uri: str
+
+
+# get_properties_for_search_results.xqy
+class GetPropertiesForSearchResultsDict(MarkLogicAPIDict):
+    uris: list[Any]
+
+
+# get_property.xqy
+class GetPropertyDict(MarkLogicAPIDict):
+    name: str
+    uri: str
+
+
+# insert_document.xqy
+class InsertDocumentDict(MarkLogicAPIDict):
+    document: str
+    uri: str
+
+
+# list_judgment_versions.xqy
+class ListJudgmentVersionsDict(MarkLogicAPIDict):
+    uri: str
+
+
+# set_boolean_property.xqy
+class SetBooleanPropertyDict(MarkLogicAPIDict):
+    name: str
+    uri: str
+    value: str
+
+
+# set_metadata_citation.xqy
+class SetMetadataCitationDict(MarkLogicAPIDict):
+    content: str
+    uri: str
+
+
+# set_metadata_court.xqy
+class SetMetadataCourtDict(MarkLogicAPIDict):
+    content: str
+    uri: str
+
+
+# set_metadata_name.xqy
+class SetMetadataNameDict(MarkLogicAPIDict):
+    content: str
+    uri: str
+
+
+# set_metadata_this_uri.xqy
+class SetMetadataThisUriDict(MarkLogicAPIDict):
+    content_with_id: str
+    content_with_xml: str
+    content_without_id: str
+    uri: str
+
+
+# set_metadata_work_expression_date.xqy
+class SetMetadataWorkExpressionDateDict(MarkLogicAPIDict):
+    content: str
+    uri: str
+
+
+# set_property.xqy
+class SetPropertyDict(MarkLogicAPIDict):
+    name: str
+    uri: str
+    value: str
+
+
+# update_judgment.xqy
+class UpdateJudgmentDict(MarkLogicAPIDict):
+    annotation: str
+    judgment: str
+    uri: str
+
+
+# update_locked_judgment.xqy
+class UpdateLockedJudgmentDict(MarkLogicAPIDict):
+    annotation: str
+    judgment: str
+    uri: str
+
+
+# user_has_privilege.xqy
+class UserHasPrivilegeDict(MarkLogicAPIDict):
+    privilege_action: str
+    privilege_uri: str
+    user: str
+
+
+# user_has_role.xqy
+class UserHasRoleDict(MarkLogicAPIDict):
+    role: str
+    user: str
+
+
+# xslt.xqy
+class XsltDict(MarkLogicAPIDict):
+    uri: str
+
+
+# xslt_transform.xqy
+class XsltTransformDict(MarkLogicAPIDict):
+    img_location: Optional[str]
+    show_unpublished: Optional[bool]
+    uri: str
+    version_uri: Optional[str]
+    xsl_filename: Optional[str]
