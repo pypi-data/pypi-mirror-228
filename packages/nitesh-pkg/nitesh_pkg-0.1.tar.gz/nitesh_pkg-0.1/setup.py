a7a8f7c20bca90489e7583bfaff47b71ec3abd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 15:08:33 +0000

    Add package nitesh-pkg/0.1/nitesh_pkg-0.1.tar.gz

---
+++ packages/nitesh-pkg/nitesh_pkg-0.1.tar.gz/nitesh_pkg-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nitesh_pkg',
+    version='0.1',
+    description='My Python calculator Package',
+    packages=find_packages(),
+    # url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+    # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+)
