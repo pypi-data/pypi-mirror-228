ba5a74ee29b8f991cee53277f26efeac42266a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:13:26 +0000

    Add package semterm/0.5.0/semterm-0.5.0.tar.gz

---
+++ packages/semterm/semterm-0.5.0.tar.gz/semterm-0.5.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="semterm",
+    version="0.5.0",
+    description="The Semantic Terminal",
+    long_description="The Semantic Terminal",
+    author="Lambrou",
+    author_email="alexanderlambrou0602@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "semterm = semterm.main:main",
+        ],
+    },
+    install_requires=[
+        "langchain",
+        "tiktoken",
+        "pexpect",
+        "pydantic",
+        "openai",
+    ],
+)
