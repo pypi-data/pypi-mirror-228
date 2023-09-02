4369d93b830304c6ee8b3a57c96ff390cf5fba9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:11:04 +0000

    Add package minecraft-object-utils/0.3.1/minecraft_object_utils-0.3.1.tar.gz

---
+++ packages/minecraft-object-utils/minecraft_object_utils-0.3.1.tar.gz/minecraft_object_utils-0.3.1/minecraft_object_utils/__init__.py
@@ -0,0 +1,13 @@
+from .block import Block, BlockProperty, BlockTraits  # noqa: F401
+from .enchantment import Enchantment, EnchantmentTraits  # noqa: F401
+from .entity import Entity, EntityTraits  # noqa: F401
+from .inventory import Inventory  # noqa: F401
+from .item import ItemStack, ItemTraits  # noqa: F401
+from .minecraft_object_factory import (  # noqa: F401
+    BlockFactory,
+    EnchantmentFactory,
+    EntityFactory,
+    ItemFactory,
+    MinecraftObjectFactory,
+)
+from .mod_info import VANILLA_JAVA_LATEST, ModInfo  # noqa: F401
