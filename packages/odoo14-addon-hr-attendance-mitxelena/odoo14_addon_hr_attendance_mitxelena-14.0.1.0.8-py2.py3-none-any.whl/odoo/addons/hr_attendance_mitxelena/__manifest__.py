c2949158dd2305c20b3e22a770bf47683076eda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 05:09:10 +0000

    Add package odoo14-addon-hr-attendance-mitxelena/14.0.1.0.8/odoo14_addon_hr_attendance_mitxelena-14.0.1.0.8-py2.py3-none-any.whl

---
+++ packages/odoo14-addon-hr-attendance-mitxelena/odoo14_addon_hr_attendance_mitxelena-14.0.1.0.8-py2.py3-none-any.whl/odoo/addons/hr_attendance_mitxelena/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'HR Attendance Mitxelena',
+    'version': '14.0.1.0.8',
+    'category': 'Human Resources',
+    'sequence': 20,
+    'summary': 'Employee Attendances Customizations for Mitxelena',
+    'description': """
+Customizations of Employee Attendances for Mitxelena
+====================================================
+This module includes customizations for handling employee attendances at Mitxelena, 
+including custom rules for shifts, public holidays and calculation of extra hours.
+""",
+    'website': 'https://www.coopdevs.org',
+    'author': 'Coopdevs',
+    'depends': [
+        'base',
+        'hr',
+        'hr_attendance',
+        'hr_holidays_public',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_attendance_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
