7ee2c7faa9dd50ce25d2247cab414c49d475cdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:12:44 +0000

    Add package rayvision-sync/2.8.8.6/rayvision_sync-2.8.8.6-py2.py3-none-any.whl

---
+++ packages/rayvision-sync/rayvision_sync-2.8.8.6-py2.py3-none-any.whl/rayvision_sync/rayvision_raysync/constants.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+"""Constant information about the sync."""
+
+PACKAGE_NAME = 'rayvision_raysync'
+
+HEADERS = {
+            'Content-Type': 'application/json'
+        }
+
+RESPONSE = {
+    "ready": 100,
+    "start": 101,
+    "stopped": 102,
+    "successful": 0,
+    "failed": 500,
+    "auth-failed": 201,
+    "proxy-closed": 202,
+    "idle": 300,
+    "stop-by-server": 301,
+    "no-permission": 405,
+    "ip-locked": 205,
+    "account-locked": 209
+}
+
+DOMAIN = "http://127.0.0.1"
+
+RAYSYNC_NAME = {
+    "Windows": "Raysync Client",
+    "Linux": "Raysync"
+}
+
+RAYSYNCEXE = {
+    "Windows": "Raysync-sdk.exe",
+    "Linux": "Raysync-sdk"
+}
+
+STATU_SLEEP = 5
