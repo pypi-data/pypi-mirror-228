36c22e1de99e053984c1044e27f8fc9298ef0a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 06:11:02 +0000

    Add package semterm/0.4.0/semterm-0.4.0.tar.gz

---
+++ packages/semterm/semterm-0.4.0.tar.gz/semterm-0.4.0/semterm/main.py
@@ -0,0 +1,13 @@
+from semterm.config.Config import Config
+from semterm.agent.MrklAgent import MrklAgent
+from semterm.UI.UserInterface import UserInterface
+
+
+def main():
+    config = Config()
+    agent = MrklAgent(config)
+    UserInterface(agent).start()
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
