d52e04864118a51fa78b50f47596273ab8b1c7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 05:08:38 +0000

    Add package chainlit/0.6.402/chainlit-0.6.402.tar.gz

---
+++ packages/chainlit/chainlit-0.6.402.tar.gz/chainlit-0.6.402/chainlit/playground/config.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+from chainlit.playground.provider import BaseProvider
+from chainlit.playground.providers import (
+    Anthropic,
+    AzureChatOpenAI,
+    AzureOpenAI,
+    ChatOpenAI,
+    OpenAI,
+)
+
+providers = {
+    AzureChatOpenAI.id: AzureChatOpenAI,
+    AzureOpenAI.id: AzureOpenAI,
+    ChatOpenAI.id: ChatOpenAI,
+    OpenAI.id: OpenAI,
+    Anthropic.id: Anthropic,
+}  # type: Dict[str, BaseProvider]
+
+
+def has_llm_provider(id: str):
+    return id in providers
+
+
+def add_llm_provider(provider: BaseProvider):
+    if not provider.is_configured():
+        raise ValueError(
+            f"{provider.name} LLM provider requires the following environment variables: {', '.join(provider.env_vars.values())}"
+        )
+    providers[provider.id] = provider
+
+
+def get_llm_providers():
+    return [provider for provider in providers.values() if provider.is_configured()]
