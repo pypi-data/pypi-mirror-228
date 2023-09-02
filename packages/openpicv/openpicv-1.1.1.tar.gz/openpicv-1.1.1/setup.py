f4747f94193db6e0a337809d79e33372f23331cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 15:08:38 +0000

    Add package openpicv/1.1.1/openpicv-1.1.1.tar.gz

---
+++ packages/openpicv/openpicv-1.1.1.tar.gz/openpicv-1.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='openpicv',
+    version='1.1.1',
+    description='Your package description',
+    author='zqs',
+    author_email='your-email@example.com',
+    packages=['openpicv'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
