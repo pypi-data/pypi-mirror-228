a81b56e7dd5a259016af35846164a14ea9d7740c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 04:12:03 +0000

    Add package pyzjr/1.1.3.1/pyzjr-1.1.3.1.tar.gz

---
+++ packages/pyzjr/pyzjr-1.1.3.1.tar.gz/pyzjr-1.1.3.1/pyzjr/dlearn/__init__.py
@@ -0,0 +1,9 @@
+"""
+Training strategies for deep learning
+- author:Auorui(夏天是冰红茶)
+- creation time:2023.9
+"""
+from .backbone import *
+from .strategy import *
+from .voc import *
+from .callbacks import *
