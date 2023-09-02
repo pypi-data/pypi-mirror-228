83a01dd28d572b8e83674b07d637581ccf2fd706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 05:10:12 +0000

    Add package via-api/2.11.0/via-api-2.11.0.tar.gz

---
+++ packages/via-api/via-api-2.11.0.tar.gz/via-api-2.11.0/src/via/geojson/retrieve.py
@@ -0,0 +1,37 @@
+import datetime
+
+from via.settings import MAX_GEOJSON_AGE
+from via.db import db
+
+
+def get_geojson(
+    journey_type: str,
+    earliest_time: int = None,
+    latest_time: int = None,
+    place: str = None,
+    version: str = None,
+    version_op: str = None,
+) -> dict:
+    # TODO: react to version/version_op/earliest_time/latest_time
+
+    if journey_type is None:
+        journey_type = "all"
+
+    data = db.parsed_journeys.find_one(
+        {
+            "journey_type": journey_type,
+            "save_time": {
+                "$gt": (
+                    datetime.datetime.utcnow()
+                    - datetime.timedelta(seconds=MAX_GEOJSON_AGE)
+                ).timestamp()
+            },
+            "place": place,
+        }
+    )
+    if not data:
+        raise LookupError()
+
+    data["_id"] = str(data["_id"])
+
+    return data
