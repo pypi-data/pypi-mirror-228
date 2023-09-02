a8d76da5b6355de2848a07074366e8ca513f7520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-02 03:10:49 +0000

    Add package ExpoSeq/3.0.6/ExpoSeq-3.0.6-py3-none-any.whl

---
+++ packages/ExpoSeq/ExpoSeq-3.0.6-py3-none-any.whl/ExpoSeq/augment_data/binding_data.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import os
+
+try:
+    import tkinter as tk
+    from tkinter import filedialog
+except:
+    pass
+
+def collect_binding_data(binding_data = None):
+    if binding_data is None:
+        binding_data = pd.DataFrame([])
+    else:
+        pass
+    second_prompt = False
+    while True:
+        # prompt the user to add a file
+        if not second_prompt:
+            print("You can either add an excel sheet or a csv file which contains the binding data. Note: the first column must contain the CDR3 sequences and its column name has to be aaSeqCDR3.")
+        else:
+            print("Please modify the file and choose it again.")
+        try:
+            binding_file = filedialog.askopenfilename()
+        except:
+            while True:
+                binding_file = input("copy and paste the path to your binding report")
+                if os.path.isfile(os.path.abspath(binding_file)):
+                    break
+                else:
+                    print("Please enter a valid filepath. ")
+
+        if binding_file.endswith(".xlsx") or binding_file.endswith(".csv"):
+            if binding_file.endswith(".xlsx"):
+                binding_new = pd.read_excel(binding_file)
+            elif binding_file.endswith(".csv"):
+                binding_new = pd.read_csv(binding_file)
+            if binding_new.columns.to_list()[0] == "aaSeqCDR3":
+                pass
+                second_prompt = False
+            else:
+                second_prompt = True
+                print("Please change the header of the first column in your csv file to aaSeqCDR3")
+        else:
+            print("Please enter a valid filepath to a csv or xlsx file")
+            second_prompt = True
+
+        if not second_prompt:
+            binding_data = pd.concat([binding_data, binding_new])
+            response = input("Do you want to continue adding files? (Y/n) ")
+            if response.lower() == "n":
+                break
+            print("The first five rows of your binding data look like this:")
+            print(binding_data.head(5))
+    return binding_data
