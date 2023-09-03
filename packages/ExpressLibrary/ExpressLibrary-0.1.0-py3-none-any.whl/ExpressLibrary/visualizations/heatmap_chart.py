f6dd976bc3e886e8bb642946553ed501b0f2bb43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:12 +0000

    Add package ExpressLibrary/0.1.0/ExpressLibrary-0.1.0-py3-none-any.whl

---
+++ packages/ExpressLibrary/ExpressLibrary-0.1.0-py3-none-any.whl/ExpressLibrary/visualizations/heatmap_chart.py
@@ -0,0 +1,22 @@
+import plotly.express as px
+from .base_heatmap import BaseHeatmapChart
+
+class HeatmapChart(BaseHeatmapChart):
+    def __init__(self,data, title="", color_scheme="Viridis"):
+        super().__init__(title=title)
+        self.data = data
+        self.color_scheme = color_scheme
+    
+
+    def set_color_scheme(self, color_scheme):
+        self.color_scheme = color_scheme
+    
+    def render(self):
+
+        fig = px.imshow(self.data, labels=dict(x="X Axis", y="Y Axis", color="Value"),
+                        title=self.title, 
+                        color_continuous_scale=self.color_scheme)
+        fig.show()
+
+
+
