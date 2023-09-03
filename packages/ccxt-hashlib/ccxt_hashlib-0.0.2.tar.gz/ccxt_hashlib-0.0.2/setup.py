710af6d34886eea4a3393bb70df3a5fbb1c17a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:40 +0000

    Add package ccxt-hashlib/0.0.2/ccxt_hashlib-0.0.2.tar.gz

---
+++ packages/ccxt-hashlib/ccxt_hashlib-0.0.2.tar.gz/ccxt_hashlib-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name = "ccxt_hashlib",
+    version = "0.0.2",
+    author = "author",
+    description = "hash functions for ccxt-lib",
+    packages = ["src"]
+)
