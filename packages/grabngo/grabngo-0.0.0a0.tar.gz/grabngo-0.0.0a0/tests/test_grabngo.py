b5d0fb83a7a52abacd6f2b8bad89a33fbd715f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:32 +0000

    Add package grabngo/0.0.0a0/grabngo-0.0.0a0.tar.gz

---
+++ packages/grabngo/grabngo-0.0.0a0.tar.gz/grabngo-0.0.0a0/tests/test_grabngo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `grabngo` package."""
+
+
+import unittest
+
+from grabngo import grabngo
+
+
+class TestGrabngo(unittest.TestCase):
+    """Tests for `grabngo` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
