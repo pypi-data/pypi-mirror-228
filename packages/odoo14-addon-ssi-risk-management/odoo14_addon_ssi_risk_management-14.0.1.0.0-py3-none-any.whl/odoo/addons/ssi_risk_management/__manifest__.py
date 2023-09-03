41946cc6ba4fcdaa95ad5238f0af7ad5ff307d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:15 +0000

    Add package odoo14-addon-ssi-risk-management/14.0.1.0.0/odoo14_addon_ssi_risk_management-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-risk-management/odoo14_addon_ssi_risk_management-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_risk_management/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Risk Managemenet",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "configuration_helper",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "menu.xml",
+    ],
+    "demo": [],
+}
