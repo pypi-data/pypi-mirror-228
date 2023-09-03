7f2f449b77d070240bb6b77f9703a245b1e0de83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:43 +0000

    Add package py-rally-sdk/1.1.1/py_rally_sdk-1.1.1.tar.gz

---
+++ packages/py-rally-sdk/py_rally_sdk-1.1.1.tar.gz/py_rally_sdk-1.1.1/py_rally/constants.py
@@ -0,0 +1,38 @@
+EIP712TYPE = [
+    {'name': 'name', 'type': 'string'},
+    {'name': 'version', 'type': 'string'},
+    {'name': 'chainId', 'type': 'uint256'},
+    {'name': 'verifyingContract', 'type': 'address'},
+]
+EIP712_SALT_TYPE = [
+    {'name': 'name', 'type': 'string'},
+    {'name': 'version', 'type': 'string'},
+    {'name': 'verifyingContract', 'type': 'address'},
+    {'name': 'salt', 'type': 'bytes32'},
+]
+FORWARDER_DATA_SIGNED_TYPE = [
+    {'name': 'from', 'type': 'address'},
+    {'name': 'to', 'type': 'address'},
+    {'name': 'value', 'type': 'uint256'},
+    {'name': 'gas', 'type': 'uint256'},
+    {'name': 'nonce', 'type': 'uint256'},
+    {'name': 'data', 'type': 'bytes'},
+    {'name': 'validUntilTime', 'type': 'uint256'},
+]
+RELAY_DATA_SIGNED_TYPE = [
+    {'name': 'maxFeePerGas', 'type': 'uint256'},
+    {'name': 'maxPriorityFeePerGas', 'type': 'uint256'},
+    {'name': 'transactionCalldataGasUsed', 'type': 'uint256'},
+    {'name': 'relayWorker', 'type': 'address'},
+    {'name': 'paymaster', 'type': 'address'},
+    {'name': 'forwarder', 'type': 'address'},
+    {'name': 'paymasterData', 'type': 'bytes'},
+    {'name': 'clientId', 'type': 'uint256'},
+]
+RELAY_REQUEST_SIGNED_TYPE = [
+    *FORWARDER_DATA_SIGNED_TYPE,
+    {'name': 'relayData', 'type': 'RelayData'},
+]
+HASH_ZERO = '0x0000000000000000000000000000000000000000000000000000000000000000'
+DOMAIN_SEPARATOR_VERSION = '3'
+OK_RESPONSE = 200
