487e8b9afe73d1264ee14c2067d136e813bd603e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:35 +0000

    Add package hydrochecksum/1.0.0/hydrochecksum-1.0.0.tar.gz

---
+++ packages/hydrochecksum/hydrochecksum-1.0.0.tar.gz/hydrochecksum-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hydrochecksum',
+    version='1.0.0',
+    description="""Hydrochecksum - New horizons of checksums.""",
+    author='Nelyon',
+    author_email='pauline.darrieux@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
