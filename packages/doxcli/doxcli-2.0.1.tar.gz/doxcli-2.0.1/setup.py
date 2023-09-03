d03fc9cdc50620b5e61d31e80bc4edcbcdc43f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:50 +0000

    Add package doxcli/2.0.1/doxcli-2.0.1.tar.gz

---
+++ packages/doxcli/doxcli-2.0.1.tar.gz/doxcli-2.0.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os
+import re
+from setuptools import setup
+
+
+def read(path):
+    return open(path, 'r').read()
+
+
+def find_version():
+    here = os.path.abspath(os.path.dirname(__file__))
+    version_file = os.path.join(here, 'doxcli', '__init__.py')
+    with open(version_file, 'r', encoding='utf8') as f:
+        content = f.read().strip()
+        version = re.search(r'__version__ = \'(\d+.\d+.\d+)', content, re.M)
+        return version.group().split('\'')[1]
+
+
+install_requires = [
+    'PyYAML==6.0',
+]
+
+setup_options = dict(
+    name='doxcli',
+    version="2.0.1",
+    description='Cli to create project structure',
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    author='Bijay Das',
+    author_email='me@bijaydas.com',
+    url='https://github.com/bijaydas/doxcli',
+    scripts=['cli.py'],
+    install_requires=install_requires,
+    package_dir={'': 'doxcli'},
+    entry_points='''
+        [console_scripts]
+        dox=cli:main
+    ''',
+    license='MIT License',
+    extras_require={},
+    python_requires='>=3.0',
+    project_urls={
+        'Source': 'https://github.com/bijaydas/doxcli',
+    },
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent"
+     ],
+)
+
+setup(**setup_options)
