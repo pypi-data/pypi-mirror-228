76aeeefd6920d25e9648e68c27975292203a309c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:44 +0000

    Add package gofile-api/0.0.3/gofile-api-0.0.3.tar.gz

---
+++ packages/gofile-api/gofile-api-0.0.3.tar.gz/gofile-api-0.0.3/CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 0.0.1
+First upload
+
+## 0.0.2
+Added more documentation
+
+## 0.0.3
+Fixed long description on PyPi
---
+++ packages/gofile-api/gofile-api-0.0.3.tar.gz/gofile-api-0.0.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3 :: Only",
+    "Natural Language :: English",
+]
+
+KEYWORDS = ["python", "gofile", "api", "gofileio", "gofile.io"]
+
+DEPENDENCIES = ["requests"]
+
+DESCRIPTION = "A python library for communicating with the Gofile API."
+LONG_DESCRIPTION = """--------------------GoFile API--------------------
+-----Description-----
+A python library for communicating with the Gofile API.
+
+-----Features-----
+Get an available server.
+Upload a file, to a directory, or to the root folder.
+Create a guest account.
+Get contents of a folder.
+Create a folder.
+Get the account information.
+Set option for a content id."""
+
+setup(
+    name="gofile-api",
+    version="0.0.3",
+    description="A python library for communicating with the Gofile API.",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/objectiveSquid/GoFile-API",
+    author="Magnus Zahle",
+    author_email="objectivesquid@outlook.com",
+    license="MIT",
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    packages=find_packages(),
+    install_requires=DEPENDENCIES,
+)
