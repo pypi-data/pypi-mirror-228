ceb674b6f200db4d995dd534b1ab9dbc598d99f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:52 +0000

    Add package fandabidozy/0.1.0/fandabidozy-0.1.0.tar.gz

---
+++ packages/fandabidozy/fandabidozy-0.1.0.tar.gz/fandabidozy-0.1.0/fandabidozy/mathops.py
@@ -0,0 +1,20 @@
+import math
+
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+def sqrt(x):
+    return math.sqrt(x)
+
+def pow(x, y):
+    return math.pow(x, y)
