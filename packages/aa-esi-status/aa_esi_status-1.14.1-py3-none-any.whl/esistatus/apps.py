d58041dbf5cc1128ffb4f885cd1f5a68dde47550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:53 +0000

    Add package aa-esi-status/1.14.1/aa_esi_status-1.14.1-py3-none-any.whl

---
+++ packages/aa-esi-status/aa_esi_status-1.14.1-py3-none-any.whl/esistatus/apps.py
@@ -0,0 +1,19 @@
+"""
+App config
+"""
+
+# Django
+from django.apps import AppConfig
+
+# AA ESI Status
+from esistatus import __version__
+
+
+class AaEsiStatusConfig(AppConfig):
+    """
+    Application config
+    """
+
+    name = "esistatus"
+    label = "esistatus"
+    verbose_name = f"ESI Status v{__version__}"
