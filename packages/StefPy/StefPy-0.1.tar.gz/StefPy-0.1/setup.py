7cb04b712f599b0cca4db1a6558e47ef0e02fe5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:37 +0000

    Add package StefPy/0.1/StefPy-0.1.tar.gz

---
+++ packages/StefPy/StefPy-0.1.tar.gz/StefPy-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'StefPy',
+    version='0.1',
+    author= 'ZaidPytele',
+    description='THE KING PYTHON PRO',
+    packages= ['StefPy'],
+    author_email='fhdhasd39@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
