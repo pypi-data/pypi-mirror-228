032b97d94eeb134eb2dd819191b5352db37b79c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:16 +0000

    Add package syntheon/0.0.2/syntheon-0.0.2.tar.gz

---
+++ packages/syntheon/syntheon-0.0.2.tar.gz/syntheon-0.0.2/test/test_inferencer.py
@@ -0,0 +1,34 @@
+import os
+import glob
+from syntheon import infer_params
+
+
+def test_dexed_inferencer():
+    """
+    just check if everything runs well for Dexed
+    """
+    output_params_file, eval_dict = infer_params(
+        "test/test_audio/dexed_test_audio_1.wav", 
+        "dexed", 
+        enable_eval=True
+    )
+    assert os.path.exists(output_params_file)
+
+    os.remove(output_params_file)
+
+
+def test_vital_inferencer_1():
+    """
+    just check if everything runs well for Vital
+    """
+    loss_lst = [0.11, 0.06, 0.37, 0.42, 0.18, 0.15]
+    audios = sorted(glob.glob("test/test_audio/vital_*.wav"))
+    for i in range(len(audios)):
+        output_params_file, eval_dict = infer_params(
+            audios[i],
+            "vital", 
+            enable_eval=True
+        )
+        assert os.path.exists(output_params_file)
+        assert eval_dict["loss"] < loss_lst[i]
+        os.remove(output_params_file)
