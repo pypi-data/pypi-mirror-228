7a30e06cc0f3cd20467fca8d68ea66dd67f2c23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:50 +0000

    Add package prodiapy/3.6/prodiapy-3.6.tar.gz

---
+++ packages/prodiapy/prodiapy-3.6.tar.gz/prodiapy-3.6/prodia/__init__.py
@@ -0,0 +1,6 @@
+from .sync_prodia import Client
+from .async_prodia import AsyncClient
+from .constants import *
+from .utils import *
+
+__version__ = "3.5"
---
+++ packages/prodiapy/prodiapy-3.6.tar.gz/prodiapy-3.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='prodiapy',
+version='3.6',
+url='https://github.com/zenafey/prodiapy',
+description='Prodia API Python Wrapper',
+long_description=long_description,
+long_description_content_type='text/markdown',
+author='zenafey',
+author_email='zenafey@eugw.ru',
+packages=['prodia'],
+license='MIT',
+install_requires=['requests', 'Pillow', 'colorama', 'aiohttp'])
