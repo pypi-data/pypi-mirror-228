9ed1c73762452a0e51d141cc2f4dac8aef3d70d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:54 +0000

    Add package pythonic_archive_kit/1.1.0/pythonic_archive_kit-1.1.0.tar.gz

---
+++ packages/pythonic_archive_kit/pythonic_archive_kit-1.1.0.tar.gz/pythonic_archive_kit-1.1.0/pythonic_archive_kit/__init__.py
@@ -0,0 +1,16 @@
+"""The Pythonic Archive Kit"""
+from .utils import __VERSION_STR__ as __version__
+
+try:
+    from . import pak
+except ImportError:
+    from . import base as pak
+    print(
+            "WARNING: cryptography is not installed. PAK files will not be encrypted.",
+            "To install cryptography, run `pip install cryptography`",
+            sep = "\n"
+    )
+
+open_pak = pak.open_pak
+load_pak = pak.load_pak
+save_pak = pak.save_pak
