1e3a1da4f273a4e9b6c50c928e8c39c9c3a64d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:38 +0000

    Add package aa-timezones/1.16.1/aa_timezones-1.16.1.tar.gz

---
+++ packages/aa-timezones/aa_timezones-1.16.1.tar.gz/aa_timezones-1.16.1/timezones/__init__.py
@@ -0,0 +1,11 @@
+"""
+Application init
+"""
+
+# Standard Library
+from importlib import metadata
+
+__version__ = metadata.version("aa-timezones")
+__title__ = "Time Zones"
+
+del metadata
---
+++ packages/aa-timezones/aa_timezones-1.16.1.tar.gz/aa_timezones-1.16.1/timezones/apps.py
@@ -0,0 +1,19 @@
+"""
+App config
+"""
+
+# Django
+from django.apps import AppConfig
+
+# AA Time Zones
+from timezones import __version__
+
+
+class AaTimezonesConfig(AppConfig):
+    """
+    AaTimezonesConfig
+    """
+
+    name: str = "timezones"
+    label: str = "timezones"
+    verbose_name: str = f"Timezones v{__version__}"
