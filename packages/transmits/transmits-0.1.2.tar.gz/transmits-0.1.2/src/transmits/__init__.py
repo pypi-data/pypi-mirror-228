cdbbb0675e1a261055888abdd6e5595ace5b7175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:09:25 +0000

    Add package transmits/0.1.2/transmits-0.1.2.tar.gz

---
+++ packages/transmits/transmits-0.1.2.tar.gz/transmits-0.1.2/src/transmits/__init__.py
@@ -0,0 +1,1 @@
+from .transmits import Transmit
---
+++ packages/transmits/transmits-0.1.2.tar.gz/transmits-0.1.2/test/test_transmit.py
@@ -0,0 +1,12 @@
+import unittest
+from transmitS import Transmit
+
+class DataTransmit(unittest.TestCase):
+
+    def test_data_transmission(self):
+        biu = Transmit()
+        biu.to.data1 = 5
+        self.assertEqual(biu.me.data1, 5)
+
+if __name__ == '__main__':
+    unittest.main()
