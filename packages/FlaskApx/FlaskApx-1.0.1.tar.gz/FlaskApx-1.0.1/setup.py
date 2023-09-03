d03d901caf1cd9751a9674c10518dec7b07c227b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:19 +0000

    Add package FlaskApx/1.0.1/FlaskApx-1.0.1.tar.gz

---
+++ packages/FlaskApx/FlaskApx-1.0.1.tar.gz/FlaskApx-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+with open("README.rst") as f:
+    readme = f.read()
+
+github_url = "https://github.com/Nux-xader/Flask-Apx"
+
+
+setup(
+    name="FlaskApx",
+    url=github_url,
+    project_urls={
+        "Github": github_url
+    },
+    author="Nux xader A.K.A Satria Rahmat",
+    python_requires=">=3.7, <4",
+    version="1.0.1",
+    keywords=[
+        "flask helper", "flask",
+        "API heler", "API Toolkit",
+        "API warper"
+    ],
+    description="The helper for Flask",
+    long_description=readme,
+    packages=["apx"],
+    install_requires=requirements
+)
