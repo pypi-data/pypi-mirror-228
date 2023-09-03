1c2877551d0f65bf6920b3c194f510a84aa1f48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:58 +0000

    Add package ops-shuv/0.3/ops_shuv-0.3-py3-none-any.whl

---
+++ packages/ops-shuv/ops_shuv-0.3-py3-none-any.whl/operations/add.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    print('this is the addition',a+b)
+def sub(a,b):
+    print('this is the subtraction',a-b)
+def mul(a,b):
+    print('this is the multiplication',a*b)
+def div(a,b):
+    print('this is the division', a//b)
+
+
+
