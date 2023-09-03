57bf2061c53a96aa49d6f3589050823ef69fc190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:36 +0000

    Add package sprit/0.1.11/sprit-0.1.11.tar.gz

---
+++ packages/sprit/sprit-0.1.11.tar.gz/sprit-0.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sprit",
+    author= "Riley Balikian",
+    author_email = "balikian@illinois.edu",
+    version="0.1.11",
+    install_requires=["obspy", "scipy", "matplotlib", "pandas", "numpy", "pyqt5", "pyproj"],
+    description="A package for processing and analyzing HVSR (Horizontal to Vertical Spectral Ratio) data",
+    package_data={'sprit': ['resources/*', 'resources/themes/*', 'resources/themes/forest-dark/*', 'resources/themes/forest-light/*']}
+    )
