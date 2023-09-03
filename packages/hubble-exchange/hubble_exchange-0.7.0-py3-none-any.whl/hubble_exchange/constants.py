19da457b522e5d13f5b42906ca157df350fada68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:14 +0000

    Add package hubble-exchange/0.7.0/hubble_exchange-0.7.0-py3-none-any.whl

---
+++ packages/hubble-exchange/hubble_exchange-0.7.0-py3-none-any.whl/hubble_exchange/constants.py
@@ -0,0 +1,9 @@
+from eth_typing import Address
+
+OrderBookContractAddress = Address("0x0300000000000000000000000000000000000000")
+IOCBookContractAddress = Address("0x635c5F96989a4226953FE6361f12B96c5d50289b")
+ClearingHouseContractAddress = Address("0x0300000000000000000000000000000000000002")
+
+CHAIN_ID = 321123
+MAX_GAS_LIMIT = 7_000_000  # 7 million
+GAS_PER_ORDER = 300_000  # 300k
