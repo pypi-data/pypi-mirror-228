4ae0b5cee9f512fe344c46c808b4e03b2879b7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:12 +0000

    Add package d4train/0.0.2.1/d4train-0.0.2.1.tar.gz

---
+++ packages/d4train/d4train-0.0.2.1.tar.gz/d4train-0.0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(name="d4train",
+      version="0.0.2.1",
+      description="this contains some kind operations in it.",
+      author="shuvam mandal",
+      author_email="shuvammandal121@gmail.com",
+      packages=["d4train"],
+      long_description= long_description,
+      long_description_content_type = "text/markdown",
+      install_requires = ['datasets','transformers','pandas'],
+      
+      )
