220133ae051665d87c1f30ae8be8380d8f4c3f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:16 +0000

    Add package MediaWorld/2023.9.1/MediaWorld-2023.9.1.tar.gz

---
+++ packages/MediaWorld/MediaWorld-2023.9.1.tar.gz/MediaWorld-2023.9.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MediaWorld",
+    version="2023.9.1",
+    author="anzechannel",
+    author_email='348834851@qq.com',
+    description="这是一个关于多媒体的软件包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
