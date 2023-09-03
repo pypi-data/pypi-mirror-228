9f8162c9aa9d481260d3c88529942b71f7ce6aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:12 +0000

    Add package SavConverter/0.1/SavConverter-0.1.tar.gz

---
+++ packages/SavConverter/SavConverter-0.1.tar.gz/SavConverter-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SavConverter',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Ryan Grissett',
+    author_email='ryan.grissett@hotmail.com',
+    description="Python-GVAS-JSON-Converter (SavConverter) is a library designed to convert Unreal Engine's Game Variable and Attribute System (GVAS) files between .sav and .json formats.",
+    url='https://github.com/afkaf/Python-GVAS-JSON-Converter',
+    license='Unlicense',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
