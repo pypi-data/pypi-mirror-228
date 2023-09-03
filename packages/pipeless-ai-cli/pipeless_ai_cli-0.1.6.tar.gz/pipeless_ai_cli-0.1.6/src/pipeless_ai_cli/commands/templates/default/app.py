531fb41a74fd92c9879d7f64ead3410881b3381c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:28 +0000

    Add package pipeless-ai-cli/0.1.6/pipeless_ai_cli-0.1.6.tar.gz

---
+++ packages/pipeless-ai-cli/pipeless_ai_cli-0.1.6.tar.gz/pipeless_ai_cli-0.1.6/src/pipeless_ai_cli/commands/templates/default/app.py
@@ -0,0 +1,36 @@
+from pipeless_ai.lib.app.app import PipelessApp
+
+class App(PipelessApp):
+    """
+    Main application class.
+
+    Pre-process, process and post-process hooks, if implemented,
+    must return a RGB image.
+
+    The context can be accessed and modified at any stage of the pipeline.
+    You can use it to share data between stages or pipeline iterations (i.e
+    between the processing of different frames)
+    """
+
+    # Hook to execute before the processing of the first image
+    def before(self, ctx):
+        pass
+
+    # Hook to execute to pre-process each image
+    def pre_process(self, frame, ctx):
+        modified_frame = frame # Do something to the frame
+        return modified_frame
+
+    # Hook to execute to process each image
+    def process(self, frame, ctx):
+        modified_frame = frame # Do something to the frame
+        return modified_frame
+
+    # Hook to execute after processing each image
+    def post_process(self, frame, ctx):
+        modified_frame = frame # Do something to the frame
+        return modified_frame
+
+    # Hook to execute after the processing of the last image
+    def after(self, ctx):
+        pass
