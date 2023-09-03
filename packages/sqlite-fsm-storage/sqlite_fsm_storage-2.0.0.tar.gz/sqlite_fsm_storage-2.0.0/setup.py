0a5c1f0a13dfd78e98ba490e5208f11732e50aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:22 +0000

    Add package sqlite-fsm-storage/2.0.0/sqlite_fsm_storage-2.0.0.tar.gz

---
+++ packages/sqlite-fsm-storage/sqlite_fsm_storage-2.0.0.tar.gz/sqlite_fsm_storage-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='sqlite_fsm_storage',
+    version='2.0.0',
+    author='EgorBlaze',
+    author_email='blazeegor@gmail.com',
+    description='SQLiteStorage is a very good FSM Storage for Telegram bots.',
+    long_description='This is FSM Storage for Telegram bots on aiogram.',
+    license='Apache License, Version 2.0, see LICENSE file',
+    packages=['sqlite_fsm_storage'],
+    install_requires=['aiogram']
+)
