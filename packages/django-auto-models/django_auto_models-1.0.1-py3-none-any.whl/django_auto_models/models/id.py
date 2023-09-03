23d98aeba2317f2c97412ac3a4ddc8a3ed520b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:22 +0000

    Add package django-auto-models/1.0.1/django_auto_models-1.0.1-py3-none-any.whl

---
+++ packages/django-auto-models/django_auto_models-1.0.1-py3-none-any.whl/django_auto_models/models/id.py
@@ -0,0 +1,53 @@
+from uuid import uuid4
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+__all__ = [
+    'AutoIDModel',
+    'AutoBigIDModel',
+    'AutoUUIDModel',
+]
+
+
+class AutoIDModel(models.Model):
+    """ explicit declaration with AutoField """
+
+    id = models.AutoField(
+        auto_created=True,
+        verbose_name=_('ID'),
+        editable=False,
+        serialize=False,
+        primary_key=True)
+
+    class Meta:
+        abstract = True
+
+
+class AutoBigIDModel(models.Model):
+    """ explicit declaration with BigAutoField """
+
+    id = models.BigAutoField(
+        auto_created=True,
+        verbose_name=_('ID'),
+        editable=False,
+        serialize=False,
+        primary_key=True)
+
+    class Meta:
+        abstract = True
+
+
+class AutoUUIDModel(models.Model):
+    """ swap id from (Big)AutoFiled to UUIDField """
+
+    id = models.UUIDField(
+        auto_created=True,
+        verbose_name=_('UUID'),
+        editable=False,
+        serialize=False,
+        primary_key=True,
+        default=uuid4)
+
+    class Meta:
+        abstract = True
