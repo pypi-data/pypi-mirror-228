8be8fea5bf7d14bc06688d11945ce33c77e2d28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 03:08:48 +0000

    Add package Jvav/1.2.35/Jvav-1.2.35.tar.gz

---
+++ packages/Jvav/Jvav-1.2.35.tar.gz/Jvav-1.2.35/jvav/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+from jvav.utils import (
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+    SjsUtil,
+)
+
+__version__ = "1.2.35"
+
+VERSION = __version__
+
+__all__ = [
+    VERSION,
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+    SjsUtil,
+]
