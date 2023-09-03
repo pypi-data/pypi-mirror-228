611f08c05fbace219ca65c896399484f863ebcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:58 +0000

    Add package ops-shuv/0.3/ops-shuv-0.3.tar.gz

---
+++ packages/ops-shuv/ops-shuv-0.3.tar.gz/ops-shuv-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="ops-shuv",
+      version="0.3",
+      description="this contains some kind operations in it.",
+      author="shuvam mandal",
+      author_email="shuvammandal121@gmail.com",
+      packages=["ops-shuv"],
+      requires=[],
+      )
