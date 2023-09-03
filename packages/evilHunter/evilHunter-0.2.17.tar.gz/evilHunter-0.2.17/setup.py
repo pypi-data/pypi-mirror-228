e950db88c9119c0fa5b242da7bbf025f42529ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:36 +0000

    Add package evilHunter/0.2.17/evilHunter-0.2.17.tar.gz

---
+++ packages/evilHunter/evilHunter-0.2.17.tar.gz/evilHunter-0.2.17/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="evilHunter",
+    version="0.2.17",
+    description="Cracking WiFi(KCRACK)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="an0mal1a",
+    url="https://github.com/an0mal1a/evilHunter",
+    author_email="pablodiez024@proton.me",
+    packages=["words", "C"],
+    scripts=["evilHunter.py", "evilCracker.py", "C/evilCracker.c"],
+    install_requires=[
+        'colorama',
+        'tqdm',
+],
+
+)
