30255daac7f63ee8d9ab2eb30ef060cfd4a75745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:09:30 +0000

    Add package cloud-bunny/0.12/cloud_bunny-0.12.tar.gz

---
+++ packages/cloud-bunny/cloud_bunny-0.12.tar.gz/cloud_bunny-0.12/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloud_bunny',
+    version='0.12',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'numpy',
+        'open3d',
+        'pye57',
+        'pytest',
+        'warp-lang',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
