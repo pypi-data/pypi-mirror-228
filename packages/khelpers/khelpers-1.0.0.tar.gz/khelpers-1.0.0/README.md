6ae99bded4308a9ead65db76ecdc7109d407e580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:10:27 +0000

    Add package khelpers/1.0.0/khelpers-1.0.0.tar.gz

---
+++ packages/khelpers/khelpers-1.0.0.tar.gz/khelpers-1.0.0/README.md
@@ -0,0 +1,3 @@
+# KHelpers-Python
+ 
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/c29f2120621640d89c233cbb4fc7f3c0)](https://app.codacy.com/gh/kumpeapps/KHelpers-Python/dashboard?utm_source=gh&utm_medium=referral&utm_content=&utm_campaign=Badge_grade)
---
+++ packages/khelpers/khelpers-1.0.0.tar.gz/khelpers-1.0.0/khelpers/config.py
@@ -0,0 +1,16 @@
+import pip
+
+
+def predepends(packages):
+    if isinstance(packages, list):
+        package_list = packages
+    elif isinstance(packages, str):
+        package_list = [packages]
+    else:
+        TypeError("Packages var must be str or list.")
+
+    for package in package_list:
+        try:
+            __import__(package)
+        except ImportError:
+            pip.main(['install', package])
