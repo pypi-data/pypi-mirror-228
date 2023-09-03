76fe86941388890a5255eb2ce5acb9fb9154505e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 14:07:28 +0000

    Add package aurask-ai/0.1.0/aurask_ai-0.1.0.tar.gz

---
+++ packages/aurask-ai/aurask_ai-0.1.0.tar.gz/aurask_ai-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "aurask_ai",
+    version='0.1.0',
+    description=' aithis is a',
+    author='code sx',
+    author_email='core@ex.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+)
