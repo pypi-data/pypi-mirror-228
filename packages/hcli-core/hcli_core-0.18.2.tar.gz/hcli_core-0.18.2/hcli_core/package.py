f1b0bf667e130fd5a68361af57abe42e2be2a25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-03 04:10:29 +0000

    Add package hcli-core/0.18.2/hcli_core-0.18.2.tar.gz

---
+++ packages/hcli-core/hcli_core-0.18.2.tar.gz/hcli_core-0.18.2/hcli_core/package.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = "0.18.2"
+dependencies = ["falcon==3.1.1",
+                "openai==0.27.2",
+                "tiktoken==0.3.2"]
