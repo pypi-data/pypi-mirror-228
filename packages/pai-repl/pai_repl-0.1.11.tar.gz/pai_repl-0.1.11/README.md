dae5e36d1b92119775491152492e0a594d6d7148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:09:33 +0000

    Add package pai-repl/0.1.11/pai_repl-0.1.11.tar.gz

---
+++ packages/pai-repl/pai_repl-0.1.11.tar.gz/pai_repl-0.1.11/README.md
@@ -0,0 +1,147 @@
+# pai: a python REPL with an integrated LLM agent
+A Python REPL with a built in AI agent that uses prior REPL history for context. Supports OpenAI and llama.cpp
+
+## Goals
+AI agents doing useful stuff, on your machine.
+
+## Installation
+```
+pip install pai-repl
+```
+
+## Usage
+
+Use with openai
+```
+$ export OPENAI_API_KEY=<your key>
+$ pai --openai gpt-4
+```
+
+Use with llama.cpp compatible models
+
+```
+$ pai --llama <path to model>
+```
+
+
+## Features
+
+### Code Generation
+Generate code by typing `gen: <prompt>`. You can accept, edit or cancel the generated code.
+
+**Example**
+
+Generate code that operates on a variable setup by the user.
+```
+INP [0]> nums = [1,2,3,3,3,3,3,3,3,4]
+INP [1]> gen: what is the mean of nums?
+OK? [1]> import statistics
+    ...>
+    ...> mean = statistics.mean(nums)
+    ...> mean
+OUT [1]> 2.8
+```
+
+### AI Agent
+Ask an AI agent to complete a task. It will continuously generate and run code until it completes the task or fails. All generated code must be approved by the user.
+
+The task: "pai: there is a csv in the current directory. find it and give me a full analysis"
+
+We can see the agent doing the following:
+- Look for csvs
+- Load the csv when it finds one
+- Attempt to import pandas for analysis. This fails
+- Self correct and use standard Python functionality to analyze the data
+- Do some analysis and print the results
+
+```
+INP [0]> pai: there is a csv in the current directory. find it and give me a full analysis
+
+First, let's list all the CSV files in the current directory.
+OK? [0]> import os
+    ...>
+    ...> # List all files in the current directory
+    ...> files = os.listdir()
+    ...>
+    ...> # Filter out the ones which are csv files
+    ...> csv_files = [file for file in files if file.endswith('.csv')]
+    ...>
+    ...> csv_files
+OUT [1]> ['running_data.csv']
+The CSV file in the current directory is named 'running_data.csv'. Now, I will load the data and analyze it. Let's start by loading the data and looking at the first few rows to understand its structure.
+OK? [1]> import pandas as pd
+    ...>
+    ...> # Load the csv file into a pandas DataFrame
+    ...> data = pd.read_csv('running_data.csv')
+    ...>
+    ...> # Display the first few rows of the DataFrame
+    ...> data.head()
+OUT [1]> Traceback (most recent call last):
+  File "<string>", line 1, in <module>
+ModuleNotFoundError: No module named 'pandas'
+It appears that the pandas library, which is typically used for data analysis in Python, is not available in this environment. However, I can still provide a basic analysis using standard Python functionality.
+
+Let's first take a look at the contents of the CSV file.
+OK? [2]> import csv
+    ...>
+    ...> # Open the csv file
+    ...> with open('running_data.csv', 'r') as file:
+    ...>     reader = csv.reader(file)
+    ...>
+    ...>     # Get the headers and the first few rows to get a sense of the data
+    ...>     headers = next(reader)
+    ...>     first_few_rows = [next(reader) for _ in range(5)]
+    ...>
+    ...> headers, first_few_rows
+OUT [3]> (['Date', 'Distance', 'Speed', 'Duration'], [['2023-07-29', '10', '9', '1.1111111111111112'], ['2023-07-30', '10', '8', '1.25'], ['2023-07-31', '8', '10', '0.8'], ['2023-08-01', '3', '10', '0.3'], ['2023-08-02', '6', '9', '0.6666666666666666']])
+OK? [3]>
+    ...> # Iterate over the rows and analyze the data
+    ...>
+    ...> # Initialize some variables
+    ...> num_rows = 0
+    ...> sum_distance = 0
+    ...> sum_speed = 0
+    ...> sum_duration = 0
+    ...>
+    ...> with open('running_data.csv', 'r') as file:
+    ...>     reader = csv.reader(file)
+    ...>
+    ...>     # Skip the header
+    ...>     next(reader)
+    ...>
+    ...>     for row in reader:
+    ...>         num_rows += 1
+    ...>         sum_distance += float(row[1])
+    ...>         sum_speed += float(row[2])
+    ...>         sum_duration += float(row[3])
+    ...>
+    ...> # Compute the averages
+    ...> average_distance = sum_distance / num_rows
+    ...> average_speed = sum_speed / num_rows
+    ...> average_duration = sum_duration / num_rows
+    ...>
+    ...> average_distance, average_speed, average_duration
+OK? [4]> (5.666666666666667, 9.466666666666667, 0.6017760942760944)
+OK? [5]> Here is the analysis of the data in the `running_data.csv` file:
+
+The data contains four columns: Date, Distance, Speed, and Duration.
+
+Here are a few rows from the data:
+
+1. ['2023-07-29', '10', '9', '1.1111111111111112']
+2. ['2023-07-30', '10', '8', '1.25']
+3. ['2023-07-31', '8', '10', '0.8']
+4. ['2023-08-01', '3', '10', '0.3']
+5. ['2023-08-02', '6', '9', '0.6666666666666666']
+
+In terms of overall metrics:
+
+- The average distance is approximately 5.67 units.
+- The average speed is approximately 9.47 units.
+- The average duration is approximately 0.60 units.
+
+Please note that these averages are calculated from the existing data in the CSV file.
+
+These measures can give you a good understanding of the data you are dealing with and can be a good starting point for further analysis. Task complete.
+INP [5]>
+```
---
+++ packages/pai-repl/pai_repl-0.1.11.tar.gz/pai_repl-0.1.11/src/pai/llms/chat_gpt.py
@@ -0,0 +1,222 @@
+import json
+from typing import Any, Generator
+import openai
+
+from pai.history import HistoryNode
+from pai.llms.llm_protocol import (
+    LLM,
+    LLMError,
+    LLMResponse,
+    LLMResponseCode,
+    LLMResponseMessage,
+    LLMStreamChunk,
+)
+
+
+system_prompt = """
+You are a helpful AI assistant
+
+You can execute Python code in a REPL context.
+You can access and modify previously defined variables and functions.
+You can access the internet
+You can access the file system
+
+Do not copy variables or functions that were already defined, you have access.
+Do not redefine variables or functions that are already defined.
+Do not assume things like what operating system you are running on. Use python to find out.
+
+All code you write will be approved by a human before it is executed.
+
+When you execute Python code, the output will be given to you.
+If the task is complete, summarize the findings.
+If a task is not complete, analyze the latest output and continue.
+
+Think through a problem step by step.
+Use code to collect data.
+Break a problem into sub problems.
+Use code to solve sub problems.
+"""
+
+
+class ChatGPT(LLM):
+    model: str = "gpt-4"
+
+    def __init__(self, model: str) -> None:
+        self.model = model
+
+    def agent_support(self) -> bool:
+        return True
+
+    def description(self) -> str:
+        return f"ChatGPT: {self.model}"
+
+    def prompt(self, history: list[HistoryNode], prompt: str) -> Any:
+        # build the system prompt using the command history
+        messages = [
+            {
+                "role": "system",
+                "content": system_prompt,
+            },
+        ]
+
+        # build the messages from the history
+        for node in history:
+            if isinstance(node.data, HistoryNode.Root):
+                # skip the root node
+                continue
+            if isinstance(node.data, HistoryNode.UserCode):
+                # check if the last message is user code.
+                # if it is, then add the node data to the last message. if it isn't, then add a new message
+                if messages[-1]["role"] == "user":
+                    messages[-1][
+                        "content"
+                    ] += f">>>{node.data.code}\\n{node.data.result}\\n"
+                else:
+                    messages.append(
+                        {
+                            "role": "user",
+                            "content": f">>>{node.data.code}\\n{node.data.result}\\n",
+                        }
+                    )
+            elif isinstance(node.data, HistoryNode.LLMCode):
+                messages.extend(
+                    [
+                        {"role": "user", "content": f"{node.data.prompt}"},
+                        {
+                            "role": "assistant",
+                            "content": None,  # type: ignore
+                            "function_call": {
+                                "name": "python",
+                                "arguments": json.dumps({"code": node.data.code}),
+                            },
+                        },
+                        {
+                            "role": "function",
+                            "name": "python",
+                            "content": f"{node.data.result}",
+                        },
+                    ]
+                )
+            elif isinstance(node.data, HistoryNode.LLMMessage):
+                messages.extend(
+                    [
+                        {"role": "user", "content": f"{node.data.prompt}"},
+                        {"role": "assistant", "content": f"{node.data.message}"},
+                    ]
+                )
+            elif isinstance(node.data, HistoryNode.LLMError):
+                messages.extend(
+                    [
+                        {"role": "user", "content": f"{node.data.prompt}"},
+                        node.data.raw_resp.choices[0].message,
+                        {"role": "user", "content": f"{node.data.error}"},
+                    ]
+                )
+
+        # if there is a user prompt, then add it to the messages
+        if prompt.strip() != "":
+            if messages[-1]["role"] == "user":
+                # if the last message is a user prompt, then add the prompt to the last message
+                messages[-1]["content"] += f"\\n{prompt}"
+            else:
+                # if the last message is not a user prompt, then add a new message
+                messages.append({"role": "user", "content": f"{prompt}"})
+
+        return messages
+
+    def call(
+        self, history: list[HistoryNode], prompt: str
+    ) -> Generator[LLMStreamChunk, None, LLMResponse]:
+        # make it an empty generator
+        yield from []
+
+        messages = self.prompt(history, prompt)
+
+        resp: Any = openai.ChatCompletion.create(
+            model=self.model,
+            messages=messages,
+            # function_call={"name": "python"},
+            functions=[
+                {
+                    "name": "python",
+                    "description": "Execute Python code in the REPL.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "code": {
+                                "type": "string",
+                                "description": "The Python code to run",
+                            },
+                        },
+                    },
+                }
+            ],
+            stream=True,
+        )
+
+        raw_chunks = []
+        response_text = ""
+        func_call = {
+            "name": "",
+            "arguments": "",
+        }
+
+        # this is nasty
+        for response_chunk in resp:
+            if "choices" in response_chunk:
+                deltas = response_chunk["choices"][0]["delta"]
+                if "function_call" in deltas:
+                    if "name" in deltas["function_call"]:
+                        func_call["name"] = deltas["function_call"]["name"]
+                    if "arguments" in deltas["function_call"]:
+                        func_call["arguments"] += deltas["function_call"]["arguments"]
+                        yield LLMStreamChunk(deltas["function_call"]["arguments"])
+                elif "content" in deltas:
+                    # if the last message is a function call, close the md code block
+                    if (
+                        len(raw_chunks) > 0
+                        and "function_call" in raw_chunks[-1]["choices"][0]["delta"]
+                    ):
+                        yield LLMStreamChunk(f"\n")
+                    response_text += deltas["content"]
+                    yield LLMStreamChunk(deltas["content"])
+                if response_chunk["choices"][0]["finish_reason"] == "function_call":
+                    yield LLMStreamChunk(f"\n")
+            raw_chunks.append(response_chunk)
+
+        yield LLMStreamChunk(f"\n")
+
+        # check if the response is a function call
+        if func_call["name"] != "":
+            # parse the arguments as json
+            # expecting something like {"code": "print('hello world')"}
+            try:
+                j = json.loads(func_call["arguments"])
+            except json.JSONDecodeError as e:
+                # sometimes the function call arguments are just code, not a json object
+                # rather than erroring, we check if it is valid python code and return it if it is
+                try:
+                    compile(func_call["arguments"], "<string>", "exec")
+                    # if it is valid python code, then we return a LLMResponseCode
+                    return LLMResponseCode(
+                        prompt=prompt,
+                        code=func_call["arguments"],
+                        message=response_text or None,
+                        raw=raw_chunks,
+                    )
+                except SyntaxError:
+                    # return the original JSONDecodeError
+                    return LLMError(prompt=prompt, error=str(e), raw=resp)
+
+            return LLMResponseCode(
+                prompt=prompt,
+                code=j["code"],
+                message=response_text or None,
+                raw=raw_chunks,
+            )
+        else:
+            return LLMResponseMessage(
+                prompt=prompt,
+                message=response_text,
+                raw=raw_chunks,
+            )
