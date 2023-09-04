9c6f0c72988507174042e5165bef04b9c5bef248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:10:10 +0000

    Add package cloudgenerator/0.0.6/cloudgenerator-0.0.6.tar.gz

---
+++ packages/cloudgenerator/cloudgenerator-0.0.6.tar.gz/cloudgenerator-0.0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.6'
+DESCRIPTION = 'A safe genetator write cloudgenerator.help() to see commands'
+
+
+# Setting up
+setup(
+    name="cloudgenerator",
+    version=VERSION,
+    author="NoticedCloud",
+    author_email="noticedcloud@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["pycryptodome"],
+    keywords=['python', 'password', 'token', 'safe'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
