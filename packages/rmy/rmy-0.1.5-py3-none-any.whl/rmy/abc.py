fa4649bb1e3edee5e21fa4deab1213f164341a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:10:05 +0000

    Add package rmy/0.1.5/rmy-0.1.5-py3-none-any.whl

---
+++ packages/rmy/rmy-0.1.5-py3-none-any.whl/rmy/abc.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from abc import ABCMeta, abstractmethod
+from typing import Any, AsyncIterator, Tuple
+
+
+class Connection(AsyncIterator[Any], metaclass=ABCMeta):
+    @abstractmethod
+    async def send(self, message: Tuple[Any, ...]) -> int:
+        ...
+
+    @abstractmethod
+    def send_nowait(self, message: Tuple[Any, ...]) -> int:
+        ...
+
+    @abstractmethod
+    async def drain(self):
+        ...
+
+    @abstractmethod
+    def close(self):
+        ...
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self) -> Any:
+        ...
