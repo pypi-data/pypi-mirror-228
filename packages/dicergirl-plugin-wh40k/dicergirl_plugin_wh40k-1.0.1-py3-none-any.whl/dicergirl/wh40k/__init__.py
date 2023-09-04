1706ca6237fc4bf71702e475542d9db644988be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:09:49 +0000

    Add package dicergirl-plugin-wh40k/1.0.1/dicergirl_plugin_wh40k-1.0.1-py3-none-any.whl

---
+++ packages/dicergirl-plugin-wh40k/dicergirl_plugin_wh40k-1.0.1-py3-none-any.whl/dicergirl/wh40k/__init__.py
@@ -0,0 +1,16 @@
+from .nbhandlers import commands
+from .wh40kcards import wh40k_cards, wh40k_cache_cards
+
+wh40k_cards.load()
+
+__version__ = "1.0.1"
+
+__type__ = "plugin"
+__charactor__ = None
+__name__ = "wh40k"
+__cname__ = "帝皇的炮灰"
+__cards__ = wh40k_cards
+__cache__ = wh40k_cache_cards
+__nbhandler__ = nbhandlers
+__nbcommands__ = commands
+__description__ = "WH40K 模式是以设定 战锤40K(Warhammer 40,000: Fire Warrior) 为背景的 TRPG 跑团模式."
---
+++ packages/dicergirl-plugin-wh40k/dicergirl_plugin_wh40k-1.0.1-py3-none-any.whl/dicergirl/wh40k/wh40kcards.py
@@ -0,0 +1,4 @@
+from dicergirl.utils.cards import Cards
+
+wh40k_cards = Cards(mode="wh40k")
+wh40k_cache_cards = Cards(mode="wh40k")
