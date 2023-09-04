aada3ae161d6161f4d3dc2e30faa8135cb6f4ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:09:31 +0000

    Add package riemapp/0.2.0/riemapp-0.2.0-py3-none-any.whl

---
+++ packages/riemapp/riemapp-0.2.0-py3-none-any.whl/riemapp/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2022 Saransh Chopra. All rights reserved.
+
+riemapp: Manim powered complex mappings.
+"""
+
+from __future__ import annotations
+
+import riemapp.geometry  # noqa: F401
+from riemapp.core import ComplexMap  # noqa: F401
+from riemapp.version import version as __version__  # noqa: F401
