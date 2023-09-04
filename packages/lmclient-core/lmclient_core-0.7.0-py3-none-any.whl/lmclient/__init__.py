c0cb4b81ea33f0b48cefcc12d96a63430555cc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:09:30 +0000

    Add package lmclient-core/0.7.0/lmclient_core-0.7.0-py3-none-any.whl

---
+++ packages/lmclient-core/lmclient_core-0.7.0-py3-none-any.whl/lmclient/__init__.py
@@ -0,0 +1,40 @@
+from lmclient.chat_engine import ChatEngine
+from lmclient.client import LMClient
+from lmclient.models import (
+    AzureChat,
+    MinimaxProChat,
+    MinimaxProChatParameters,
+    OpenAIChat,
+    OpenAIChatParameters,
+    WenxinChat,
+    WenxinChatParameters,
+    ZhiPuChat,
+    ZhiPuChatParameters,
+)
+from lmclient.utils import BaseSchema, PydanticVersion, function
+from lmclient.version import __version__
+
+if PydanticVersion == 1:
+    from pydantic import BaseModel
+
+    BaseModel.model_copy = BaseModel.copy  # type: ignore
+    BaseModel.model_dump = BaseModel.dict  # type: ignore
+    BaseModel.model_dump_json = BaseModel.json  # type: ignore
+
+
+__all__ = [
+    'LMClient',
+    'ChatEngine',
+    'AzureChat',
+    'OpenAIChat',
+    'OpenAIChatParameters',
+    'MinimaxProChat',
+    'MinimaxProChatParameters',
+    'ZhiPuChat',
+    'ZhiPuChatParameters',
+    'WenxinChat',
+    'WenxinChatParameters',
+    'BaseSchema',
+    'function',
+    '__version__',
+]
