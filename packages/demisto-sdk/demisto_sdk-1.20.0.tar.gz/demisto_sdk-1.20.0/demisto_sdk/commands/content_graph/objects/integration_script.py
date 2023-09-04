2d321ac12078f1d59e32cbcc27971745b3c84f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:10:13 +0000

    Add package demisto-sdk/1.20.0/demisto_sdk-1.20.0.tar.gz

---
+++ packages/demisto-sdk/demisto_sdk-1.20.0.tar.gz/demisto_sdk-1.20.0/demisto_sdk/commands/content_graph/objects/integration_script.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from typing import List, Optional
+
+from pydantic import Field
+
+from demisto_sdk.commands.common.constants import (
+    NATIVE_IMAGE_FILE_NAME,
+    MarketplaceVersions,
+)
+from demisto_sdk.commands.common.logger import logger
+from demisto_sdk.commands.common.native_image import (
+    ScriptIntegrationSupportedNativeImages,
+    file_to_native_image_config,
+)
+from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
+from demisto_sdk.commands.prepare_content.integration_script_unifier import (
+    IntegrationScriptUnifier,
+)
+
+
+class IntegrationScript(ContentItem):
+    type: str
+    docker_image: Optional[str]
+    description: Optional[str] = Field("")
+    is_unified: bool = Field(False, exclude=True)
+    code: Optional[str] = Field(None, exclude=True)
+    unified_data: dict = Field(None, exclude=True)
+
+    def prepare_for_upload(
+        self,
+        current_marketplace: MarketplaceVersions = MarketplaceVersions.XSOAR,
+        **kwargs,
+    ) -> dict:
+        data = (
+            self.data
+            if kwargs.get("unify_only")
+            else super().prepare_for_upload(current_marketplace)
+        )
+        data = IntegrationScriptUnifier.unify(
+            self.path, data, current_marketplace, **kwargs
+        )
+        self.unified_data = data
+        return data
+
+    def get_supported_native_images(
+        self, marketplace: MarketplaceVersions, ignore_native_image: bool = False
+    ) -> List[str]:
+        if marketplace == MarketplaceVersions.XSOAR and not ignore_native_image:
+            if not Path(f"Tests/{NATIVE_IMAGE_FILE_NAME}").exists():
+                logger.debug(f"The {NATIVE_IMAGE_FILE_NAME} file could not be found.")
+                return []
+            return ScriptIntegrationSupportedNativeImages(
+                _id=self.object_id,
+                docker_image=self.docker_image,
+                native_image_config=file_to_native_image_config(),
+            ).get_supported_native_image_versions(get_raw_version=True)
+        return []
