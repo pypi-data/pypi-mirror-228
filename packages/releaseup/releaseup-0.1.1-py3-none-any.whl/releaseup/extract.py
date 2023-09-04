09e258fd7788df437de3a00d8037748af75e5bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:10:02 +0000

    Add package releaseup/0.1.1/releaseup-0.1.1-py3-none-any.whl

---
+++ packages/releaseup/releaseup-0.1.1-py3-none-any.whl/releaseup/extract.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+import subprocess
+
+
+def get_diff(
+    tags: list[str], path: str = "./", output_filename: str = "diff.txt"
+) -> None:
+    """
+    Writes `git diff` to a file.
+
+    Args:
+        tags:
+            A list of tags or commits (ideally 2 most recent) to find diff between.
+        path:
+            Path of the sub-directory to find the diff of.
+        output_filename:
+            Path of the file to store the diff in.
+    """
+    with open(output_filename, "w+") as f:
+        subprocess.run(["git", "diff", tags[0], tags[1], "--", path], stdout=f)
+
+
+def extract_additions(filename: str) -> list[str]:
+    """
+    Extracts all the additions made to a codebase from a file containing `git diff`.
+
+    Args:
+        filename:
+            File containing the `git diff`.
+
+    Returns:
+        extracted_additions:
+            A list of additions made to the codebase.
+    """
+    with open(filename) as f:
+        lines = f.readlines()
+        extracted_additions = [line for line in lines if line[0] == "+"]
+
+    return extracted_additions
+
+
+def preprocess_additions(extracted_additions: list[str]) -> list[str]:
+    """
+    Preprocesses the extracted additions from `git diff`.
+
+    Args:
+        extracted_additions:
+            A `git diff` containing only the additions.
+
+    Returns:
+        preprocessed_additions:
+            Preprocessed additions.
+    """
+    preprocessed_additions = extracted_additions.copy()
+    preprocessed_additions = [f"{line.strip()}\n" for line in preprocessed_additions]
+    for line in preprocessed_additions:
+        if line[:3] == "+++":
+            preprocessed_additions.pop(preprocessed_additions.index(line))
+
+    preprocessed_additions = [line[1:] for line in preprocessed_additions]
+    preprocessed_additions = [f"{line.strip()}\n" for line in preprocessed_additions]
+
+    return preprocessed_additions
+
+
+def get_comments_and_docstrings(preprocessed_additions: list[str]) -> list[str]:
+    """
+    Extracts comments and docstrings from preprocessed `git diff` output.
+
+    Args:
+        preprocessed_additions:
+            A list of preprocessed additions from `git diff`.
+
+    Returns:
+        extracted_docs:
+            Extracted comments and docstrings from the preprocessed_additions.
+    """
+    comments_and_docstrings = [
+        line[line.find("#") + 1 :].replace("\n", "").strip()
+        for line in preprocessed_additions
+        if "#" in line
+    ]
+    lines = " ".join(preprocessed_additions).replace("\n", " ")
+    i = 0
+
+    while i < len(lines):
+        if lines[i : i + 3] == '"""':
+            idx_next_quotes = lines[i + 3 :].find('"""')
+            # args = (
+            #     lines[i + 3 :].find("Args:")
+            #     if lines[i + 3 :].find("Args:") != -1
+            #     else sys.maxsize
+            # )
+            # returns = (
+            #     lines[i + 3 :].find("Returns:")
+            #     if lines[i + 3 :].find("Returns:") != -1
+            #     else sys.maxsize
+            # )
+            # examples = (
+            #     lines[i + 3 :].find("Examples:")
+            #     if lines[i + 3 :].find("Examples:") != -1
+            #     else sys.maxsize
+            # )
+            # min(args, returns, examples)
+            # if idx != sys.maxsize:
+            comments_and_docstrings.append(lines[i + 3 : i + idx_next_quotes + 3])
+            i += idx_next_quotes + 6
+            continue
+        i += 1
+
+    extracted_docs = [x.strip() for x in comments_and_docstrings]
+    extracted_docs = [doc.replace("\n", "").replace("\t", "") for doc in extracted_docs]
+    extracted_docs = [f"{x.strip()}\n" for x in comments_and_docstrings]
+
+    i = 0
+    while i < len(extracted_docs):
+        line = extracted_docs[i]
+        if "noqa" in line or "type:" in line or line == "\n" or "todo" in line.lower():
+            extracted_docs.pop(i)
+        else:
+            i += 1
+
+    return extracted_docs
+
+
+def save(content: list[str], filename: str) -> None:
+    """
+    Save contents in a file.
+
+    Args:
+        content:
+            A list of contents to be saved.
+        filename:
+            Path of the file in which the contents have to be saved
+    """
+    with open(filename, "w+") as f:
+        f.write("".join(content))
---
+++ packages/releaseup/releaseup-0.1.1-py3-none-any.whl/releaseup/nlp_backend.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from heapq import nlargest
+
+import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def get_tfid_scores(comments: list[str]) -> dict[str, float]:
+    """
+    Extract TF-IDF scores for a list of strings.
+
+    Args:
+        comments:
+            Strings for the TF-IDF score.
+
+    Returns:
+        word_score:
+            A dict of words mapped to their TF-IDF scores.
+    """
+    v = TfidfVectorizer()
+    v.fit(comments)
+    v.transform(comments)
+
+    all_feature_names = v.get_feature_names_out()
+    word_score = {}
+
+    for word in all_feature_names:
+        indx = v.vocabulary_.get(word)
+        word_score[word] = v.idf_[indx]
+
+    return word_score
+
+
+def get_release_notes(
+    comments: list[str],
+    word_score: dict[str, float],
+    output_filename: str = "RELEASE_NOTES.txt",
+    model_name: str = "en_core_web_sm",
+    threshold: float = 0.3,
+) -> list[str]:
+    """
+    Generate release_notes (or release notes) from the extracted
+    comments and TF-IDF scores.
+
+    Args:
+        comments:
+            A list of extracted comments.
+        word_score:
+            A dict of words mapped to their TF-IDF scores.
+        output_filename:
+            Path of the file where release notes are to be dumped.
+        model_name:
+            spacy's model name. See https://spacy.io/usage/models.
+        threshold:
+            Percentage of comments to be included in the release notes.
+
+    Returns:
+        release_notes:
+            Release notes a s a list of strings.
+    """
+    nlp = spacy.load(model_name)
+
+    comments_str = " ".join(comments).replace("\n", "")
+    doc = nlp(comments_str)
+    sentences = list(doc.sents)
+    sent_scores: dict[str, float] = {}
+
+    for sent in sentences:
+        for word in sent:
+            if word.text.lower() in word_score.keys():
+                if sent not in sent_scores.keys():
+                    sent_scores[sent] = word_score[word.text.lower()]
+                else:
+                    sent_scores[sent] += word_score[word.text.lower()]
+
+    select_length = int(len(sentences) * 0.3)
+    release_notes = nlargest(
+        select_length, sent_scores, key=sent_scores.get
+    )  # type: ignore[arg-type]
+    release_notes = [f"{sentence}\n" for sentence in release_notes]
+
+    with open(output_filename, "w+") as f:
+        f.writelines(release_notes)
+
+    return release_notes
