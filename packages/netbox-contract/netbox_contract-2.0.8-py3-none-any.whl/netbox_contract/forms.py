ccd7d171ff6770d98b67530cc9ca8eab2b34032d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:10:11 +0000

    Add package netbox-contract/2.0.8/netbox_contract-2.0.8-py3-none-any.whl

---
+++ packages/netbox-contract/netbox_contract-2.0.8-py3-none-any.whl/netbox_contract/forms.py
@@ -0,0 +1,241 @@
+from django import forms
+from django.contrib.contenttypes.models import ContentType
+import django_filters
+from netbox.forms import NetBoxModelForm, NetBoxModelFilterSetForm, NetBoxModelBulkEditForm, NetBoxModelImportForm
+from utilities.forms.fields import CommentField, CSVChoiceField, DynamicModelChoiceField, DynamicModelMultipleChoiceField, CSVModelChoiceField, SlugField, CSVContentTypeField
+from utilities.forms.widgets import DatePicker
+from extras.filters import TagFilter
+from circuits.models import Circuit
+from tenancy.models import Tenant
+from .models import Contract, Invoice, ServiceProvider, StatusChoices, ContractAssignement
+
+class ContractForm(NetBoxModelForm):
+    comments = CommentField()
+
+    external_partie=DynamicModelChoiceField(
+        queryset=ServiceProvider.objects.all()
+    )
+    tenant=DynamicModelChoiceField(
+        queryset=Tenant.objects.all()
+    )
+    parent=DynamicModelChoiceField(
+        queryset=Contract.objects.all(),
+        required=False
+    )
+
+    class Meta:
+        model = Contract
+        fields = ('name', 'external_partie', 'external_reference', 'internal_partie','tenant', 'status',
+          'start_date', 'end_date','initial_term', 'renewal_term', 'currency','accounting_dimensions',
+          'mrc', 'nrc','invoice_frequency','parent','documents', 'comments', 'tags')
+
+        widgets = {
+            'start_date': DatePicker(),
+            'end_date': DatePicker(),
+        }
+
+class InvoiceForm(NetBoxModelForm):
+    contracts=DynamicModelMultipleChoiceField(
+        queryset=Contract.objects.all(),
+        required=False
+    )
+
+    class Meta:
+        model = Invoice
+        fields = ('number', 'date', 'contracts','period_start', 'period_end',
+          'currency','accounting_dimensions','amount', 'documents','comments','tags')
+        widgets = {
+            'date': DatePicker(),
+            'period_start': DatePicker(),
+            'period_end': DatePicker(),
+        }
+
+class ContractFilterSetForm(NetBoxModelFilterSetForm):
+    model = Contract
+    external_partie=DynamicModelChoiceField(
+        queryset=ServiceProvider.objects.all(),
+        required=False
+    )
+    tenant=DynamicModelChoiceField(
+        queryset=Tenant.objects.all(),
+        required=False
+    )
+    external_reference=forms.CharField(
+        required=False
+    )
+    internal_partie= forms.CharField(
+        required=False
+    )
+    status = forms.ChoiceField(
+        choices=StatusChoices,
+        required= False
+    )
+    parent=DynamicModelChoiceField(
+        queryset=Contract.objects.all(),
+        required=False
+    )
+
+class InvoiceFilterSetForm(NetBoxModelFilterSetForm):
+    model = Invoice
+    contracts=DynamicModelMultipleChoiceField(
+        queryset=Contract.objects.all(),
+        required=False
+    )
+
+class ContractCSVForm(NetBoxModelImportForm):
+    external_partie = CSVModelChoiceField(
+        queryset=ServiceProvider.objects.all(),
+        to_field_name='name',
+        help_text='Service provider name'
+    )
+    tenant = CSVModelChoiceField(
+        queryset=Tenant.objects.all(),
+        to_field_name='name',
+        help_text='Tenant name',
+        required=False
+    )
+    status = CSVChoiceField(
+        choices=StatusChoices,
+        help_text='Contract status'
+    )
+    parent = CSVModelChoiceField(
+        queryset=Contract.objects.all(),
+        to_field_name='name',
+        help_text='Contract name',
+        required=False
+    )
+
+    class Meta:
+        model = Contract
+        fields = [
+            'name', 'external_partie', 'external_reference', 'internal_partie', 'tenant', 'status',
+            'start_date', 'end_date','initial_term', 'renewal_term', 'currency', 'accounting_dimensions',
+            'mrc', 'nrc','invoice_frequency', 'documents', 'comments', 'parent'
+        ]
+
+class ContractBulkEditForm(NetBoxModelBulkEditForm):
+    name = forms.CharField(
+        max_length=100,
+        required=True
+    )
+    external_partie = DynamicModelChoiceField(
+        queryset=ServiceProvider.objects.all(),
+        required=False
+    )
+    external_reference=forms.CharField(
+        max_length=100,
+        required=False
+    )
+    internal_partie = forms.CharField(
+        max_length=30,
+        required=True
+    )
+    comments = CommentField()
+    circuit=DynamicModelChoiceField(
+        queryset=Circuit.objects.all(),
+        required=False
+    )
+    parent = DynamicModelChoiceField(
+        queryset=Contract.objects.all(),
+        required=False
+    )
+
+    nullable_fields = (
+        'comments',
+    )
+    model = Contract
+
+class InvoiceCSVForm(NetBoxModelImportForm):
+    contracts = CSVModelChoiceField(
+        queryset=Contract.objects.all(),
+        to_field_name='name',
+        help_text='Related Contracts'
+    )
+
+    class Meta:
+        model = Invoice
+        fields = [
+            'number','date','contracts','period_start', 'period_end',
+            'currency','accounting_dimensions','amount','documents','comments','tags'
+        ]
+
+class InvoiceBulkEditForm(NetBoxModelBulkEditForm):
+    number = forms.CharField(
+        max_length=100,
+        required=True
+    )
+    contracts=DynamicModelMultipleChoiceField(
+        queryset=Contract.objects.all(),
+        required=False
+    )
+    model = Invoice
+
+# service Provider forms
+
+class ServiceProviderForm(NetBoxModelForm):
+    slug = SlugField()
+    comments = CommentField()
+
+    class Meta:
+        model = ServiceProvider
+        fields = ('name', 'slug','portal_url',
+            'comments', 'tags')
+
+class ServiceProviderFilterSetForm(NetBoxModelFilterSetForm):
+    model = ServiceProvider
+    tag = TagFilter()
+
+class ServiceProviderCSVForm(NetBoxModelImportForm):
+    slug = SlugField()
+    comments = CommentField()
+    class Meta:
+        model = ServiceProvider
+        fields = [
+            'name', 'slug','portal_url',
+            'comments', 'tags'
+        ]
+
+class ServiceProviderBulkEditForm(NetBoxModelBulkEditForm):
+    name = forms.CharField(
+        max_length=100,
+        required=True
+    )
+    comments = CommentField()
+    nullable_fields = (
+        'comments',
+    )
+    model = ServiceProvider
+
+# ContractAssignement
+
+class ContractAssignementForm(NetBoxModelForm):
+    contract=DynamicModelChoiceField(
+        queryset=Contract.objects.all()
+    )
+
+    class Meta:
+        model = ContractAssignement
+        fields = ['content_type', 'object_id', 'contract','tags']
+        widgets = {
+            'content_type': forms.HiddenInput(),
+            'object_id': forms.HiddenInput(),
+        }
+
+class ContractAssignementFilterSetForm(NetBoxModelFilterSetForm):
+    model = ContractAssignement
+    contract=DynamicModelChoiceField(
+        queryset=Contract.objects.all()
+    )
+
+class ContractAssignementImportForm(NetBoxModelImportForm):
+    content_type = CSVContentTypeField(
+        queryset=ContentType.objects.all(),
+        help_text="Content Type in the form <app>.<model>"
+    )
+    contract = CSVModelChoiceField(
+        queryset=Contract.objects.all(),
+        help_text="Contract id"
+    )
+    class Meta:
+        model = ContractAssignement
+        fields = ['content_type', 'object_id', 'contract','tags']
---
+++ packages/netbox-contract/netbox_contract-2.0.8-py3-none-any.whl/netbox_contract/template_content.py
@@ -0,0 +1,63 @@
+from django.contrib.contenttypes.models import ContentType
+from extras.plugins import PluginTemplateExtension
+from circuits.models import Circuit
+from dcim.models import Device
+from virtualization.models import VirtualMachine
+from .models import ContractAssignement
+from . import tables
+
+class CircuitContracts(PluginTemplateExtension):
+    model = 'circuits.circuit'
+
+    def full_width_page(self):
+        circuit = self.context['object']
+        table = tables.ContractListBottomTable(circuit.contracts.all())
+        table.configure(self.context['request'])
+
+        return self.render('contract_list_bottom.html', extra_context={
+            'contracts_table': table,
+        })
+
+class CircuitContractAssignements(PluginTemplateExtension):
+    model = 'circuits.circuit'
+
+    def full_width_page(self):
+        circuit = self.context['object']
+        circuit_type = ContentType.objects.get_for_model(Circuit)
+        contract_assignements = ContractAssignement.objects.filter(content_type__pk=circuit_type.id, object_id=circuit.id)
+        assignements_table = tables.ContractAssignementObjectTable(contract_assignements)
+        assignements_table.configure(self.context['request'])
+
+        return self.render('contract_assignements_bottom.html', extra_context={
+            'assignements_table': assignements_table,
+        })
+
+class DeviceContractAssignements(PluginTemplateExtension):
+    model = 'dcim.device'
+
+    def full_width_page(self):
+        device = self.context['object']
+        device_type = ContentType.objects.get_for_model(Device)
+        contract_assignements = ContractAssignement.objects.filter(content_type__pk=device_type.id, object_id=device.id)
+        assignements_table = tables.ContractAssignementObjectTable(contract_assignements)
+        assignements_table.configure(self.context['request'])
+
+        return self.render('contract_assignements_bottom.html', extra_context={
+            'assignements_table': assignements_table,
+        })
+
+class VMContractAssignements(PluginTemplateExtension):
+    model = 'virtualization.virtualmachine'
+
+    def full_width_page(self):
+        device = self.context['object']
+        device_type = ContentType.objects.get_for_model(VirtualMachine)
+        contract_assignements = ContractAssignement.objects.filter(content_type__pk=device_type.id, object_id=device.id)
+        assignements_table = tables.ContractAssignementObjectTable(contract_assignements)
+        assignements_table.configure(self.context['request'])
+
+        return self.render('contract_assignements_bottom.html', extra_context={
+            'assignements_table': assignements_table,
+        })
+
+template_extensions = [ CircuitContracts, CircuitContractAssignements, DeviceContractAssignements, VMContractAssignements]
