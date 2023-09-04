09ee8acc7a5aeb57dbdbef7324ae0a6b4d37b94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:10:00 +0000

    Add package AsiaChannel/2023.9.1/AsiaChannel-2023.9.1.tar.gz

---
+++ packages/AsiaChannel/AsiaChannel-2023.9.1.tar.gz/AsiaChannel-2023.9.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools  # 导入setuptools打包工具
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AsiaChannel",
+    version="2023.9.1",
+    author="anzechannel",
+    author_email="348834851@qq.com",
+    description="这是一个分享亚洲风土人情的软件包。",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
